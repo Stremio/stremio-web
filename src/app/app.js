c610ab6012d5bf9a7c8f9723f1d15938998590ee
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-06-04 17:40:36 +0300

    Checkbox rendered in the app root

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Checkbox } from 'stremio-common';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <span>Stremio</span>
+                <Checkbox checked={true} />
             </div>
         );
     }
