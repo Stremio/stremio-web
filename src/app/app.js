ae412046f97cc67444cce79014737535518b2216
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-06-11 16:41:01 +0300

    vector icon rendered

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Checkbox } from 'stremio-common';
+import Icon from 'stremio-icons/dom'
 
 class App extends Component {
     render() {
@@ -8,6 +9,11 @@ class App extends Component {
             <BrowserRouter>
                 <div>
                     <Checkbox checked={true} />
+                    <Icon
+                        style={{width: 100, height: 100}}
+                        icon={'ic_pause'}
+                        fill={'yellow'}
+                    />
                     <Route path={'/home/:message'} component={({ match }) => <span>{match.params.message}</span>} />
                 </div>
             </BrowserRouter>
