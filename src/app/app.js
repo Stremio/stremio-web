4a755af113fcc514d1efb3f44514d018ccc2a2d9
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-06-14 19:49:04 +0300

    Calendar route added

--- src/app/app.js
+++ src/app/app.js
@@ -3,9 +3,10 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import NavBar from './NavBar';
 import styles from './styles';
 
-const Board = () => <div style={{ backgroundColor: 'pink', paddingTop: 40 }}>You're on the Board Tab</div>;
-const Discover = () => <div style={{ backgroundColor: 'pink', paddingTop: 40 }}>You're on the Discover Tab</div>;
-const Library = () => <div style={{ backgroundColor: 'pink', paddingTop: 40 }}>You're on the Library Tab</div>;
+const Board = () => <div style={{ color: 'pink', paddingTop: 40 }}>You're on the Board Tab</div>;
+const Discover = () => <div style={{ color: 'pink', paddingTop: 40 }}>You're on the Discover Tab</div>;
+const Library = () => <div style={{ color: 'pink', paddingTop: 40 }}>You're on the Library Tab</div>;
+const Calendar = () => <div style={{ color: 'pink', paddingTop: 40 }}>You're on the Calendar Tab</div>;
 
 class App extends Component {
     render() {
@@ -17,6 +18,7 @@ class App extends Component {
                         <Route path={'/'} exact={true} component={Board} />
                         <Route path={'/discover'} component={Discover} />
                         <Route path={'/library'} component={Library} />
+                        <Route path={'/calendar'} component={Calendar} />
                         <Redirect to={'/'} />
                     </Switch>
                 </div>
