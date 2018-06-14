7b0b06bf88e3c9f62a7e268bbd87960738764685
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-06-14 15:27:42 +0300

    simple tab navigation implemented

--- src/app/app.js
+++ src/app/app.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { Checkbox } from 'stremio-common';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import Icon from 'stremio-icons/dom';
 import styles from './styles';
+
+const Board = () => <div style={{ backgroundColor: 'pink', paddingTop: 40 }}>You're on the Board Tab</div>;
+const Discover = () => <div style={{ backgroundColor: 'pink', paddingTop: 40 }}>You're on the Discover Tab</div>;
+const Library = () => <div style={{ backgroundColor: 'pink', paddingTop: 40 }}>You're on the Library Tab</div>;
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <div>
-                    <Checkbox checked={true} />
-                    <Icon
-                        style={{width: 100, height: 100}}
-                        icon={'ic_pause'}
-                        fill={'yellow'}
-                    />
-                    <Route path={'/home/:message'} component={({ match }) => <span>{match.params.message}</span>} />
+                <div className={styles['app']}>
+                    <div className={styles['nav-bar']}>
+                        <NavLink className={styles['nav-tab']} activeClassName={styles['active']} to={'/'} exact={true} replace={false}>
+                            <Icon
+                                className={styles['nav-tab-icon']}
+                                icon={'ic_board'}
+                            />
+                            Board
+                        </NavLink>
+                        <NavLink className={styles['nav-tab']} activeClassName={styles['active']} to={'/discover'} replace={false}>
+                            <Icon
+                                className={styles['nav-tab-icon']}
+                                icon={'ic_discover'}
+                            />
+                            Discover
+                        </NavLink>
+                        <NavLink className={styles['nav-tab']} activeClassName={styles['active']} to={'/library'} replace={false}>
+                            <Icon
+                                className={styles['nav-tab-icon']}
+                                icon={'ic_library'}
+                            />
+                            Library
+                        </NavLink>
+                    </div>
+                    <Switch>
+                        <Route path={'/'} exact={true} component={Board} />
+                        <Route path={'/discover'} component={Discover} />
+                        <Route path={'/library'} component={Library} />
+                    </Switch>
                 </div>
             </BrowserRouter>
         );
