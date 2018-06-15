aeb4c0e943b807e606afd35552c2ff62281365eb
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-06-15 14:33:05 +0300

    navigation bar configured for tab navigation

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { Board, Discover, Library, Calendar } from 'stremio-routes';
+import { Board, Discover, Library, Calendar, Addons } from 'stremio-routes';
 import NavBar from './NavBar';
 import styles from './styles';
 
@@ -9,12 +9,26 @@ class App extends Component {
         return (
             <BrowserRouter>
                 <div className={styles['app']}>
-                    <NavBar />
                     <Switch>
-                        <Route path={'/'} exact={true} component={Board} />
-                        <Route path={'/discover'} component={Discover} />
-                        <Route path={'/library'} component={Library} />
-                        <Route path={'/calendar'} component={Calendar} />
+                        <Route path={'/(discover|library|calendar)?'} exact={true}>
+                            <div className={styles['app']}>
+                                <NavBar
+                                    tabs={[
+                                        { icon: 'ic_board', label: 'Board', to: '/', exact: true },
+                                        { icon: 'ic_discover', label: 'Discover', to: '/discover' },
+                                        { icon: 'ic_library', label: 'Library', to: '/library' },
+                                        { icon: 'ic_calendar', label: 'Calendar', to: '/calendar' }
+                                    ]}
+                                />
+                                <Switch>
+                                    <Route path={'/'} exact={true} component={Board} />
+                                    <Route path={'/discover'} component={Discover} />
+                                    <Route path={'/library'} component={Library} />
+                                    <Route path={'/calendar'} component={Calendar} />
+                                </Switch>
+                            </div>
+                        </Route>
+                        <Route path={'/addons'} component={Addons} />
                         <Redirect to={'/'} />
                     </Switch>
                 </div>
