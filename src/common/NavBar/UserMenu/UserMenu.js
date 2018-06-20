60ba1c91724a7a502b87fc585283bdb27220dea0
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-06-20 17:31:14 +0300

    user menu rendered as a popup

--- src/common/NavBar/UserMenu/UserMenu.js
+++ src/common/NavBar/UserMenu/UserMenu.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Icon from 'stremio-icons/dom';
+import { Popup } from 'stremio-common';
 import styles from './styles';
 
-class UserMenu extends Component {
-    open = () => {
-
-    }
-
+class UserMenu extends PureComponent {
     render() {
         return (
-            <button className={styles['menu-button']} onClick={this.open}>
-                <Icon className={styles['user-icon']} icon={'ic_user'} />
-            </button>
+            <Popup>
+                <Popup.Label>
+                    <button className={styles['popup-label-container']}>
+                        <Icon className={styles['user-icon']} icon={'ic_user'} />
+                    </button>
+                </Popup.Label>
+                <Popup.Menu width={300}>
+                    <div style={{ background: 'red', width: '100%', height: 300 }}>kopele</div>
+                </Popup.Menu>
+            </Popup>
         );
     }
 }
