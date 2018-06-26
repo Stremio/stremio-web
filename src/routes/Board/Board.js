d8bb2a8ba764f888c3c5cdc9cc161682d7316c62
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-06-26 13:06:15 +0300

    board sync catalogs

--- src/routes/Board/Board.js
+++ src/routes/Board/Board.js
@@ -1,9 +1,36 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import { Catalogs } from 'stremio-aggregators';
+import { addons } from 'stremio-services';
 
-class Board extends Component {
+class Board extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.aggregator = new Catalogs(addons.addons);
+
+        this.state = {
+            catalogs: []
+        };
+    }
+
+    componentDidMount() {
+        this.aggregator.evs.addListener('updated', this.onCatalogsUpdated);
+        this.aggregator.run();
+    }
+
+    componentWillUnmount() {
+        this.aggregator.evs.removeListener('updated', this.onCatalogsUpdated);
+    }
+
+    onCatalogsUpdated = () => {
+        this.setState({ catalogs: this.aggregator.results.slice() });
+    }
+
     render() {
         return (
-            <div style={{ color: 'pink', paddingTop: 40 }}>You're on the Board Tab</div>
+            <div style={{ paddingTop: 40 }}>
+                You're on the Board Tab
+            </div>
         );
     }
 }
