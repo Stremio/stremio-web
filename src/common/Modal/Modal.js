262077e6036f77ae8ed5f95c466ec53160af1bcf
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-11-18 12:58:52 +0200

    Modal simplified

--- src/common/Modal/Modal.js
+++ src/common/Modal/Modal.js
@@ -1,32 +1,6 @@
-import { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Modal extends PureComponent {
-    constructor(props) {
-        super(props);
-
-        this.modalElement = document.createElement('div');
-    }
-
-    componentWillMount() {
-        document.body.appendChild(this.modalElement);
-    }
-
-    componentWillUnmount() {
-        document.body.removeChild(this.modalElement);
-    }
-
-    render() {
-        this.modalElement.className = this.props.className;
-        this.modalElement.onclick = this.props.onRequestClose;
-        return ReactDOM.createPortal(this.props.children, this.modalElement);
-    }
-}
-
-Modal.propTypes = {
-    onRequestClose: PropTypes.func,
-    className: PropTypes.string
-};
+const Modal = (props) => ReactDOM.createPortal(<div {...props} />, document.body);
 
 export default Modal;
