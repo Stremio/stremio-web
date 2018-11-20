a154d9ef2023fc64c70c48862914543a50d47407
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-11-20 15:41:11 +0200

    ref prop override other props in Popup.Label

--- src/common/Popup/Label.js
+++ src/common/Popup/Label.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Label = ({ children, ...props }, ref) => {
-    return React.cloneElement(React.Children.only(children), { ref, ...props });
+    return React.cloneElement(React.Children.only(children), { ...props, ref });
 };
 
 export default React.forwardRef(Label);
