6ad9f72a380b6061d7de74cb49ca5ca6aa9ef1f4
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2018-11-20 10:53:24 +0200

    Popup.Label used named function for forwardRef

--- src/common/Popup/Label.js
+++ src/common/Popup/Label.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Label = React.forwardRef(({ children, ...props }, ref) => {
+const Label = ({ children, ...props }, ref) => {
     return React.cloneElement(React.Children.only(children), { ref, ...props });
-});
+};
 
-export default Label;
+export default React.forwardRef(Label);
