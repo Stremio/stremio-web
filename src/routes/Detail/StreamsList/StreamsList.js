9fe40eb5b6379b3255cab7618fccc20bb4a1e3cb
svetlagasheva <svetulka_24@mail.bg>
2018-12-05 18:28:18 +0200

    streams list created

---
+++ src/routes/Detail/StreamsList/StreamsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StreamsList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default StreamsList;
