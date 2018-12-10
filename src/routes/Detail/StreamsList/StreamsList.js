e3f6896e9fa196be33a86d138a3104b90a0eba93
svetlagasheva <svetulka_24@mail.bg>
2018-12-10 13:49:19 +0200

    streams list fixes

--- src/routes/Detail/StreamsList/StreamsList.js
+++ src/routes/Detail/StreamsList/StreamsList.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 import Icon from 'stremio-icons/dom';
 import Stream from './Stream';
 import styles from './styles';
 
-class StreamsList extends Component {
-    render() {
-        return (
-            <div className={styles['streams-list-container']}>
-                <div className={styles['scroll-container']}>
-                    <div className={classnames(styles['streams-list'])}>
-                        {this.props.streams
-                            .map((stream) =>
-                                <Stream key={stream.id}
-                                    className={styles['stream']}
-                                    logo={stream.logo}
-                                    sourceName={stream.sourceName}
-                                    title={stream.title}
-                                    subtitle={stream.subtitle}
-                                    progress={stream.progress}
-                                />
-                            )}
-                    </div>
-                    <div className={styles['button']} onClick={this.onMoreAddonsClicked}>
-                        <Icon className={styles['button-icon']} icon={'ic_addons'} /> More Add-ons
-                    </div>
+const StreamsList = (props) => {
+    return (
+        <div className={styles['streams-list-container']}>
+            <div className={styles['scroll-container']}>
+                <div className={styles['streams-list']}>
+                    {props.streams
+                        .map((stream) =>
+                            <Stream key={stream.id}
+                                className={styles['stream']}
+                                logo={stream.logo}
+                                sourceName={stream.sourceName}
+                                title={stream.title}
+                                subtitle={stream.subtitle}
+                                progress={stream.progress}
+                            />
+                        )}
+                </div>
+                <div className={styles['button']} onClick={props.onMoreAddonsClicked}>
+                    <Icon className={styles['button-icon']} icon={'ic_addons'} /> More Add-ons
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 StreamsList.propTypes = {
