f1c96c73ea26980e9f8323c3684b10115a4efe20
svetlagasheva <svetulka_24@mail.bg>
2019-02-12 15:32:19 +0200

    relations between calendar and future episodes added

--- src/routes/Calendar/Calendar.js
+++ src/routes/Calendar/Calendar.js
@@ -11,8 +11,54 @@ class Calendar extends Component {
     constructor(props) {
         super(props);
 
+        this.resetMonth(new Date());
+
+        this.dateRef = React.createRef();
+        this.futureEpisodesRef = React.createRef();
+
+        this.state = {
+            date: new Date(),
+            episodeInfo: '',
+            selectedDate: new Date()
+        };
+
+        // this.episodesNames = this.props.metaItems
+        //     .map((metaitem) => metaitem.metaItem.videos
+        //         .filter((video) => video.released.getMonth() === new Date().getMonth() && video.released.getDate() === this.state.selectedDate.getDate())
+        //         .map((video) => video.name)
+        //         .join())
+        //     .filter((element) => element.length > 0)
+
+        // console.log(this.episodesNames);
+    }
+
+    changeDate = (event) => {
+        const date = new Date(parseInt(event.currentTarget.dataset.date));
+        this.resetMonth(date);
+        this.setState({ date });
+    }
+
+    showEpisodeInfo = (event) => {
+        this.setState({ episodeInfo: event.currentTarget.dataset.videoName });
+    }
+
+    changeSelectedDate = (event) => {
+        this.setState({ selectedDate: new Date(event.currentTarget.dataset.day) });
+    }
+
+    scrollTo = () => {
+        if (this.dateRef.current !== null) {
+            var topPosition = this.dateRef.current.offsetTop;
+            this.futureEpisodesRef.current.scrollTop = topPosition - 16;
+            // this.futureEpisodesRef.current.animate({ scrollTop: this.dateRef.current.offsetTop }, 300);
+        } else {
+            this.setState({ episodeInfo: '' });
+        }
+    }
+
+    resetMonth(newDate) {
         this.monthDays = [];
-        this.monthStart = new Date(new Date().getFullYear(), new Date().getMonth());
+        this.monthStart = new Date(newDate.getFullYear(), newDate.getMonth());
         this.monthEnd = new Date(this.monthStart.getFullYear(), this.monthStart.getMonth() + 1, 0);
         this.pads = [];
         var pad = (this.monthStart.getDay() + 6) % 7;
@@ -24,35 +70,23 @@ class Calendar extends Component {
         for (var i = 0; i != this.monthEnd.getDate(); i++) {
             this.monthDays.push(i);
         }
-
-        // this.episodesDates = this.props.metaItems.map((metaitem) => metaitem.metaItem.videos
-        //     .filter((video) => video.released.getMonth() === new Date().getMonth()))
-        //     .map((videos) => videos.map((video) => video.released.getDate()))
-        //     .join()
-        //     .split(',')
-        //     .filter((date, index, dates) => dates.indexOf(date) === index)
-        //     .sort(function(a, b) {
-        //         return a - b;
-        //     });
-
-        // console.log(this.episodesDates);
-
-        this.state = {
-            date: new Date()
-        };
-    }
-
-    changeDate = (event) => {
-        this.setState({ date: new Date(parseInt(event.currentTarget.dataset.date)) });
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        return nextState.date !== this.state.date;
+        return nextState.date !== this.state.date ||
+            nextState.episodeInfo !== this.state.episodeInfo ||
+            nextState.selectedDate !== this.state.selectedDate;
     }
 
     componentDidUpdate(prevProps, prevState) {
+        if (prevState.selectedDate !== this.state.selectedDate) {
+            this.scrollTo();
+        }
+
         if (prevState.date.getMonth() !== this.state.date.getMonth()) {
-            //reset month-days
+            this.setState({ selectedDate: new Date() });
+            const newDate = this.state.date;
+            this.resetMonth(newDate);
         }
     }
 
@@ -61,8 +95,6 @@ class Calendar extends Component {
             <div className={styles['month']} data-date={date.getTime()} onClick={this.changeDate}>{months[date.getMonth()]}</div>
         );
     }
-
-    renderEpisodeInfo = () => { }
 
     render() {
         return (
@@ -81,25 +113,29 @@ class Calendar extends Component {
                             <div className={styles['pad']} key={pad} />
                         )}
                         {this.monthDays.map((day) =>
-                            <div className={styles['day']} key={day}>
+                            <div className={classnames(styles['day'], { [styles['selected']]: this.state.date.getMonth() === this.state.selectedDate.getMonth() && this.state.selectedDate.getDate() === day + 1 })} key={day} data-day={new Date(new Date().getFullYear(), this.state.date.getMonth(), day + 1)} onClick={this.changeSelectedDate}>
                                 <div className={styles['date-container']}>
-                                    <div className={classnames(styles['date'], { [styles['selected']]: this.state.date.getDate() === day + 1 })}>{day + 1}</div>
+                                    <div className={classnames(styles['date'], { [styles['selected']]: this.state.date.getMonth() === new Date().getMonth() && this.state.date.getDate() === day + 1 })}>{day + 1}</div>
                                 </div>
                                 <div className={styles['episodes']}>
-                                    {this.props.metaItems.map((metaitem) => metaitem.metaItem.videos
-                                        .filter((video) => video.released.getMonth() === this.state.date.getMonth() && video.released.getDate() === day + 1)
-                                        .map((video) =>
-                                            //////
-                                            <div style={{ backgroundImage: `url('${metaitem.metaItem.background}')` }} className={styles['poster']} key={video.id} />
-                                        ))}
+                                    {this.props.metaItems
+                                        .map((metaitem) => metaitem.metaItem.videos
+                                            .filter((video) => video.released.getMonth() === this.state.date.getMonth() && video.released.getDate() === day + 1)
+                                            .map((video) =>
+                                                //////indicator for >3 posters?
+                                                //getTime()?
+                                                <div style={{ backgroundImage: `url('${metaitem.metaItem.background}')` }} className={classnames(styles['poster'], { [styles['past']]: video.released.getDate() < new Date().getDate() && video.released.getMonth() <= new Date().getMonth() })} key={video.id} data-video-name={video.name} onClick={this.showEpisodeInfo} />
+                                            )
+                                        )}
                                 </div>
                             </div>
                         )}
                     </div>
                 </div>
-                <div className={styles['future-episodes']}>
-                    {this.props.metaItems.map((metaitem) => metaitem.metaItem.videos
-                        .filter((video) => video.released.getMonth() === this.state.date.getMonth()))
+                <div ref={this.futureEpisodesRef} className={styles['future-episodes']}>
+                    {this.props.metaItems
+                        .map((metaitem) => metaitem.metaItem.videos
+                            .filter((video) => video.released.getMonth() === this.state.date.getMonth()))
                         .map((videos) => videos.map((video) => video.released.getDate()))
                         .join()
                         .split(',')
@@ -108,36 +144,40 @@ class Calendar extends Component {
                             return a - b;
                         })
                         .map((videoDate) =>
-                            <div className={styles['episode-container']} key={videoDate}>
+                            <div ref={parseInt(videoDate) === this.state.selectedDate.getDate() ? this.dateRef : null} className={classnames(styles['episode-container'], { [styles['selected']]: parseInt(videoDate) === this.state.selectedDate.getDate() })} key={videoDate} data-day={new Date(new Date().getFullYear(), new Date().getMonth(), parseInt(videoDate))} onClick={this.changeSelectedDate}>
                                 <div className={styles['date']}>
                                     {months[this.state.date.getMonth()].slice(0, 3)} {videoDate}
                                 </div>
-                                <div className={styles['episodes']}>
-                                    {this.props.metaItems.map((metaitem) => metaitem.metaItem.videos
+                                {this.props.metaItems
+                                    .map((metaitem) => metaitem.metaItem.videos
                                         .filter((video) => video.released.getMonth() === this.state.date.getMonth() && video.released.getDate() === parseInt(videoDate))
                                         .map((video) =>
-                                            <div className={classnames(styles['episode'], styles['selected'])} key={video.id}>
-                                                <div className={styles['main-info']}>
-                                                    <div className={styles['serie-name']}>
-                                                        {metaitem.metaItem.name}
+                                            <div className={classnames(styles['episode'], { [styles['selected']]: this.state.episodeInfo === video.name }, { [styles['today']]: parseInt(videoDate) === new Date().getDate() })} key={video.name} data-video-name={video.name} onClick={this.showEpisodeInfo}>
+                                                <div className={classnames(styles['info'])} >
+                                                    <div className={styles['main-info']}>
+                                                        <div className={styles['serie-name']}>
+                                                            {metaitem.metaItem.name}
+                                                        </div>
+                                                        <div className={styles['episode-number']}>
+                                                            S{video.season} E{video.episode}
+                                                        </div>
                                                     </div>
-                                                    <div className={styles['episode-number']}>
-                                                        S{video.season} E{video.episode}
+                                                    <div className={styles['name']}>
+                                                        "{video.name}"
                                                     </div>
+                                                    <div className={styles['description']}>
+                                                        {video.description}
+                                                    </div>
+                                                    <a className={styles['watch-button-container']} href={'#/detail'}>
+                                                        <div className={styles['watch-button']}>
+                                                            <Icon className={styles['icon']} icon={'ic_play'} />
+                                                            <div className={styles['label']}>WATCH NOW</div>
+                                                        </div>
+                                                    </a>
                                                 </div>
-                                                {/* <div className={styles['name']}>
-                                                    "{video.name}"
-                                                </div>
-                                                <div className={styles['description']}>
-                                                    {video.description}
-                                                </div>
-                                                <div className={styles['watch-button']}>
-                                                    <Icon className={styles['icon']} icon={'ic_play'} />
-                                                    <div className={styles['label']}>WATCH NOW</div>
-                                                </div> */}
                                             </div>
-                                        ))}
-                                </div>
+                                        )
+                                    )}
                             </div>
                         )}
                 </div>
@@ -147,12 +187,10 @@ class Calendar extends Component {
 }
 
 Calendar.propTypes = {
-    inLibrary: PropTypes.bool.isRequired,
     metaItems: PropTypes.arrayOf(PropTypes.object).isRequired,
     toggleLibraryButton: PropTypes.func
 };
 Calendar.defaultProps = {
-    inLibrary: false,
     metaItems: [
         {
             metaItem: {
@@ -161,8 +199,8 @@ Calendar.defaultProps = {
                 released: new Date(2018, 4, 23),
                 name: 'Vikings',
                 videos: [
-                    { id: '1', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 16, name: 'Homeland', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 1, 4), isUpcoming: true, isWatched: true, season: 5 },
-                    { id: '2', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 17, name: 'The Buddha', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 1, 11), isUpcoming: true, isWatched: true, season: 5 },
+                    { id: '1', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 16, name: 'Homeland', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 1, 6), isUpcoming: true, isWatched: true, season: 5 },
+                    { id: '2', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 17, name: 'The Buddha', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 1, 12), isUpcoming: true, isWatched: true, season: 5 },
                     { id: '3', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 18, name: 'Boneless', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 1, 18), isUpcoming: true, isWatched: true, season: 5 },
                     { id: '4', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 19, name: 'Revenge', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 4, 3), isUpcoming: true, isWatched: true, season: 5 },
                     { id: '5', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 20, name: 'Ragnarok', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 5, 10), isUpcoming: true, isWatched: true, season: 5 }
@@ -176,8 +214,8 @@ Calendar.defaultProps = {
                 released: new Date(2018, 4, 23),
                 name: 'The Walking Dead',
                 videos: [
-                    { id: '1', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 16, name: 'Sing me a song', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 1, 4), isUpcoming: true, isWatched: true, season: 5 },
-                    { id: '2', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 17, name: 'Say yes', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 1, 11), isUpcoming: true, isWatched: true, season: 5 },
+                    { id: '1', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 16, name: 'Sing me a song', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 1, 6), isUpcoming: true, isWatched: true, season: 5 },
+                    { id: '2', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 17, name: 'Say yes', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 1, 12), isUpcoming: true, isWatched: true, season: 5 },
                     { id: '3', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 18, name: 'Bury me here', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 1, 19), isUpcoming: true, isWatched: true, season: 5 },
                     { id: '4', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 19, name: 'Honor', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 4, 3), isUpcoming: true, isWatched: true, season: 5 },
                     { id: '5', poster: 'https://www.stremio.com/websiste/home-stremio.png', episode: 20, name: 'The Bridge', description: 'Bjorn achieves one of Ragnars dreams. Back in Kattegat, Ivar hatches a new plan while preparing for a divine arrival. In Iceland, a settler returns in a terrible state. King Alfred faces his greatest threat yet.', released: new Date(2019, 5, 10), isUpcoming: true, isWatched: true, season: 5 }
