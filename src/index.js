9159479cfada970f2e0a570e38381d24b4218e7e
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-03-01 12:39:02 +0200

    stremio-shell script injected properly

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+window.runApp = () => {
+    ReactDOM.render(<App />, document.getElementById('app'));
+};
--- webpack.config.js
+++ webpack.config.js
@@ -98,7 +98,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebPackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            inject: false
         }),
         new CopyWebpackPlugin([
             { from: 'images', to: 'images' },
