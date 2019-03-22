93343bac1f441c862368ee4a7f101fc9ddfc9536
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-03-23 01:22:27 +0200

    mute icon state fixed

--- src/routes/Player/ControlBar/MuteButton/MuteButton.js
+++ src/routes/Player/ControlBar/MuteButton/MuteButton.js
@@ -16,9 +16,10 @@ class MuteButton extends React.Component {
 
     render() {
         const icon = this.props.muted ? 'ic_volume0' :
-            this.props.volume < 30 ? 'ic_volume1' :
-                this.props.volume < 70 ? 'ic_volume2' :
-                    'ic_volume3';
+            (this.props.volume === null || isNaN(this.props.volume)) ? 'ic_volume3' :
+                this.props.volume < 30 ? 'ic_volume1' :
+                    this.props.volume < 70 ? 'ic_volume2' :
+                        'ic_volume3';
         return (
             <div className={classnames(this.props.className, { 'disabled': this.props.muted === null })} onClick={this.toggleMuted}>
                 <Icon className={'icon'} icon={icon} />
