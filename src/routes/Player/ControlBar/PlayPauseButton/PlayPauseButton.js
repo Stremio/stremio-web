a8975eed4b937129bcad171afafc4e1070328fa8
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-03-23 01:22:38 +0200

    play icon state fixed

--- src/routes/Player/ControlBar/PlayPauseButton/PlayPauseButton.js
+++ src/routes/Player/ControlBar/PlayPauseButton/PlayPauseButton.js
@@ -14,7 +14,7 @@ class PlayPauseButton extends React.Component {
     }
 
     render() {
-        const icon = this.props.paused ? 'ic_play' : 'ic_pause';
+        const icon = this.props.paused === null || this.props.paused ? 'ic_play' : 'ic_pause';
         return (
             <div className={classnames(this.props.className, { 'disabled': this.props.paused === null })} onClick={this.togglePaused}>
                 <Icon className={'icon'} icon={icon} />
