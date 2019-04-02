087bba5e71d8e4f718acf785a8f3f3a3c56a6e72
svetlagasheva <svetulka_24@mail.bg>
2019-04-02 16:35:30 +0300

    user-notifications improved

--- src/common/NavBar/UserNotifications/UserNotifications.js
+++ src/common/NavBar/UserNotifications/UserNotifications.js
@@ -16,17 +16,30 @@ class UserNotifications extends PureComponent {
                 </Popup.Label>
                 <Popup.Menu>
                     <div className={styles['popup-content']}>
-                        {this.props.metaItems.map((metaItem) =>
-                            <div className={styles['meta-item']} key={metaItem.metaItem.videos.map((video) => video.id)}>
-                                <div className={styles['poster']} style={{ backgroundImage: `url(${metaItem.metaItem.poster})` }}></div>
-                                <div className={styles['info-container']}>
-                                    <div className={styles['name']}>{metaItem.metaItem.videos.map((video) => video.name)}</div>
-                                    <div className={styles['released']}>{metaItem.metaItem.videos.map((video) => video.released.getDate() + '.' + video.released.getMonth() + '.' + video.released.getFullYear())}</div>
-                                </div>
-                                <div className={styles['video-poster']} style={{ backgroundImage: `url(${metaItem.metaItem.videos.map((video) => video.poster)})` }}></div>
-                                <Icon className={styles['icon']} icon={'ic_more'} />
+                        <div className={styles['notifications-bar']}>
+                            <div className={styles['label']}>Notifications</div>
+                            <div className={styles['icons-container']}>
+                                <Icon className={styles['drawer-icon']} icon={'ic_drawer'} />
+                                <Icon className={styles['settings-icon']} icon={'ic_settings'} />
                             </div>
-                        )}
+                        </div>
+                        <div className={styles['scroll-container']}>
+                            {this.props.metaItems.map((metaItem) =>
+                                <div className={styles['meta-item']} key={metaItem.metaItem.videos.map((video) => video.id)}>
+                                    <div className={styles['poster']} style={{ backgroundImage: `url(${metaItem.metaItem.poster})` }} />
+                                    <div className={styles['info-container']}>
+                                        <div className={styles['name']}>{metaItem.metaItem.videos.map((video) => video.name)}</div>
+                                        <div className={styles['released']}>{metaItem.metaItem.videos.map((video) => Math.floor(Math.abs((Date.now() - video.released) / (24 * 60 * 60 * 1000))) + ' days ago')}</div>
+                                    </div>
+                                    <div className={styles['video-poster']} style={{ backgroundImage: `url(${metaItem.metaItem.videos.map((video) => video.poster)})` }}>
+                                        <div className={styles['arrow-container']}>
+                                            <Icon className={styles['arrow']} icon={'ic_play'} />
+                                        </div>
+                                    </div>
+                                    <Icon className={styles['icon']} icon={'ic_more'} />
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </Popup.Menu>
             </Popup>
@@ -68,6 +81,66 @@ UserNotifications.defaultProps = {
                 name: 'Fantastic Beasts and Where to Find Them: The Original Screenplay',
                 videos: [
                     { id: '3', poster: 'https://www.stremio.com/website/home-testimonials.jpg', episode: 1, name: 'Evolution', description: 'dasdasda', released: new Date(2019, 1, 12), isUpcoming: true, isWatched: true, season: 1 }
+                ]
+            }
+        },
+        {
+            metaItem: {
+                poster: 'https://www.stremio.com/website/home-stremio.png',
+                released: new Date(2019, 1, 9),
+                name: 'Fantastic Beasts and Where to Find Them: The Original Screenplay',
+                videos: [
+                    { id: '4', poster: 'https://www.stremio.com/website/home-testimonials.jpg', episode: 1, name: 'Katy Perry - Cozy Little Christmas', released: new Date(2019, 1, 9), isUpcoming: true, isWatched: false, season: 1 }
+                ]
+            }
+        },
+        {
+            metaItem: {
+                poster: 'https://www.stremio.com/website/home-stremio.png',
+                released: new Date(2019, 1, 7),
+                name: 'Fantastic Beasts and Where to Find Them: The Original Screenplay',
+                videos: [
+                    { id: '5', poster: 'https://www.stremio.com/website/home-testimonials.jpg', episode: 1, name: 'Hefty Shades of Gray', description: 'dasdasda', released: new Date(2019, 1, 7), isUpcoming: true, isWatched: true, season: 1 }
+                ]
+            }
+        },
+        {
+            metaItem: {
+                poster: 'https://www.stremio.com/website/home-stremio.png',
+                released: new Date(2019, 1, 13),
+                name: 'Fantastic Beasts and Where to Find Them: The Original Screenplay',
+                videos: [
+                    { id: '6', poster: 'https://www.stremio.com/website/home-testimonials.jpg', episode: 1, name: 'Evolution', description: 'dasdasda', released: new Date(2019, 1, 12), isUpcoming: true, isWatched: true, season: 1 }
+                ]
+            }
+        },
+        {
+            metaItem: {
+                poster: 'https://www.stremio.com/website/home-stremio.png',
+                released: new Date(2019, 1, 9),
+                name: 'Fantastic Beasts and Where to Find Them: The Original Screenplay',
+                videos: [
+                    { id: '7', poster: 'https://www.stremio.com/website/home-testimonials.jpg', episode: 1, name: 'Katy Perry - Cozy Little Christmas', released: new Date(2019, 1, 9), isUpcoming: true, isWatched: false, season: 1 }
+                ]
+            }
+        },
+        {
+            metaItem: {
+                poster: 'https://www.stremio.com/website/home-stremio.png',
+                released: new Date(2019, 1, 7),
+                name: 'Fantastic Beasts and Where to Find Them: The Original Screenplay',
+                videos: [
+                    { id: '8', poster: 'https://www.stremio.com/website/home-testimonials.jpg', episode: 1, name: 'Hefty Shades of Gray', description: 'dasdasda', released: new Date(2019, 1, 7), isUpcoming: true, isWatched: true, season: 1 }
+                ]
+            }
+        },
+        {
+            metaItem: {
+                poster: 'https://www.stremio.com/website/home-stremio.png',
+                released: new Date(2019, 1, 13),
+                name: 'Fantastic Beasts and Where to Find Them: The Original Screenplay',
+                videos: [
+                    { id: '9', poster: 'https://www.stremio.com/website/home-testimonials.jpg', episode: 1, name: 'Evolution', description: 'dasdasda', released: new Date(2019, 1, 12), isUpcoming: true, isWatched: true, season: 1 }
                 ]
             }
         }
