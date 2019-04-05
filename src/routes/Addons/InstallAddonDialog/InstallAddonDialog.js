59e86d5317128a45c10ee13cdeb1b00ba6b7322b
svetlagasheva <svetulka_24@mail.bg>
2019-04-05 16:16:59 +0300

    install addon dialog refactored

--- src/routes/Addons/InstallAddonDialog/InstallAddonDialog.js
+++ src/routes/Addons/InstallAddonDialog/InstallAddonDialog.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Icon, { dataUrl as iconDataUrl } from 'stremio-icons/dom';
-import colors from 'stremio-colors';
+import classnames from 'classnames';
+import { Input } from 'stremio-common';
+import Icon from 'stremio-icons/dom';
 import styles from './styles';
 
 const MAX_DESCRIPTION_SYMBOLS = 200;
@@ -22,7 +23,7 @@ const renderVersion = (version) => {
     }
 
     return (
-        <div className={styles['version']}>{'v. ' + version}</div>
+        <div className={styles['version']}>v. {version}</div>
     );
 }
 
@@ -49,16 +50,11 @@ const renderDescription = (description) => {
 }
 
 const InstallAddonDialog = (props) => {
-    const placeholderIconUrl = iconDataUrl({ icon: 'ic_x', fill: colors.surface });
-    const imageStyle = {
-        backgroundImage: `url('${placeholderIconUrl}')`
-    };
-
     return (
         <div className={styles['install-addon-dialog']}>
-            <div className={styles['x-container']}>
-                <div style={imageStyle} className={styles['x-icon']} />
-            </div>
+            <Input className={styles['x-container']} type={'button'}>
+                <Icon className={styles['icon']} icon={'ic_x'} onClick={props.onClose} />
+            </Input>
             <div className={styles['info-container']}>
                 <div className={styles['install-label']}>Install Add-on</div>
                 <div className={styles['basic-info']}>
@@ -78,12 +74,12 @@ const InstallAddonDialog = (props) => {
                     Using third-party add-ons will always be subject to your responsibility and the governing law of the jurisdiction you are located.
                 </div>
                 <div className={styles['buttons']}>
-                    <div className={styles['cancel-button']} >
-                        <span className={styles['label']}>Cancel</span>
-                    </div>
-                    <div className={styles['install-button']} onClick={props.onInstallClicked} >
-                        <span className={styles['label']}>Install</span>
-                    </div>
+                    <Input className={classnames(styles['button'], styles['cancel-button'])} type={'button'} onClick={props.onClose}>
+                        <div className={styles['label']}>Cancel</div>
+                    </Input>
+                    <Input className={classnames(styles['button'], styles['install-button'])} type={'button'} onClick={props.onInstallClicked} >
+                        <div className={styles['label']}>Install</div>
+                    </Input>
                 </div>
             </div>
         </div>
@@ -97,6 +93,7 @@ InstallAddonDialog.propTypes = {
     version: PropTypes.string.isRequired,
     types: PropTypes.arrayOf(PropTypes.string).isRequired,
     description: PropTypes.string.isRequired,
+    onClose: PropTypes.func.isRequired,
     onInstallClicked: PropTypes.func.isRequired
 };
 InstallAddonDialog.defaultProps = {
