67420c70b9697f83a850a646cecb6d283b6f941b
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-04-07 22:35:59 +0300

    fix color converter bug with single digits

--- src/routes/Player/Video/stremio-video/colorConverter.js
+++ src/routes/Player/Video/stremio-video/colorConverter.js
@@ -1,10 +1,14 @@
+function padWithZero(str) {
+    return ('0' + str).slice(-2);
+}
+
 function rgbaToHex(rgbaString) {
     var values = rgbaString.split('(')[1].split(')')[0].split(',');
     var red = parseInt(values[0]).toString(16);
     var green = parseInt(values[1]).toString(16);
     var blue = parseInt(values[2]).toString(16);
-    var alpha = typeof values[3] === 'string' ? Math.round(values[3] * 255).toString(16) : '00';
-    return '#' + red + green + blue + alpha;
+    var alpha = Math.round((values[3] || 0) * 255).toString(16);
+    return '#' + padWithZero(red) + padWithZero(green) + padWithZero(blue) + padWithZero(alpha);
 }
 
 module.exports = {
