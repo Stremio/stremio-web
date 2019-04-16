5f99f0034667ddd49e9ce241b06e9da7641a8618
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-04-16 10:27:37 +0300

    dynamic import plugin dropped

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,7 @@ module.exports = {
                         presets: ['@babel/preset-env', '@babel/preset-react'],
                         plugins: [
                             '@babel/plugin-proposal-class-properties',
-                            '@babel/plugin-proposal-object-rest-spread',
-                            '@babel/plugin-syntax-dynamic-import'
+                            '@babel/plugin-proposal-object-rest-spread'
                         ]
                     }
                 }
