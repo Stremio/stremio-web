e05642df3cf4e659568955f768dec580a1eb3cca
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-04-17 15:33:37 +0300

    withFocusable uses arrow function

--- src/common/Focusable/withFocusable.js
+++ src/common/Focusable/withFocusable.js
@@ -2,13 +2,11 @@ const React = require('react');
 const FocusableContext = require('./FocusableContext');
 
 const withFocusable = (Component) => {
-    return function withFocusable(props) {
-        return (
-            <FocusableContext.Consumer>
-                {focusable => <Component {...props} focusable={focusable} />}
-            </FocusableContext.Consumer>
-        );
-    };
+    return (props) => (
+        <FocusableContext.Consumer>
+            {focusable => <Component {...props} focusable={focusable} />}
+        </FocusableContext.Consumer>
+    );
 };
 
 module.exports = withFocusable;
