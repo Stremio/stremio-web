bd949bca742357c4b31b614264af5838cfa37ee2
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-04-21 21:36:43 +0300

    NavBarButton reacts to hash changes

--- src/common/NavBar/NavBarButton/NavBarButton.js
+++ src/common/NavBar/NavBarButton/NavBarButton.js
@@ -6,7 +6,16 @@ const styles = require('./styles');
 
 const NavBarButton = ({ className, icon, label, href, onClick }) => {
     const type = typeof onClick === 'function' ? 'button' : 'link';
-    const active = window.location.hash === href;
+    const [active, setActive] = React.useState(window.location.hash === href);
+    const onHashChanged = React.useCallback(() => {
+        setActive(window.location.hash === href);
+    }, [href]);
+    React.useEffect(() => {
+        window.addEventListener('hashchange', onHashChanged);
+        return () => {
+            window.removeEventListener('hashchange', onHashChanged);
+        };
+    }, []);
     return (
         <Input className={classnames(className, styles['button'], { 'active': active })} type={type} href={href} onClick={onClick}>
             <Icon className={styles['icon']} icon={icon} />
