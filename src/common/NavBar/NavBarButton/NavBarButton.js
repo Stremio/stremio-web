18fbdb4f1f3c21c75f53a891eddfd65490c041a3
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-04-22 18:06:00 +0300

    NavBarButton use useLocationHash hook

--- src/common/NavBar/NavBarButton/NavBarButton.js
+++ src/common/NavBar/NavBarButton/NavBarButton.js
@@ -1,27 +1,25 @@
 const React = require('react');
 const classnames = require('classnames');
+const UrlUtils = require('url');
 const Icon = require('stremio-icons/dom');
 const { Input } = require('stremio-navigation');
+const useLocationHash = require('../../useLocationHash');
 const styles = require('./styles');
 
-const NavBarButton = ({ className, icon, label, href, onClick }) => {
-    const type = typeof onClick === 'function' ? 'button' : 'link';
-    const [active, setActive] = React.useState(window.location.hash === href);
-    const onHashChanged = React.useCallback(() => {
-        setActive(window.location.hash === href);
-    }, [href]);
-    React.useEffect(() => {
-        window.addEventListener('hashchange', onHashChanged);
-        return () => {
-            window.removeEventListener('hashchange', onHashChanged);
-        };
-    }, []);
+const NavBarButton = React.memo(({ className, icon, label, href, onClick }) => {
+    const locationHash = useLocationHash();
+    const active = React.useMemo(() => {
+        const locationHashPath = locationHash.startsWith('#') ? locationHash.slice(1) : '';
+        const { pathname } = UrlUtils.parse(locationHashPath);
+        return `#${pathname}` === href;
+    }, [href, locationHash]);
+    const inputType = typeof onClick === 'function' ? 'button' : 'link';
     return (
-        <Input className={classnames(className, styles['button'], { 'active': active })} type={type} href={href} onClick={onClick}>
+        <Input className={classnames(className, styles['button'], { 'active': active })} type={inputType} href={href} onClick={onClick}>
             <Icon className={styles['icon']} icon={icon} />
             <div className={styles['label']}>{label}</div>
         </Input>
     );
-};
+});
 
 module.exports = NavBarButton;
