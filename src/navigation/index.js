39199df1acb7574201aa7f1c34d20a1fe2ccd480
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-04-25 19:19:57 +0300

    useFocusable and withFocusable exported from stremio-navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,8 +1,11 @@
+const { useFocusable, withFocusable } = require('./FocusableContext');
 const Input = require('./Input');
 const Modal = require('./Modal');
 const Router = require('./Router');
 
 module.exports = {
+    useFocusable,
+    withFocusable,
     Input,
     Modal,
     Router
