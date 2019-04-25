6d18c057714053f08bd811179ba49a9c5a8f78db
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-04-25 13:29:59 +0300

    NavBar restructured and styled better

--- src/common/NavBar/NavBar.js
+++ src/common/NavBar/NavBar.js
@@ -1,30 +1,30 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const NavBarButton = require('./NavBarButton');
+const SearchBar = require('./SearchBar');
 const UserMenu = require('./UserMenu');
 const styles = require('./styles');
 
-const onBackButtonClick = () => {
-    window.history.back();
-};
-
-const NavBar = ({ backButton, tabs, title, searchInput, userMenu }) => (
-    <nav className={styles['nav-bar']}>
-        {
-            backButton ?
-                <NavBarButton
-                    className={styles['nav-bar-button']}
-                    icon={'ic_back_ios'}
-                    label={'back'}
-                    onClick={onBackButtonClick}
-                />
-                :
-                null
-        }
-        {
-            tabs.length > 0 ?
-                <div className={styles['tabs-container']}>
-                    {tabs.map(({ href, icon, label }) => (
+const NavBar = ({ backButton, tabs, title, searchBar, userMenu }) => {
+    const onBackButtonClick = React.useCallback(() => {
+        window.history.back();
+    }, []);
+    return (
+        <nav className={styles['nav-bar']}>
+            {
+                backButton ?
+                    <NavBarButton
+                        className={styles['nav-bar-button']}
+                        icon={'ic_back_ios'}
+                        label={'back'}
+                        onClick={onBackButtonClick}
+                    />
+                    :
+                    null
+            }
+            {
+                tabs.length > 0 ?
+                    tabs.map(({ href, icon, label }) => (
                         <NavBarButton
                             key={href}
                             className={styles['nav-bar-button']}
@@ -32,25 +32,25 @@ const NavBar = ({ backButton, tabs, title, searchInput, userMenu }) => (
                             icon={icon}
                             label={label}
                         />
-                    ))}
-                </div>
-                :
-                <h2 className={styles['title']}>{title}</h2>
-        }
-        {
-            searchInput ?
-                <div className={styles['search-input']} />
-                :
-                null
-        }
-        {
-            userMenu ?
-                <UserMenu className={styles['user-menu']} />
-                :
-                null
-        }
-    </nav>
-);
+                    ))
+                    :
+                    <h2 className={styles['title']}>{title}</h2>
+            }
+            {
+                searchBar ?
+                    <SearchBar className={styles['search-bar']} />
+                    :
+                    null
+            }
+            {
+                userMenu ?
+                    <UserMenu className={styles['user-menu']} />
+                    :
+                    null
+            }
+        </nav>
+    );
+};
 
 NavBar.propTypes = {
     tabs: PropTypes.arrayOf(PropTypes.exact({
@@ -60,7 +60,7 @@ NavBar.propTypes = {
     })).isRequired,
     title: PropTypes.string.isRequired,
     backButton: PropTypes.bool.isRequired,
-    searchInput: PropTypes.bool.isRequired,
+    searchBar: PropTypes.bool.isRequired,
     userMenu: PropTypes.bool.isRequired
 };
 
@@ -68,7 +68,7 @@ NavBar.defaultProps = {
     tabs: Object.freeze([]),
     title: '',
     backButton: false,
-    searchInput: false,
+    searchBar: false,
     userMenu: false
 };
 
