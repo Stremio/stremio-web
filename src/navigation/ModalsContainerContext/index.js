216ab2cfdc99d3ca4ee10ba54ce9d77db91ba166
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-05-16 15:35:07 +0300

    not used HOCs dropped

--- src/navigation/ModalsContainerContext/index.js
+++ src/navigation/ModalsContainerContext/index.js
@@ -1,9 +1,7 @@
 const ModalsContainerProvider = require('./ModalsContainerProvider');
 const useModalsContainer = require('./useModalsContainer');
-const withModalsContainer = require('./withModalsContainer');
 
 module.exports = {
     ModalsContainerProvider,
-    useModalsContainer,
-    withModalsContainer
+    useModalsContainer
 };
--- src/navigation/RoutesContainerContext/index.js
+++ src/navigation/RoutesContainerContext/index.js
@@ -1,9 +1,7 @@
 const RoutesContainerProvider = require('./RoutesContainerProvider');
 const useRoutesContainer = require('./useRoutesContainer.js');
-const withRoutesContainer = require('./withRoutesContainer');
 
 module.exports = {
     RoutesContainerProvider,
-    useRoutesContainer,
-    withRoutesContainer
+    useRoutesContainer
 };
