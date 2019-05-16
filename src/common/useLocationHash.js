092a09fe36db6acbb3d8e8ed9e1a767a6f8c28ac
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-05-16 18:39:48 +0300

    useLocationHash refactored to use less hooks

--- src/common/useLocationHash.js
+++ src/common/useLocationHash.js
@@ -2,10 +2,10 @@ const React = require('react');
 
 const useLocationHash = () => {
     const [locationHash, setLocationHash] = React.useState(window.location.hash);
-    const onLocationHashChanged = React.useCallback(() => {
-        setLocationHash(window.location.hash);
-    }, []);
     React.useEffect(() => {
+        const onLocationHashChanged = () => {
+            setLocationHash(window.location.hash);
+        };
         window.addEventListener('hashchange', onLocationHashChanged);
         return () => {
             window.removeEventListener('hashchange', onLocationHashChanged);
