759ecdd9705e97a65956b11d0b6c6f083a489b9b
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-05-17 12:45:12 +0300

    Board styles updated

--- src/routes/Board/Board.js
+++ src/routes/Board/Board.js
@@ -34,27 +34,11 @@ class Board extends React.PureComponent {
         });
     }
 
-    renderMetaItemPlaceholders() {
-        const metaItemPlaceholders = [];
-        for (let placeholderNumber = 0; placeholderNumber < 20; placeholderNumber++) {
-            metaItemPlaceholders.push(
-                <div key={placeholderNumber} className={styles['placeholder']}>
-                    <div className={styles['poster-image-placeholder']}>
-                        <div className={styles['poster']} />
-                    </div>
-                    <div className={styles['title-bar-placeholder']} />
-                </div>
-            )
+    render() {
+        if (this.props.groups.length === 0) {
+            return null;
         }
 
-        return (
-            <div className={styles['placeholder-container']}>
-                {metaItemPlaceholders}
-            </div>
-        );
-    }
-
-    render() {
         return (
             <div className={styles['board-container']}>
                 <NavBar
@@ -70,50 +54,43 @@ class Board extends React.PureComponent {
                     userMenu={true}
                 />
                 <div className={styles['board-content']} tabIndex={-1}>
+                    <div className={classnames(styles['board-row'], styles['continue-watching-row'])}>
+                        <div className={styles['meta-items-container']}>
+                            {this.props.groups[0].items.map((item) => (
+                                <MetaItem
+                                    {...item}
+                                    key={item.id}
+                                    className={classnames(styles['meta-item'], styles[`poster-shape-${item.posterShape === 'landscape' ? 'square' : item.posterShape}`])}
+                                    menuClassName={styles['menu-container']}
+                                    posterShape={item.posterShape === 'landscape' ? 'square' : item.posterShape}
+                                    menuOptions={CONTINUE_WATCHING_MENU}
+                                    menuOptionOnSelect={this.menuOptionOnSelect}
+                                    onClick={this.onClick}
+                                />
+                            ))}
+                        </div>
+                        <Input className={styles['show-more-container']} type={'button'} />
+                    </div>
                     {
-                        this.props.groups.length === 0 ?
-                            this.renderMetaItemPlaceholders()
-                            :
-                            <React.Fragment>
-                                <div className={classnames(styles['board-row'], styles['continue-watching-row'])}>
+                        this.props.groups.slice(1, this.props.groups.length).map((group) => {
+                            return (
+                                <div key={group.id} className={classnames(styles['board-row'], styles['addon-catalog-row'])}>
                                     <div className={styles['meta-items-container']}>
-                                        {this.props.groups[0].items.map((item) => (
+                                        {group.items.map((item, _, metaItems) => (
                                             <MetaItem
                                                 {...item}
                                                 key={item.id}
-                                                className={classnames(styles['meta-item'], styles[`poster-shape-${item.posterShape === 'landscape' ? 'square' : item.posterShape}`])}
-                                                menuClassName={styles['menu-container']}
-                                                posterShape={item.posterShape === 'landscape' ? 'square' : item.posterShape}
-                                                menuOptions={CONTINUE_WATCHING_MENU}
-                                                menuOptionOnSelect={this.menuOptionOnSelect}
+                                                className={classnames(styles['meta-item'], styles[`poster-shape-${metaItems[0].posterShape}`])}
+                                                posterShape={metaItems[0].posterShape}
+                                                progress={0}
                                                 onClick={this.onClick}
                                             />
                                         ))}
                                     </div>
                                     <Input className={styles['show-more-container']} type={'button'} />
                                 </div>
-                                {
-                                    this.props.groups.slice(1, this.props.groups.length).map((group) => {
-                                        return (
-                                            <div key={group.id} className={classnames(styles['board-row'], styles['addon-catalog-row'])}>
-                                                <div className={styles['meta-items-container']}>
-                                                    {group.items.map((item, _, metaItems) => (
-                                                        <MetaItem
-                                                            {...item}
-                                                            key={item.id}
-                                                            className={classnames(styles['meta-item'], styles[`poster-shape-${metaItems[0].posterShape}`])}
-                                                            posterShape={metaItems[0].posterShape}
-                                                            progress={0}
-                                                            onClick={this.onClick}
-                                                        />
-                                                    ))}
-                                                </div>
-                                                <Input className={styles['show-more-container']} type={'button'} />
-                                            </div>
-                                        );
-                                    })
-                                }
-                            </React.Fragment>
+                            );
+                        })
                     }
                 </div>
             </div>
