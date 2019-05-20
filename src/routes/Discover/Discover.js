8e6d9514991d24742ce5b54cf94f57fedbe93fcc
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-05-20 21:40:32 +0300

    discover grid implemented

--- src/routes/Discover/Discover.js
+++ src/routes/Discover/Discover.js
@@ -1,26 +1,43 @@
 const React = require('react');
-const { NavBar } = require('stremio-common');
+const { NavBar, MetaItem } = require('stremio-common');
 const styles = require('./styles');
 
-class Discover extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <NavBar
-                    backButton={false}
-                    tabs={[
-                        { label: 'Board', icon: 'ic_board', href: '#/' },
-                        { label: 'Discover', icon: 'ic_discover', href: '#/discover' },
-                        { label: 'Library', icon: 'ic_library', href: '#/library' },
-                        { label: 'Calendar', icon: 'ic_calendar', href: '#/calendar' },
-                    ]}
-                    searchBar={true}
-                    userMenu={true}
-                />
-                <div className={styles['discover-container']} />
-            </React.Fragment>
-        );
-    }
-}
+const Discover = () => {
+
+
+    return (
+        <div className={styles['discover-container']}>
+            <NavBar
+                className={styles['nav-bar']}
+                backButton={false}
+                tabs={[
+                    { label: 'Board', icon: 'ic_board', href: '#/' },
+                    { label: 'Discover', icon: 'ic_discover', href: '#/discover' },
+                    { label: 'Library', icon: 'ic_library', href: '#/library' },
+                    { label: 'Calendar', icon: 'ic_calendar', href: '#/calendar' },
+                ]}
+                searchBar={true}
+                userMenu={true}
+            />
+            <div className={styles['discover-content']}>
+                <div className={styles['meta-items-container']}>
+                    {
+                        Array(145).fill(null).map((_, index) => (
+                            <div key={index} className={styles['meta-item-container']}>
+                                <MetaItem
+                                    className={styles['meta-item']}
+                                    id={`tt${index}`}
+                                    type={'movie'}
+                                    title={index === 4 ? 'title' : ''}
+                                    posterShape={'poster'}
+                                />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
 
 module.exports = Discover;
