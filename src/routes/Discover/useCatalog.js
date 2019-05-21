f7f1cfe0ddd6cf6c8445567df648b261e30794e9
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-05-21 16:15:24 +0300

    Discover demo catalog size changed

--- src/routes/Discover/useCatalog.js
+++ src/routes/Discover/useCatalog.js
@@ -2,7 +2,7 @@ const React = require('react');
 
 const useCatalog = () => {
     return React.useMemo(() => {
-        return Array(2224).fill(null).map((_, index) => ({
+        return Array(303).fill(null).map((_, index) => ({
             id: `tt${index}`,
             type: 'movie',
             name: 'Stremio demo item',
