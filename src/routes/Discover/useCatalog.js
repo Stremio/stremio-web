b607a8ea5d6c8ad4ba0adf29fc23b9aba64cee69
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-05-27 11:29:33 +0300

    demo items uses dummyimage

--- src/routes/Discover/useCatalog.js
+++ src/routes/Discover/useCatalog.js
@@ -6,6 +6,7 @@ const useCatalog = () => {
             id: `tt${index}`,
             type: 'movie',
             name: 'Stremio demo item',
+            poster: `https://dummyimage.com/300x400/000/0011ff.jpg&text=${index + 1}`,
             posterShape: 'poster'
         }));
     }, []);
