5002c08b667df65f9f2f4f13db7e04e2e4cd26f7
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-07-19 17:39:43 +0300

    jsx storybook addon installed

--- storybook/config.js
+++ storybook/config.js
@@ -1,8 +1,16 @@
 const React = require('react');
 const { withConsole } = require('@storybook/addon-console');
-const { addDecorator, configure } = require('@storybook/react');
+const { addDecorator, addParameters, configure } = require('@storybook/react');
+const { jsxDecorator } = require('storybook-addon-jsx');
 const RouterDecorator = require('./RouterDecorator');
 
+addParameters({
+    jsx: {
+        indent_size: 4,
+        showFunctions: false
+    }
+});
+addDecorator((renderStory, context) => jsxDecorator(renderStory, context));
 addDecorator((renderStory, context) => withConsole()(renderStory)(context));
 addDecorator((renderStory) => (
     <RouterDecorator>
