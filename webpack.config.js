5525a37f0735ef6dade050cd0c75b55ce225a6b4
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-07-19 15:40:41 +0300

    app config exclude node_modules instead of include src directory

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                include: [
-                    path.resolve(__dirname, 'src')
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
                 ],
                 use: {
                     loader: 'babel-loader',
