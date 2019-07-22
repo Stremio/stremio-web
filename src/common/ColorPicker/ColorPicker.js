e00b747694b9d018810935696862183a88d17624
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-07-22 15:30:11 +0300

    react-color dropped

--- src/common/ColorPicker/ColorPicker.js
+++ src/common/ColorPicker/ColorPicker.js
@@ -1,40 +1,13 @@
 const React = require('react');
 const PropTypes = require('prop-types');
-const { SketchPicker } = require('react-color');
 
-class ColorPicker extends React.Component {
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.className !== this.props.className ||
-            nextProps.value !== this.props.value;
-    }
-
-    onContainerClicked = (e) => {
-        e.stopPropagation();
-    }
-
-    onColorChanged = (color) => {
-        const value = '#' + `0${color.rgb.r.toString(16)}`.slice(-2) +
-            `0${color.rgb.g.toString(16)}`.slice(-2) +
-            `0${color.rgb.b.toString(16)}`.slice(-2) +
-            `0${Math.round(color.rgb.a * 255).toString(16)}`.slice(-2);
-        if (typeof this.props.onChange === 'function') {
-            this.props.onChange(value);
-        }
-    }
-
-    render() {
-        return (
-            <div className={this.props.className} onClick={this.onContainerClicked}>
-                <SketchPicker
-                    width={''}
-                    color={this.props.value}
-                    presetColors={[]}
-                    onChangeComplete={this.onColorChanged}
-                />
-            </div>
-        );
-    }
-}
+const ColorPicker = ({ className, value, onChange }) => {
+    return (
+        <div style={{ backgroundColor: 'red' }} className={className}>
+            {value}
+        </div>
+    );
+};
 
 ColorPicker.propTypes = {
     className: PropTypes.string,
