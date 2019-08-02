e8f9e1c20295c7695dc9ce94649c197ac2de8d65
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-08-02 16:15:13 +0300

    keyboard navigation bindings removed from MainNavBar

--- src/common/MainNavBar/MainNavBar.js
+++ src/common/MainNavBar/MainNavBar.js
@@ -1,32 +1,15 @@
 const React = require('react');
 const PropTypes = require('prop-types');
-const { useFocusable } = require('stremio-navigation');
 const NavBar = require('../NavBar');
 
 const TABS = [
-    { label: 'Board', icon: 'ic_board', href: '#/', keyCode: 112 },
-    { label: 'Discover', icon: 'ic_discover', href: '#/discover', keyCode: 113 },
-    { label: 'Library', icon: 'ic_library', href: '#/library', keyCode: 114 },
-    { label: 'Calendar', icon: 'ic_calendar', href: '#/calendar', keyCode: 115 }
+    { label: 'Board', icon: 'ic_board', href: '#/' },
+    { label: 'Discover', icon: 'ic_discover', href: '#/discover' },
+    { label: 'Library', icon: 'ic_library', href: '#/library' },
+    { label: 'Calendar', icon: 'ic_calendar', href: '#/calendar' }
 ];
 
 const MainNavBar = ({ className }) => {
-    const focusable = useFocusable();
-    React.useEffect(() => {
-        const onKeyDown = (event) => {
-            const tab = TABS.find(({ keyCode }) => event.keyCode === keyCode);
-            if (tab) {
-                event.preventDefault();
-                window.location = tab.href;
-            }
-        };
-        if (focusable) {
-            window.addEventListener('keydown', onKeyDown);
-        }
-        return () => {
-            window.removeEventListener('keydown', onKeyDown);
-        };
-    }, [focusable]);
     return (
         <NavBar
             className={className}
