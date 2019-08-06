e11c6ea2e11bb27e06bd5d162278ea4333641870
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-08-06 10:25:34 +0300

    Checkbox props simplified

--- src/common/Checkbox/Checkbox.js
+++ src/common/Checkbox/Checkbox.js
@@ -5,11 +5,11 @@ const Icon = require('stremio-icons/dom');
 const Button = require('../Button');
 const styles = require('./styles');
 
-const Checkbox = React.forwardRef(({ children, ...props }, ref) => {
+const Checkbox = React.forwardRef((props, ref) => {
     return (
         <Button {...props} ref={ref} className={classnames(props.className, styles['checkbox-container'], { 'checked': props.checked })}>
             <Icon className={styles['icon']} icon={props.checked ? 'ic_check' : 'ic_box_empty'} />
-            {React.isValidElement(children) ? children : null}
+            {props.children}
         </Button>
     );
 });
