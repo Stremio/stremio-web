494f63b679fd729eb52676272b6d7f8f0ddeee8e
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-08-09 14:16:58 +0300

    className added to Modal props

--- src/router/Modal/Modal.js
+++ src/router/Modal/Modal.js
@@ -1,10 +1,11 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const PropTypes = require('prop-types');
+const classnames = require('classnames');
 const { FocusableProvider } = require('../FocusableContext');
 const { useModalsContainer } = require('../ModalsContainerContext');
 
-const Modal = ({ children }) => {
+const Modal = ({ className, children }) => {
     const modalsContainer = useModalsContainer();
     const onRoutesContainerDomTreeChange = React.useCallback(({ routesContainer, contentContainer }) => {
         return routesContainer.lastElementChild === contentContainer.parentElement.parentElement;
@@ -14,13 +15,14 @@ const Modal = ({ children }) => {
     }, []);
     return ReactDOM.createPortal(
         <FocusableProvider onRoutesContainerDomTreeChange={onRoutesContainerDomTreeChange} onModalsContainerDomTreeChange={onModalsContainerDomTreeChange}>
-            <div className={'modal-container'}>{children}</div>
+            <div className={classnames(className, 'modal-container')}>{children}</div>
         </FocusableProvider>,
         modalsContainer
     );
 };
 
 Modal.propTypes = {
+    className: PropTypes.string,
     children: PropTypes.oneOfType([
         PropTypes.arrayOf(PropTypes.node),
         PropTypes.node
