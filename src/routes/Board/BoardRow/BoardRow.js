f59f6377526aded23a3ce9d5086b07b16cecd4f8
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-08-19 16:25:02 +0300

    message added to BoardRow props

--- src/routes/Board/BoardRow/BoardRow.js
+++ src/routes/Board/BoardRow/BoardRow.js
@@ -5,25 +5,43 @@ const Icon = require('stremio-icons/dom');
 const { Button, MetaItem } = require('stremio/common');
 require('./styles');
 
-const BoardRow = ({ className, title, items = [], itemMenuOptions = [] }) => {
+const BoardRow = ({ className, title = '', message = '', items = [], itemMenuOptions = [] }) => {
     return (
         <div className={classnames(className, 'board-row-container')}>
-            <div className={'title-container'}>{title}</div>
-            <div className={'meta-items-container'}>
-                {items.map((item) => (
-                    <MetaItem
-                        {...item}
-                        key={item.id}
-                        className={classnames('meta-item', `poster-shape-${item.posterShape}`)}
-                        title={item.name}
-                        menuOptions={itemMenuOptions}
-                    />
-                ))}
-            </div>
-            <Button className={'see-all-container'} title={'SEE ALL'}>
-                <div className={'label'}>SEE ALL</div>
-                <Icon className={'icon'} icon={'ic_arrow_thin_right'} />
-            </Button>
+            {
+                typeof title === 'string' && title.length > 0 ?
+                    <div className={'title-container'}>{title}</div>
+                    :
+                    null
+            }
+            {
+                typeof message === 'string' && message.length > 0 ?
+                    <div className={'message-container'}>{message}</div>
+                    :
+                    null
+            }
+            {
+                Array.isArray(items) && items.length > 0 ?
+                    <React.Fragment>
+                        <div className={'meta-items-container'}>
+                            {items.map((item) => (
+                                <MetaItem
+                                    {...item}
+                                    key={item.id}
+                                    className={classnames('meta-item', `poster-shape-${item.posterShape}`)}
+                                    title={item.name}
+                                    menuOptions={itemMenuOptions}
+                                />
+                            ))}
+                        </div>
+                        <Button className={'see-all-container'} title={'SEE ALL'}>
+                            <div className={'label'}>SEE ALL</div>
+                            <Icon className={'icon'} icon={'ic_arrow_thin_right'} />
+                        </Button>
+                    </React.Fragment>
+                    :
+                    null
+            }
         </div>
     );
 };
@@ -31,6 +49,7 @@ const BoardRow = ({ className, title, items = [], itemMenuOptions = [] }) => {
 BoardRow.propTypes = {
     className: PropTypes.string,
     title: PropTypes.string,
+    message: PropTypes.string,
     items: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
