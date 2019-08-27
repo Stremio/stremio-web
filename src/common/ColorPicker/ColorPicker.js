5845dd12ce9ec7c6cdc343849aecfa97192255f8
svetlagasheva <svetulka_24@mail.bg>
2019-08-27 17:48:06 +0300

    color picker implemented

--- src/common/ColorPicker/ColorPicker.js
+++ src/common/ColorPicker/ColorPicker.js
@@ -1,18 +1,43 @@
 const React = require('react');
 const PropTypes = require('prop-types');
+const AColorPicker = require('a-color-picker');
 
-// TODO: impl this
+const COLOR_FORMAT = 'rgbacss';
+
 const ColorPicker = ({ className, value, onChange }) => {
+    value = AColorPicker.parseColor(value, COLOR_FORMAT);
+    const pickerRef = React.useRef(null);
+    const pickerElementRef = React.useRef(null);
+    React.useEffect(() => {
+        pickerRef.current = AColorPicker.createPicker(pickerElementRef.current, {
+            color: value,
+            showHSL: false,
+            showHEX: false,
+            showRGB: false,
+            showAlpha: true
+        });
+    }, []);
+    React.useEffect(() => {
+        pickerRef.current.off('change');
+        pickerRef.current.on('change', (picker, color) => {
+            if (typeof onChange === 'function') {
+                onChange(AColorPicker.parseColor(color, COLOR_FORMAT));
+            }
+        });
+    }, [onChange]);
+    React.useEffect(() => {
+        if (AColorPicker.parseColor(pickerRef.current.color, COLOR_FORMAT) !== value) {
+            pickerRef.current.color = value;
+        }
+    }, [value]);
     return (
-        <div style={{ backgroundColor: 'red' }} className={className}>
-            {value}
-        </div>
+        <div ref={pickerElementRef} className={className} />
     );
 };
 
 ColorPicker.propTypes = {
     className: PropTypes.string,
-    value: PropTypes.string.isRequired,
+    value: PropTypes.string,
     onChange: PropTypes.func
 };
 
