bd99f377a0169b96aaef5434b5ef3a51d2aad398
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-08-29 16:04:50 +0300

    useSeasons custom hook implemented

---
+++ src/routes/Detail/VideosList/useSeasons.js
@@ -0,0 +1,35 @@
+const React = require('react');
+
+const useSeasons = (metaItem) => {
+    const seasons = React.useMemo(() => {
+        return metaItem !== null ?
+            metaItem.videos
+                .map(({ season }) => season)
+                .filter((season, index, seasons) => {
+                    return typeof season === 'string' &&
+                        season.length > 0 &&
+                        seasons.indexOf(season) === index;
+                })
+            :
+            [];
+    }, [metaItem]);
+    const [season, setSeason] = React.useState(seasons[0]);
+    const setPrevSeason = React.useCallback(() => {
+        const seasonIndex = seasons.indexOf(season);
+        if (seasonIndex > 0) {
+            setSeason(seasons[seasonIndex - 1]);
+        }
+    }, [seasons, season]);
+    const setNextSeason = React.useCallback(() => {
+        const seasonIndex = seasons.indexOf(season);
+        if (seasonIndex < seasons.length - 1) {
+            setSeason(seasons[seasonIndex + 1]);
+        }
+    }, [seasons, season]);
+    React.useEffect(() => {
+        setSeason(seasons[0]);
+    }, [seasons]);
+    return [season, seasons, setSeason, setPrevSeason, setNextSeason];
+};
+
+module.exports = useSeasons;
