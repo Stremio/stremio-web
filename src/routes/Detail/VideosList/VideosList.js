4bf90a8936ec7c13d4dd6021af829f851dc8cf69
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-09-02 22:49:19 +0300

    VideoPlaceholder rendered in videos list while loading

--- src/routes/Detail/VideosList/VideosList.js
+++ src/routes/Detail/VideosList/VideosList.js
@@ -4,6 +4,7 @@ const classnames = require('classnames');
 const Icon = require('stremio-icons/dom');
 const { Button, Popup, useBinaryState } = require('stremio/common');
 const Video = require('./Video');
+const VideoPlaceholder = require('./VideoPlaceholder');
 const useSeasons = require('./useSeasons');
 require('./styles');
 
@@ -20,56 +21,67 @@ const VideosList = ({ className, metaItem }) => {
     return (
         <div className={classnames(className, 'videos-list-container')}>
             {
-                seasons.length > 1 ?
-                    <div className={'seasons-bar'}>
-                        <Button className={'prev-season-button'} onClick={setPrevSeason}>
-                            <Icon className={'icon'} icon={'ic_arrow_left'} />
-                        </Button>
-                        <Popup
-                            open={menuOpen}
-                            menuMatchLabelWidth={true}
-                            onCloseRequest={closeMenu}
-                            renderLabel={(ref) => (
-                                <Button ref={ref} className={classnames('seasons-popup-label-container', { 'active': menuOpen })} onClick={toggleMenu}>
-                                    <div className={'season-label'}>Season</div>
-                                    <div className={'number-label'}>{season}</div>
-                                </Button>
-                            )}
-                            renderMenu={() => (
-                                <div className={'seasons-menu-container'}>
-                                    {seasons.map((season) => (
-                                        <Button key={season} className={'season-option-container'} data-season={season} onClick={seasonOnClick}>
-                                            <div className={'season-label'}>Season</div>
-                                            <div className={'number-label'}>{season}</div>
-                                        </Button>
-                                    ))}
-                                </div>
-                            )}
-                        />
-                        <Button className={'next-season-button'} onClick={setNextSeason}>
-                            <Icon className={'icon'} icon={'ic_arrow_right'} />
-                        </Button>
-                    </div>
-                    :
-                    null
-            }
-            {
                 metaItem !== null ?
-                    <div className={'videos-container'}>
+                    <React.Fragment>
                         {
-                            metaItem.videos
-                                .filter(video => isNaN(season) || video.season === season)
-                                .map(video => (
-                                    <Video
-                                        {...video}
-                                        key={video.id}
-                                        className={'video'}
+                            seasons.length > 1 ?
+                                <div className={'seasons-bar'}>
+                                    <Button className={'prev-season-button'} onClick={setPrevSeason}>
+                                        <Icon className={'icon'} icon={'ic_arrow_left'} />
+                                    </Button>
+                                    <Popup
+                                        open={menuOpen}
+                                        menuMatchLabelWidth={true}
+                                        onCloseRequest={closeMenu}
+                                        renderLabel={(ref) => (
+                                            <Button ref={ref} className={classnames('seasons-popup-label-container', { 'active': menuOpen })} onClick={toggleMenu}>
+                                                <div className={'season-label'}>Season</div>
+                                                <div className={'number-label'}>{season}</div>
+                                            </Button>
+                                        )}
+                                        renderMenu={() => (
+                                            <div className={'seasons-menu-container'}>
+                                                {seasons.map((season) => (
+                                                    <Button key={season} className={'season-option-container'} data-season={season} onClick={seasonOnClick}>
+                                                        <div className={'season-label'}>Season</div>
+                                                        <div className={'number-label'}>{season}</div>
+                                                    </Button>
+                                                ))}
+                                            </div>
+                                        )}
                                     />
-                                ))
+                                    <Button className={'next-season-button'} onClick={setNextSeason}>
+                                        <Icon className={'icon'} icon={'ic_arrow_right'} />
+                                    </Button>
+                                </div>
+                                :
+                                null
                         }
-                    </div>
+                        <div className={'videos-container'}>
+                            {
+                                metaItem.videos
+                                    .filter((video) => isNaN(season) || video.season === season)
+                                    .map((video) => (
+                                        <Video
+                                            {...video}
+                                            key={video.id}
+                                            className={'video'}
+                                        />
+                                    ))
+                            }
+                        </div>
+                    </React.Fragment>
                     :
-                    null
+                    <React.Fragment>
+                        <div className={'videos-container'}>
+                            <VideoPlaceholder className={'video'} />
+                            <VideoPlaceholder className={'video'} />
+                            <VideoPlaceholder className={'video'} />
+                            <VideoPlaceholder className={'video'} />
+                            <VideoPlaceholder className={'video'} />
+                            <VideoPlaceholder className={'video'} />
+                        </div>
+                    </React.Fragment>
             }
         </div>
     );
