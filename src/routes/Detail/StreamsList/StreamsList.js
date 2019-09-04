bf9baea81a0fd1a4e1bbc3bb114617e3d22ebf48
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-09-04 23:46:52 +0300

    streams placeholders fixed

--- src/routes/Detail/StreamsList/StreamsList.js
+++ src/routes/Detail/StreamsList/StreamsList.js
@@ -2,7 +2,7 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const classnames = require('classnames');
 const Icon = require('stremio-icons/dom');
-const { Button } = require('stremio/common');
+const { Button, placeholderStyles } = require('stremio/common');
 const Stream = require('./Stream');
 const StreamPlaceholder = require('./StreamPlaceholder');
 const useStreams = require('./useStreams');
@@ -12,22 +12,27 @@ const StreamsList = ({ className, metaItem }) => {
     const streams = useStreams(metaItem);
     return (
         <div className={classnames(className, 'streams-list-container')}>
-            {
-                streams.length > 0 ?
-                    streams.map((stream) => (
-                        <Stream
-                            {...stream}
-                            key={stream.id}
-                            className={'stream'}
-                        />
-                    ))
-                    :
-                    <React.Fragment>
-                        <StreamPlaceholder className={'stream'} />
-                        <StreamPlaceholder className={'stream'} />
-                        <StreamPlaceholder className={'stream'} />
-                    </React.Fragment>
-            }
+            <div className={classnames('streams-container', { [placeholderStyles['placeholder-container']]: streams.length === 0 })}>
+                {
+                    streams.length > 0 ?
+                        streams.map((stream) => (
+                            <Stream
+                                {...stream}
+                                key={stream.id}
+                                className={'stream'}
+                            />
+                        ))
+                        :
+                        <React.Fragment>
+                            <StreamPlaceholder className={'stream'} />
+                            <StreamPlaceholder className={'stream'} />
+                            <StreamPlaceholder className={'stream'} />
+                            <StreamPlaceholder className={'stream'} />
+                            <StreamPlaceholder className={'stream'} />
+                            <StreamPlaceholder className={'stream'} />
+                        </React.Fragment>
+                }
+            </div>
             <Button className={'install-addons-container'} title={'Install addons'} href={'#/addons'}>
                 <Icon className={'icon'} icon={'ic_addons'} />
                 <div className={'label'}>Install addons</div>
