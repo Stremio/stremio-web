2ce0bc8c5672db011408fd78c2eb4ec5f8fdce96
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-09-04 23:33:47 +0300

    StreamPlaceholder adapted to the latest changes in placeholder styles

--- src/routes/Detail/StreamsList/StreamPlaceholder/StreamPlaceholder.js
+++ src/routes/Detail/StreamsList/StreamPlaceholder/StreamPlaceholder.js
@@ -1,15 +1,22 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const classnames = require('classnames');
-const { animatedPlaceholderStyles } = require('stremio/common');
+const Icon = require('stremio-icons/dom');
 require('./styles');
 
 const StreamPlaceholder = ({ className }) => {
     return (
-        <div className={classnames(className, 'stream-placeholder-container', animatedPlaceholderStyles['animated-placeholder'])}>
-            <div className={'addon-container'} />
-            <div className={'info-container'} />
-            <div className={'play-icon-container'} />
+        <div className={classnames(className, 'stream-placeholder-container')}>
+            <div className={'addon-container'}>
+                <div className={'addon-name'} />
+            </div>
+            <div className={'info-container'}>
+                <div className={'description-container'} />
+                <div className={'description-container'} />
+            </div>
+            <div className={'play-icon-container'}>
+                <Icon className={'play-icon'} icon={'ic_play'} />
+            </div>
         </div>
     );
 };
