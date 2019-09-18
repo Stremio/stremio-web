837078921140ba792b06f4df32520e3f96d70800
Vladimir Borisov <borisov1024@gmail.com>
2019-09-18 09:34:08 +0300

    Moved the styles in the styles.less file. Unwrapped the setColor function

--- storybook/stories/ColorPicker/ColorPicker.js
+++ storybook/stories/ColorPicker/ColorPicker.js
@@ -6,13 +6,12 @@ const styles = require('./styles');
 const ColorPickerExampleusage = () => {
     const [color, setColor] = React.useState('rgba(166, 196, 213, 0.97)');
     return (
-        <div style={{ color: 'var(--color-surfacelighter)', padding: '1em' }}>
+        <div className={styles['demo-container']}>
             <ColorPicker
-                className={styles['color-picker-container']}
                 value={color}
-                onChange={(color) => { setColor(color) }}
+                onChange={setColor}
             />
-            <div style={{ marginTop: '1em' }}>Current color is {color}</div>
+            <div className={styles['test-output']}>Current color is {color}</div>
         </div>
     );
 }
