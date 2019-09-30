3dac8f96f3732b5dbfc7467891e03cc59b4e0d4e
Vladimir Borisov <borisov1024@gmail.com>
2019-09-30 16:37:38 +0300

    useSettings hook

--- src/routes/Settings/Settings.js
+++ src/routes/Settings/Settings.js
@@ -4,29 +4,9 @@ const styles = require('./styles');
 const SectionsSelector = require('./SectionsSelector');
 const SectionsList = require('./SectionsList');
 const { settingsSections } = require('./constants');
+const settingsValues = require('./useSettings');
 
 const devTestWithUser = true;
-
-const settingsValues = {
-    "user": devTestWithUser ? {
-        "_id": "neo",
-        "email": "neo@example.com",
-        "avatar": "https://www.thenational.ae/image/policy:1.891803:1566372420/AC17-Matrix-20-04.jpg?f=16x9&w=1200&$p$f$w=5867e40",
-    } : null,
-    "ui_language": "eng",
-    "default_subtitles_language": "bul",
-    "default_subtitles_size": "100%",
-    "subtitles_background": "",
-    "subtitles_color": "#ffffff",
-    "subtitles_outline_color": "#000",
-    "auto-play_next_episode": true,
-    "pause_playback_when_minimized": false,
-    "hardware-accelerated_decoding": true,
-    "launch_player_in_a_separate_window_(advanced)": true,
-    "caching": "2048",
-    "torrent_profile": "profile-default",
-    "streaming_server_is_available.": true,
-};
 
 const Settings = () => {
     const sections = Object.keys(settingsSections)
@@ -36,15 +16,13 @@ const Settings = () => {
             ref: React.useRef(null)
         }));
     const [selectedSectionId, setSelectedSectionId] = React.useState(sections[0].id);
-    const [preferences, setPreferences] = React.useState(settingsValues);
+    const [preferences, setPreferences] = settingsValues(devTestWithUser);
     const scrollContainerRef = React.useRef(null);
 
     /////////////////
 
     const updatePreference = (option, value) => {
-        const newPrefs = { ...preferences };
-        newPrefs[option] = value;
-        setPreferences(newPrefs);
+        setPreferences({ ...preferences, [option]: value });
     }
 
     const changeSection = React.useCallback((event) => {
