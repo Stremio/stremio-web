47ecf701cc8404823f2e36ace42eaba6af116787
svetlagasheva <svetulka_24@mail.bg>
2019-09-30 10:53:53 +0300

    SharePrompt reimplemented with hooks

--- src/common/SharePrompt/SharePrompt.js
+++ src/common/SharePrompt/SharePrompt.js
@@ -2,38 +2,48 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const classnames = require('classnames');
 const Icon = require('stremio-icons/dom');
+const { useFocusable } = require('stremio-router');
 const Button = require('stremio/common/Button');
 const TextInput = require('stremio/common/TextInput');
 const styles = require('./styles');
 
-const SharePrompt = (props) => {
+const SharePrompt = ({ className, label, url, close }) => {
     const inputRef = React.useRef(null);
-    const copyToClipboard = () => {
+    const focusable = useFocusable();
+    const copyToClipboard = React.useCallback(() => {
         inputRef.current.select();
         document.execCommand('copy');
-    };
-
-    if (props.url.length === 0) {
-        return null;
-    }
-
+    }, []);
+    React.useEffect(() => {
+        const onKeyUp = (event) => {
+            if (event.key === 'Escape') {
+                cancel();
+            }
+        };
+        if (focusable) {
+            window.addEventListener('keyup', onKeyUp);
+        }
+        return () => {
+            window.removeEventListener('keyup', onKeyUp);
+        };
+    }, [close, focusable]);
     return (
-        <div className={classnames(props.className, styles['share-prompt-container'])}>
+        <div className={classnames(className, styles['share-prompt-container'])}>
             <Button className={styles['close-button-container']}>
-                <Icon className={styles['icon']} icon={'ic_x'} onClick={props.onClose} />
+                <Icon className={styles['icon']} icon={'ic_x'} onClick={close} />
             </Button>
             <div className={styles['share-prompt-content']}>
-                <div className={styles['share-prompt-label']}>{props.label}</div>
+                <div className={styles['share-prompt-label']}>{label}</div>
                 <div className={styles['buttons-container']}>
-                    <Button className={classnames(styles['button-container'], styles['facebook-button'])} href={`https://www.facebook.com/sharer/sharer.php?u=${props.url}`} target={'_blank'}>
+                    <Button className={classnames(styles['button-container'], styles['facebook-button'])} href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} target={'_blank'}>
                         <Icon className={styles['icon']} icon={'ic_facebook'} />FACEBOOK
                     </Button>
-                    <Button className={classnames(styles['button-container'], styles['twitter-button'])} href={`https://www.facebook.com/sharer/sharer.php?u=${props.url}`} target={'_blank'}>
+                    <Button className={classnames(styles['button-container'], styles['twitter-button'])} href={`https://www.facebook.com/sharer/sharer.php?u=${url}`} target={'_blank'}>
                         <Icon className={styles['icon']} icon={'ic_twitter'} />TWITTER
                     </Button>
                 </div>
                 <div className={styles['url-container']}>
-                    <TextInput ref={inputRef} className={styles['url-content']} type={'text'} tabIndex={'-1'} defaultValue={props.url} readOnly />
+                    <TextInput ref={inputRef} className={styles['url-content']} type={'text'} tabIndex={'-1'} defaultValue={url} readOnly />
                     <Button className={styles['copy-button']} onClick={copyToClipboard}>
                         <Icon className={styles['icon']} icon={'ic_link'} />
                         <div className={styles['label']}>Copy</div>
@@ -48,7 +58,7 @@ SharePrompt.propTypes = {
     className: PropTypes.string,
     label: PropTypes.string.isRequired,
     url: PropTypes.string.isRequired,
-    onClose: PropTypes.func
+    close: PropTypes.func
 };
 SharePrompt.defaultProps = {
     label: 'Share',
