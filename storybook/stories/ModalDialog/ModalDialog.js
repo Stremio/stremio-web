2487052b6f978446e5ddfd45281845ecd523bcb8
Vladimir Borisov <borisov1024@gmail.com>
2019-10-02 16:48:14 +0300

    Do not use other widgets

--- storybook/stories/ModalDialog/ModalDialog.js
+++ storybook/stories/ModalDialog/ModalDialog.js
@@ -4,7 +4,6 @@ const { storiesOf } = require('@storybook/react');
 const { action } = require('@storybook/addon-actions');
 const { ModalDialog } = require('stremio/common');
 const Icon = require('stremio-icons/dom');
-const ColorPicker = require('stremio/common/ColorPicker');
 const styles = require('./styles');
 
 storiesOf('ModalDialog', module).add('ModalDialog', () => {
@@ -44,11 +43,12 @@ storiesOf('ModalDialog', module).add('ModalDialog', () => {
         <ModalDialog className={styles['modal-dialog']} title={'Test dialog'} buttons={buttons} onClose={action('ModalDialog close button clicked')}>
             <div className={styles['content-container']}>
                 <div className={styles['content-column']}>
-                    <ColorPicker className={styles['content-centered']} onChange={action('Color changed')} />
+                    <h2>What is Lorem Ipsum?</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
                 <div className={styles['content-column']}>
-                    <h2>Some text here</h2>
-                    <p>Lorem ipsum dolor sit amet</p>
+                    <h2>Where does it come from?</h2>
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
                 </div>
             </div>
         </ModalDialog>
