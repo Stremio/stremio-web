96dc4fafc8f12786507ec77ae3c909837a015693
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-10-03 14:12:40 +0300

    PlayPause button implemented with hooks

--- src/routes/Player/ControlBar/PlayPauseButton/PlayPauseButton.js
+++ src/routes/Player/ControlBar/PlayPauseButton/PlayPauseButton.js
@@ -2,31 +2,28 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const classnames = require('classnames');
 const Icon = require('stremio-icons/dom');
+const { Button } = require('stremio/common');
 
-class PlayPauseButton extends React.Component {
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.className !== this.props.className ||
-            nextProps.paused !== this.props.paused;
-    }
-
-    togglePaused = () => {
-        this.props.dispatch({ propName: 'paused', propValue: !this.props.paused });
-    }
-
-    render() {
-        const icon = this.props.paused === null || this.props.paused ? 'ic_play' : 'ic_pause';
-        return (
-            <div className={classnames(this.props.className, { 'disabled': this.props.paused === null })} onClick={this.togglePaused}>
-                <Icon className={'icon'} icon={icon} />
-            </div>
-        );
-    }
-}
+const PlayPauseButton = ({ className, paused, dispatch }) => {
+    const togglePaused = React.useCallback(() => {
+        if (typeof dispatch === 'function') {
+            dispatch({ propName: 'paused', propValue: !paused });
+        }
+    }, [paused, dispatch]);
+    return (
+        <Button className={classnames(className, { 'disabled': paused === null })} onClick={togglePaused}>
+            <Icon
+                className={'icon'}
+                icon={typeof paused !== 'boolean' || paused ? 'ic_play' : 'ic_pause'}
+            />
+        </Button>
+    );
+};
 
 PlayPauseButton.propTypes = {
     className: PropTypes.string,
     paused: PropTypes.bool,
-    dispatch: PropTypes.func.isRequired
+    dispatch: PropTypes.func
 };
 
 module.exports = PlayPauseButton;
