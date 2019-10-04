083ace9e0d68483322d428b48e78b188e7f0b10f
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-10-04 21:23:02 +0300

    storybook adapted to the changes in App

--- storybook/RouterDecorator/RouterDecorator.js
+++ storybook/RouterDecorator/RouterDecorator.js
@@ -1,18 +1,21 @@
 const React = require('react');
 const { RoutesContainerProvider } = require('stremio/router/RoutesContainerContext');
 const Route = require('stremio/router/Route');
+const { ScrollEventEmitter } = require('stremio/common');
 const appStyles = require('stremio/App/styles');
 const styles = require('./styles');
 
 const RouterDecorator = ({ children }) => (
     <div id={'app'}>
-        <RoutesContainerProvider className={appStyles['router']}>
-            <Route>
-                <div className={styles['route-content-container']}>
-                    {children}
-                </div>
-            </Route>
-        </RoutesContainerProvider>
+        <ScrollEventEmitter className={appStyles['app-content']}>
+            <RoutesContainerProvider className={appStyles['router']}>
+                <Route>
+                    <div className={styles['route-content-container']}>
+                        {children}
+                    </div>
+                </Route>
+            </RoutesContainerProvider>
+        </ScrollEventEmitter>
     </div>
 );
 
