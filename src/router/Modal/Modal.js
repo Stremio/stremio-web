e426afb07ac793e12083d699c9986b290b9ca7d5
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-10-05 22:05:41 +0300

    disable focus lock in modal implemented

--- src/router/Modal/Modal.js
+++ src/router/Modal/Modal.js
@@ -4,10 +4,10 @@ const classnames = require('classnames');
 const FocusLock = require('react-focus-lock').default;
 const { useModalsContainer } = require('../ModalsContainerContext');
 
-const Modal = ({ className, children, ...props }) => {
+const Modal = ({ className, disabled, children, ...props }) => {
     const modalsContainer = useModalsContainer();
     return ReactDOM.createPortal(
-        <FocusLock className={classnames(className, 'modal-container')} autoFocus={false} lockProps={props}>
+        <FocusLock className={classnames(className, 'modal-container')} autoFocus={false} disabled={disabled} lockProps={props}>
             {children}
         </FocusLock>,
         modalsContainer
