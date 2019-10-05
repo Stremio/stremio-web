1e583c9a94787c74f9a2094f6f5bc7b25592d203
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-10-05 22:08:18 +0300

    propTypes added to Modal

--- src/router/Modal/Modal.js
+++ src/router/Modal/Modal.js
@@ -4,14 +4,24 @@ const classnames = require('classnames');
 const FocusLock = require('react-focus-lock').default;
 const { useModalsContainer } = require('../ModalsContainerContext');
 
-const Modal = ({ className, disabled, children, ...props }) => {
+const Modal = ({ className, autoFocus, disabled, children, ...props }) => {
     const modalsContainer = useModalsContainer();
     return ReactDOM.createPortal(
-        <FocusLock className={classnames(className, 'modal-container')} autoFocus={false} disabled={disabled} lockProps={props}>
+        <FocusLock className={classnames(className, 'modal-container')} autoFocus={autoFocus} disabled={disabled} lockProps={props}>
             {children}
         </FocusLock>,
         modalsContainer
     );
+};
+
+Modal.propTypes = {
+    className: PropTypes.string,
+    autoFocus: PropTypes.bool,
+    disabled: PropTypes.bool,
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
 };
 
 module.exports = Modal;
