19dcbc648004584c6327786183e6d697cb977115
Vladimir Borisov <borisov1024@gmail.com>
2019-10-07 12:39:45 +0300

    Handle properly the state container

--- src/routes/Settings/useSettings.js
+++ src/routes/Settings/useSettings.js
@@ -2,48 +2,41 @@ const React = require('react');
 const { useServices } = require('stremio/services');
 
 module.exports = (devTestWithUser) => {
+    const IGNORED_SETTINGS = Object.freeze(['user']);
+
     const { core } = useServices();
-    // TODO: move these values in the state container
     const [settings, setSettings] = React.useState({});
-    /*
-        {
-        'user': devTestWithUser ? {
-            '_id': 'neo',
-            'email': 'neo@example.com',
-            'avatar': 'https://www.thenational.ae/image/policy:1.891803:1566372420/AC17-Matrix-20-04.jpg',
-        } : null,
-        'ui_language': "eng",
-        'subtitles_language': 'bul',
-        'subtitles_size': '100%',
-        'subtitles_background': '',
-        'subtitles_color': '#ffffff',
-        'subtitles_outline_color': '#000',
-        'auto-play_next_episode': 'true',
-        'pause_playback_when_minimized': 'false',
-        'hardware-accelerated_decoding': 'true',
-        'launch_player_in_a_separate_window_(advanced)': 'true',
-        'caching': '2048',
-        'torrent_profile': 'profile-default',
-        'streaming_server_is_available.': 'true',
-    });
-    */
 
     React.useEffect(() => {
-        const onNewState = () => {
-            const state = core.getState();
+        const updateState = (state) => {
             try {
-                setSettings({ ...settings, ...state.ctx.content.settings });
-            } catch (e) { }
-        };
-        window.top.core = core;
-        core.on('NewModel', onNewState);
-        core.dispatch({
-            action: 'Load',
-            args: {
-                load: 'CatalogGrouped',
-                args: { extra: [] }
+                setSettings({
+                    ...settings, ...state.ctx.content.settings, user: devTestWithUser ? {
+                        '_id': 'neo',
+                        'email': 'neo@example.com',
+                        'avatar': 'https://www.thenational.ae/image/policy:1.891803:1566372420/AC17-Matrix-20-04.jpg',
+                    } : state.ctx.content.auth && state.ctx.content.auth.user
+                });
+            } catch (e) {
+                console.log('Cannot update settings state', e);
             }
-        });
+        }
+
+        const state = core.getState();
+        try {
+            if (state.ctx.is_loaded) {
+                updateState(state);
+                return;
+            }
+        } catch (e) {
+            console.log('Cannot find state context', e);
+        }
+
+        const onNewState = () => {
+            updateState(core.getState());
+        };
+        core.on('NewModel', onNewState);
+        core.dispatch({ action: 'LoadCtx' });
         return () => {
             // Destructor function
             core.off('NewModel', onNewState);
@@ -51,10 +44,10 @@ module.exports = (devTestWithUser) => {
     }, []);
 
     const setTheSettings = React.useCallback(newSettings => {
-        const { user, ...args } = { ...newSettings };
-        console.log('Save', args);
-        if(args.language === "bul") throw new Error('why')
-        Object.keys(args).forEach(key=> args[key] = args[key].toString())
+        const args = {};
+        Object.keys(newSettings)
+            .filter(prop => !IGNORED_SETTINGS.includes(prop))
+            .forEach(key => args[key] = newSettings[key].toString())
         setSettings(newSettings);
         core.dispatch({ action: 'Settings', args: { settings: 'Store', args } });
     }, [settings])
