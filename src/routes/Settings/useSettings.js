d31a07c4990f94dbdc9e58d52f254e307f208d88
Vladimir Borisov <borisov1024@gmail.com>
2019-10-09 13:11:42 +0300

    Load core context on boot; Drop development test user data

--- src/routes/Settings/useSettings.js
+++ src/routes/Settings/useSettings.js
@@ -1,45 +1,31 @@
 const React = require('react');
 const { useServices } = require('stremio/services');
 
-module.exports = (devTestWithUser) => {
+module.exports = () => {
     const IGNORED_SETTINGS = Object.freeze(['user']);
 
     const { core } = useServices();
     const [settings, setSettings] = React.useState({});
 
     React.useEffect(() => {
-        const updateState = (state) => {
-            try {
+        const updateState = () => {
+            const state = core.getState();
+            if (state.ctx && state.ctx.is_loaded && state.ctx.content && state.ctx.content.settings) {
                 setSettings({
-                    ...settings, ...state.ctx.content.settings, user: devTestWithUser ? {
-                        '_id': 'neo',
-                        'email': 'neo@example.com',
-                        'avatar': 'https://www.thenational.ae/image/policy:1.891803:1566372420/AC17-Matrix-20-04.jpg',
-                    } : state.ctx.content.auth && state.ctx.content.auth.user
+                    ...settings,
+                    ...state.ctx.content.settings,
+                    user: state.ctx.content.auth ? state.ctx.content.auth.user : null
                 });
-            } catch (e) {
-                console.log('Cannot update settings state', e);
             }
         }
 
-        const state = core.getState();
-        try {
-            if (state.ctx.is_loaded) {
-                updateState(state);
-                return;
-            }
-        } catch (e) {
-            console.log('Cannot find state context', e);
-        }
+        core.on('NewModel', updateState);
 
-        const onNewState = () => {
-            updateState(core.getState());
-        };
-        core.on('NewModel', onNewState);
-        core.dispatch({ action: 'LoadCtx' });
+        updateState();
+
         return () => {
             // Destructor function
-            core.off('NewModel', onNewState);
+            core.off('NewModel', updateState);
         };
     }, []);
 
@@ -48,9 +34,8 @@ module.exports = (devTestWithUser) => {
         Object.keys(newSettings)
             .filter(prop => !IGNORED_SETTINGS.includes(prop))
             .forEach(key => args[key] = newSettings[key].toString())
-        setSettings(newSettings);
         core.dispatch({ action: 'Settings', args: { settings: 'Store', args } });
-    }, [settings])
+    }, [])
 
     return [settings, setTheSettings];
 };
