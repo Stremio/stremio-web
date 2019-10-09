481264cb876bfa803e772de20fcba752e3d1a728
svetlagasheva <svetulka_24@mail.bg>
2019-10-09 16:21:26 +0300

    NotificationsList implemented

---
+++ src/common/NavBar/NotificationsMenu/NotificationsList/NotificationsList.js
@@ -0,0 +1,52 @@
+const React = require('react');
+const PropTypes = require('prop-types');
+const classnames = require('classnames');
+const Notification = require('./Notification');
+const useMetaItems = require('./useMetaItems');
+const styles = require('./styles');
+
+const NotificationsList = ({ className, metaItems }) => {
+    const notifications = useMetaItems(metaItems);
+    return (
+        <div className={classnames(className, styles['notifications-list-scroll-container'])}>
+            {
+                notifications.length > 0 ?
+                    notifications.map((notification) => (
+                        notification.videos.length === 1 ?
+                            <Notification
+                                {...notification}
+                                key={notification.id}
+                                posterThumbnail={true}
+                                poster={notification.videos[0].poster}
+                                season={notification.videos[0].season}
+                                episode={notification.videos[0].episode}
+                                released={notification.videos[0].released}
+                                className={styles['notification']}
+                            />
+                            :
+                            <Notification
+                                {...notification}
+                                key={notification.id}
+                                posterThumbnail={false}
+                                name={notification.videos.length + ' new videos for ' + notification.name}
+                                released={notification.videos[notification.videos.length - 1].released}
+                                className={styles['notification']}
+                            />
+                    ))
+                    :
+                    <React.Fragment>
+                        <div className={styles['no-notifications-label']}>
+                            No new notifications.
+                        </div>
+                    </React.Fragment>
+            }
+        </div>
+    );
+}
+
+NotificationsList.propTypes = {
+    className: PropTypes.string,
+    metaItems: PropTypes.object
+};
+
+module.exports = NotificationsList;
