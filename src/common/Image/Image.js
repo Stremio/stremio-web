73ffe015499a024b67407700eda9611a6b532e06
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-10-10 18:16:53 +0300

    Image container removed

--- src/common/Image/Image.js
+++ src/common/Image/Image.js
@@ -1,36 +1,28 @@
 const React = require('react');
 const PropTypes = require('prop-types');
-const styles = require('./styles');
 
 const Image = ({ className, src, alt, fallbackSrc, renderFallback }) => {
     const [broken, setBroken] = React.useState(false);
-    const onError = React.useCallback((event) => {
-        if (typeof src !== 'string' || event.currentTarget.src === src) {
-            setBroken(true);
-        }
-    }, [src]);
+    const onError = React.useCallback(() => {
+        setBroken(true);
+    }, []);
     React.useLayoutEffect(() => {
         setBroken(false);
     }, [src]);
-    return (
-        <div className={className}>
-            {
-                (broken || typeof src !== 'string' || src.length === 0) && (typeof renderFallback === 'function' || typeof fallbackSrc === 'string') ?
-                    typeof renderFallback === 'function' ?
-                        renderFallback()
-                        :
-                        <img className={styles['fallback-image']} src={fallbackSrc} alt={alt} />
-                    :
-                    <img className={styles['image']} src={src} alt={alt} onError={onError} />
-            }
-        </div>
-    );
+    return (broken || typeof src !== 'string' || src.length === 0) && (typeof renderFallback === 'function' || typeof fallbackSrc === 'string') ?
+        typeof renderFallback === 'function' ?
+            renderFallback()
+            :
+            <img className={className} src={fallbackSrc} alt={alt} />
+        :
+        <img className={className} src={src} alt={alt} onError={onError} />;
 };
 
 Image.propTypes = {
     className: PropTypes.string,
     src: PropTypes.string,
     alt: PropTypes.string,
+    fallbackSrc: PropTypes.string,
     renderFallback: PropTypes.func
 };
 
