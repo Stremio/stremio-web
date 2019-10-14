443db842fb4ebca30289f007d6aa332897a04b1a
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-10-14 15:09:20 +0300

    board ui finalized

---
+++ src/common/MetaItem/MetaItem.js
@@ -0,0 +1,129 @@
+const React = require('react');
+const PropTypes = require('prop-types');
+const classnames = require('classnames');
+const Icon = require('stremio-icons/dom');
+const Button = require('stremio/common/Button');
+const Image = require('stremio/common/Image');
+const Multiselect = require('stremio/common/Multiselect');
+const useBinaryState = require('stremio/common/useBinaryState');
+const useDataset = require('stremio/common/useDataset');
+const PlayIconCircleCentered = require('./PlayIconCircleCentered');
+const styles = require('./styles');
+
+const ICON_FOR_TYPE = Object.assign(Object.create(null), {
+    'movie': 'ic_movies',
+    'series': 'ic_series',
+    'channel': 'ic_channels',
+    'tv': 'ic_tv',
+    'other': 'ic_movies'
+});
+
+const MetaItem = React.memo(({ className, type, name, poster, posterShape, playIcon, progress, menuOptions, onSelect, menuOptionOnSelect, ...props }) => {
+    const dataset = useDataset(props);
+    const [menuOpen, onMenuOpen, onMenuClose] = useBinaryState(false);
+    const metaItemOnClick = React.useCallback((event) => {
+        if (!event.nativeEvent.selectMetaItemPrevented && typeof onSelect === 'function') {
+            onSelect({
+                type: 'select',
+                dataset: dataset,
+                reactEvent: event,
+                nativeEvent: event.nativeEvent
+            });
+        }
+    }, [onSelect, dataset]);
+    const multiselectOnClick = React.useCallback((event) => {
+        event.nativeEvent.selectMetaItemPrevented = true;
+    }, []);
+    const multiselectOnSelect = React.useCallback((event) => {
+        if (typeof menuOptionOnSelect === 'function') {
+            menuOptionOnSelect({
+                type: 'select-option',
+                dataset: dataset,
+                reactEvent: event.reactEvent,
+                nativeEvent: event.nativeEvent
+            });
+        }
+    }, [menuOptionOnSelect, dataset]);
+    return (
+        <Button className={classnames(className, styles['meta-item-container'], styles['poster-shape-poster'], styles[`poster-shape-${posterShape}`], { 'active': menuOpen })} title={name} onClick={metaItemOnClick}>
+            <div className={styles['poster-container']}>
+                <div className={styles['poster-image-layer']}>
+                    <Image
+                        className={styles['poster-image']}
+                        src={poster}
+                        alt={' '}
+                        renderFallback={() => (
+                            <Icon
+                                className={styles['placeholder-icon']}
+                                icon={typeof ICON_FOR_TYPE[type] === 'string' ? ICON_FOR_TYPE[type] : ICON_FOR_TYPE['other']}
+                            />
+                        )}
+                    />
+                </div>
+                {
+                    playIcon ?
+                        <div className={styles['play-icon-layer']}>
+                            <PlayIconCircleCentered className={styles['play-icon']} />
+                        </div>
+                        :
+                        null
+                }
+                {
+                    progress > 0 ?
+                        <div className={styles['progress-bar-layer']}>
+                            <div className={styles['progress-bar']} style={{ width: `${Math.min(progress, 1) * 100}%` }} />
+                        </div>
+                        :
+                        null
+                }
+            </div>
+            {
+                (typeof name === 'string' && name.length > 0) || (Array.isArray(menuOptions) && menuOptions.length > 0) ?
+                    <div className={styles['title-bar-container']}>
+                        {
+                            typeof name === 'string' && name.length > 0 ?
+                                <div className={styles['title-label']}>{name}</div>
+                                :
+                                null
+                        }
+                        {
+                            Array.isArray(menuOptions) && menuOptions.length > 0 ?
+                                <div className={styles['multiselect-container']} onClick={multiselectOnClick}>
+                                    <Multiselect
+                                        className={styles['multiselect-label-container']}
+                                        renderLabelContent={() => (
+                                            <Icon className={styles['icon']} icon={'ic_more'} />
+                                        )}
+                                        options={menuOptions}
+                                        onOpen={onMenuOpen}
+                                        onClose={onMenuClose}
+                                        onSelect={multiselectOnSelect}
+                                    />
+                                </div>
+                                :
+                                null
+                        }
+                    </div>
+                    :
+                    null
+            }
+        </Button>
+    );
+});
+
+MetaItem.displayName = 'MetaItem';
+
+MetaItem.propTypes = {
+    className: PropTypes.string,
+    type: PropTypes.string,
+    name: PropTypes.string,
+    poster: PropTypes.string,
+    posterShape: PropTypes.oneOf(['poster', 'landscape', 'square']),
+    playIcon: PropTypes.bool,
+    progress: PropTypes.number,
+    menuOptions: PropTypes.array,
+    onSelect: PropTypes.func,
+    menuOptionOnSelect: PropTypes.func
+};
+
+module.exports = MetaItem;
--- src/common/MetaRow/MetaRow.js
+++ src/common/MetaRow/MetaRow.js
@@ -3,70 +3,62 @@ const PropTypes = require('prop-types');
 const classnames = require('classnames');
 const Icon = require('stremio-icons/dom');
 const Button = require('stremio/common/Button');
-const MetaItem = require('./MetaItem');
+const MetaItem = require('stremio/common/MetaItem');
+const MetaRowPlaceholder = require('./MetaRowPlaceholder');
 const styles = require('./styles');
 
-const MetaRow = ({ className, title, message, items, itemMenuOptions }) => {
+const MetaRow = ({ className, title, message, items, maximumItemsCount, itemMenuOptions }) => {
+    maximumItemsCount = maximumItemsCount !== null && isFinite(maximumItemsCount) ? maximumItemsCount : 20;
+    items = Array.isArray(items) ? items.slice(0, maximumItemsCount) : [];
     return (
         <div className={classnames(className, styles['meta-row-container'])}>
             {
                 typeof title === 'string' && title.length > 0 ?
-                    <div className={styles['title-container']}>{title}</div>
+                    <div className={styles['title-container']} title={title}>{title}</div>
                     :
                     null
             }
             {
                 typeof message === 'string' && message.length > 0 ?
-                    <div className={styles['message-container']}>{message}</div>
+                    <div className={styles['message-container']} title={message}>{message}</div>
                     :
-                    null
-            }
-            {
-                Array.isArray(items) && items.length > 0 ?
                     <React.Fragment>
                         <div className={styles['meta-items-container']}>
-                            {items.map((item) => (
+                            {items.map((item, index) => (
                                 <MetaItem
                                     {...item}
-                                    key={item.id}
+                                    key={index}
+                                    data-id={item.id}
+                                    data-type={item.type}
                                     className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${item.posterShape}`])}
-                                    title={item.name}
                                     menuOptions={itemMenuOptions}
                                 />
                             ))}
-                            <div className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${items[0].posterShape}`])} />
-                            <div className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${items[0].posterShape}`])} />
-                            <div className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${items[0].posterShape}`])} />
-                            <div className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${items[0].posterShape}`])} />
-                            <div className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${items[0].posterShape}`])} />
-                            <div className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${items[0].posterShape}`])} />
-                            <div className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${items[0].posterShape}`])} />
-                            <div className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${items[0].posterShape}`])} />
-                            <div className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${items[0].posterShape}`])} />
-                            <div className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${items[0].posterShape}`])} />
+                            {Array(Math.max(maximumItemsCount - items.length, 0)).fill(null).map((_, index) => (
+                                <div key={index} className={classnames(styles['meta-item'], styles['poster-shape-poster'])} />
+                            ))}
                         </div>
                         <Button className={styles['see-all-container']} title={'SEE ALL'}>
-                            <div className={styles['see-all-label']}>SEE ALL</div>
+                            <div className={styles['label']}>SEE ALL</div>
                             <Icon className={styles['icon']} icon={'ic_arrow_thin_right'} />
                         </Button>
                     </React.Fragment>
-                    :
-                    null
             }
         </div>
     );
 };
+
+MetaRow.Placeholder = MetaRowPlaceholder;
 
 MetaRow.propTypes = {
     className: PropTypes.string,
     title: PropTypes.string,
     message: PropTypes.string,
     items: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
         posterShape: PropTypes.string
     })),
-    itemMenuOptions: PropTypes.array
+    maximumItemsCount: PropTypes.number,
+    itemMenuOptions: PropTypes.any
 };
 
 module.exports = MetaRow;
