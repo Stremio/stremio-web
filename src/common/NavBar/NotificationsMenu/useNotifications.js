10e7e98dd72b044e8945222b5492684e13b4f375
svetlagasheva <svetulka_24@mail.bg>
2019-10-15 17:06:41 +0300

    useNotifications hook implemented

---
+++ src/common/NavBar/NotificationsMenu/useNotifications.js
@@ -0,0 +1,27 @@
+const React = require('react');
+const { useServices } = require('stremio/services');
+
+const useNotifications = () => {
+    const [notifications, setNotifications] = React.useState([]);
+    const { core } = useServices();
+    React.useEffect(() => {
+        const onNewState = () => {
+            const state = core.getState();
+            setNotifications(state.notifications.groups);
+        };
+        core.on('NewModel', onNewState);
+        core.dispatch({
+            action: 'Load',
+            args: {
+                load: 'Notifications',
+                args: { extra: [] }
+            }
+        });
+        return () => {
+            core.off('NewModel', onNewState);
+        };
+    }, []);
+    return notifications;
+};
+
+module.exports = useNotifications;
