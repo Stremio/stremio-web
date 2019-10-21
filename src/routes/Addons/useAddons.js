9ce3e28def4f3a0418c0a9e732eb26143db9806f
svetlagasheva <svetulka_24@mail.bg>
2019-10-21 13:57:45 +0300

    filter addons in useAddons hook

--- src/routes/Addons/useAddons.js
+++ src/routes/Addons/useAddons.js
@@ -6,57 +6,63 @@ const DEFAULT_CATEGORY = 'community';
 const DEFAULT_TYPE = 'all';
 
 const useAddons = (category, type) => {
-    category = CATEGORIES.includes(category) ? category : DEFAULT_CATEGORY;
-    type = typeof type === 'string' && type.length > 0 ? type : DEFAULT_TYPE;
-    const [addons, setAddons] = React.useState([]);
     const { core } = useServices();
+    const [addons, setAddons] = React.useState([[], []]);
     React.useEffect(() => {
+        category = CATEGORIES.includes(category) ? category : DEFAULT_CATEGORY;
+        type = typeof type === 'string' && type.length > 0 ? type : DEFAULT_TYPE;
+        const onSelect = (event) => {
+            const { value } = event.reactEvent.currentTarget.dataset;
+            const name = event.dataset.name;
+            if (name === 'category') {
+                const nextCategory = CATEGORIES.includes(value) ? value : '';
+                window.location.replace(`#/addons/${nextCategory}/${DEFAULT_TYPE}`);
+            } else if (name === 'type') {
+                const nextType = typeof value === 'string' ? value : '';
+                window.location.replace(`#/addons/${category}/${nextType}`);
+            }
+        };
+        const categoryDropdown = () => {
+            const selected = CATEGORIES.includes(category) ? [category] : [];
+            const options = CATEGORIES
+                .map((category) => ({ label: category, value: category }));
+            return {
+                'data-name': 'category',
+                selected,
+                options,
+                onSelect
+            };
+        };
+        const typeDropdown = () => {
+            const selected = typeof type === 'string' && type.length > 0 ? [type] : [];
+            const options = ['all', 'movie', 'series', 'channel']
+                .concat(selected)
+                .filter((type, index, types) => types.indexOf(type) === index)
+                .map((type) => ({ label: type, value: type }));
+            return {
+                'data-name': 'type',
+                selected,
+                options,
+                onSelect
+            };
+        };
         const onNewState = () => {
             const state = core.getState();
-            setAddons(state.ctx.content.addons);
+            setAddons([
+                state.ctx.content.addons.filter((addon) => {
+                    return (categoryDropdown().selected.join('') === 'official' ? addon.flags.official : !addon.flags.official) &&
+                        (typeDropdown().selected.join('') === 'all' || addon.manifest.types.includes(typeDropdown().selected.join('')));
+                }),
+                [categoryDropdown(), typeDropdown()]
+            ]);
         };
         core.on('NewModel', onNewState);
         onNewState();
         return () => {
             core.off('NewModel', onNewState);
         };
-    }, []);
-    const onSelect = React.useCallback((event) => {
-        const { value } = event.reactEvent.currentTarget.dataset;
-        const name = event.dataset.name;
-        if (name === 'category') {
-            const nextCategory = CATEGORIES.includes(value) ? value : '';
-            window.location.replace(`#/addons/${nextCategory}/${DEFAULT_TYPE}`);
-        } else if (name === 'type') {
-            const nextType = typeof value === 'string' ? value : '';
-            window.location.replace(`#/addons/${category}/${nextType}`);
-        }
     }, [category, type]);
-    const categoryDropdown = React.useMemo(() => {
-        const selected = CATEGORIES.includes(category) ? [category] : [];
-        const options = CATEGORIES
-            .map((category) => ({ label: category, value: category }));
-        return {
-            'data-name': 'category',
-            selected,
-            options,
-            onSelect
-        };
-    }, [category, onSelect]);
-    const typeDropdown = React.useMemo(() => {
-        const selected = typeof type === 'string' && type.length > 0 ? [type] : [];
-        const options = ['all', 'movie', 'series', 'channel']
-            .concat(selected)
-            .filter((type, index, types) => types.indexOf(type) === index)
-            .map((type) => ({ label: type, value: type }));
-        return {
-            'data-name': 'type',
-            selected,
-            options,
-            onSelect
-        };
-    }, [type, onSelect]);
-    return [addons, [categoryDropdown, typeDropdown]];
+    return addons;
 };
 
 module.exports = useAddons;
