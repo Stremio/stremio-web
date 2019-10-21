0c4618b919ec49e9968953526d07e2304e513038
svetlagasheva <svetulka_24@mail.bg>
2019-10-21 15:26:47 +0300

    addons types linked with stremio-core

--- src/routes/Addons/useAddons.js
+++ src/routes/Addons/useAddons.js
@@ -11,43 +11,47 @@ const useAddons = (category, type) => {
     React.useEffect(() => {
         category = CATEGORIES.includes(category) ? category : DEFAULT_CATEGORY;
         type = typeof type === 'string' && type.length > 0 ? type : DEFAULT_TYPE;
-        const onSelect = (event) => {
-            const { value } = event.reactEvent.currentTarget.dataset;
-            const name = event.dataset.name;
-            if (name === 'category') {
-                const nextCategory = CATEGORIES.includes(value) ? value : '';
-                window.location.replace(`#/addons/${nextCategory}/${DEFAULT_TYPE}`);
-            } else if (name === 'type') {
-                const nextType = typeof value === 'string' ? value : '';
-                window.location.replace(`#/addons/${category}/${nextType}`);
-            }
-        };
-        const categoryDropdown = () => {
-            const selected = CATEGORIES.includes(category) ? [category] : [];
-            const options = CATEGORIES
-                .map((category) => ({ label: category, value: category }));
-            return {
-                'data-name': 'category',
-                selected,
-                options,
-                onSelect
-            };
-        };
-        const typeDropdown = () => {
-            const selected = typeof type === 'string' && type.length > 0 ? [type] : [];
-            const options = ['all', 'movie', 'series', 'channel']
-                .concat(selected)
-                .filter((type, index, types) => types.indexOf(type) === index)
-                .map((type) => ({ label: type, value: type }));
-            return {
-                'data-name': 'type',
-                selected,
-                options,
-                onSelect
-            };
-        };
         const onNewState = () => {
             const state = core.getState();
+            const onSelect = (event) => {
+                const { value } = event.reactEvent.currentTarget.dataset;
+                const name = event.dataset.name;
+                if (name === 'category') {
+                    const nextCategory = CATEGORIES.includes(value) ? value : '';
+                    window.location.replace(`#/addons/${nextCategory}/${DEFAULT_TYPE}`);
+                } else if (name === 'type') {
+                    const nextType = typeof value === 'string' ? value : '';
+                    window.location.replace(`#/addons/${category}/${nextType}`);
+                }
+            };
+            const categoryDropdown = () => {
+                const selected = CATEGORIES.includes(category) ? [category] : [];
+                const options = CATEGORIES
+                    .map((category) => ({ label: category, value: category }));
+                return {
+                    'data-name': 'category',
+                    selected,
+                    options,
+                    onSelect
+                };
+            };
+            const typeDropdown = () => {
+                const selected = typeof type === 'string' && type.length > 0 ? [type] : [];
+                const options = [...new Set(
+                    [].concat.apply([],
+                        ['all'].concat(
+                            state.ctx.content.addons.map(addon => addon.manifest.types),
+                            selected
+                        )
+                    ))]
+                    .map((type) => ({ label: type, value: type }));
+                return {
+                    'data-name': 'type',
+                    selected,
+                    options,
+                    onSelect
+                };
+            };
             setAddons([
                 state.ctx.content.addons.filter((addon) => {
                     return (categoryDropdown().selected.join('') === 'official' ? addon.flags.official : !addon.flags.official) &&
