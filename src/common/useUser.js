5c349c4fcaae5d785b317d1373ec483890247cae
svetlagasheva <svetulka_24@mail.bg>
2019-11-11 09:35:38 +0200

    getState called inside onNewState

--- src/common/useUser.js
+++ src/common/useUser.js
@@ -7,6 +7,7 @@ const useUser = () => {
     const [user, setUser] = React.useState(state.ctx.content.auth ? state.ctx.content.auth.user : null);
     React.useEffect(() => {
         const onNewState = () => {
+            const state = core.getState();
             setUser(state.ctx.content.auth ? state.ctx.content.auth.user : null);
         };
         core.on('NewModel', onNewState);
