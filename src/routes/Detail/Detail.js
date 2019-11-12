05f41224803d69ae3c7ac1fdd9037ca56bc441f3
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-11-12 15:49:30 +0200

    Detail wrap metaState in reducer to keep state consistent

--- src/routes/Detail/Detail.js
+++ src/routes/Detail/Detail.js
@@ -3,42 +3,132 @@ const { NavBar, MetaPreview } = require('stremio/common');
 const VideosList = require('./VideosList');
 const StreamsList = require('./StreamsList');
 const useMetaDetails = require('./useMetaDetails');
-const useInLibrary = require('./useInLibrary');
 const styles = require('./styles');
 
 const Detail = ({ urlParams }) => {
-    const [meta, streams] = useMetaDetails(urlParams);
-    const [inLibrary, addToLibrary, removeFromLibrary, toggleInLibrary] = useInLibrary(urlParams.id);
+    const [metaGroups, streamsGroups, [metaResourceRef, streamsResourceRef]] = useMetaDetails(urlParams);
+    const [metaState, metaStateDispatch] = React.useReducer(
+        (state, action) => {
+            switch (action.type) {
+                case 'groups-changed': {
+                    if (state.selectedGroup) {
+                        const selectedGroupIncluded = action.groups.some((group) => {
+                            return group.req.base === state.selectedGroup.req.base &&
+                                group.content.type === 'Ready';
+                        });
+                        if (selectedGroupIncluded) {
+                            return {
+                                ...state,
+                                resourceRef: action.resourceRef,
+                                groups: action.groups
+                            };
+                        }
+                    }
+
+                    const readyGroup = action.groups.find((group) => group.content.type === 'Ready');
+                    const selectedGroup = readyGroup ? readyGroup : null;
+                    return {
+                        ...state,
+                        resourceRef: action.resourceRef,
+                        groups: action.groups,
+                        selectedGroup
+                    };
+                }
+                case 'group-selected': {
+                    const selectedGroup = state.groups.find((group) => {
+                        return group.req.base === action.base &&
+                            group.content.type === 'Ready';
+                    });
+                    if (selectedGroup) {
+                        return {
+                            ...state,
+                            selectedGroup
+                        };
+                    }
+
+                    return state;
+                }
+                default: {
+                    return state;
+                }
+            }
+        },
+        [metaResourceRef, metaGroups],
+        ([resourceRef, groups]) => {
+            const readyGroup = groups.find((group) => group.content.type === 'Ready');
+            const selectedGroup = readyGroup ? readyGroup : null;
+            return {
+                resourceRef,
+                groups,
+                selectedGroup
+            };
+        }
+    );
+    React.useEffect(() => {
+        metaStateDispatch({
+            type: 'groups-changed',
+            resourceRef: metaResourceRef,
+            groups: metaGroups
+        });
+    }, [metaGroups]);
     return (
         <div className={styles['detail-container']}>
             <NavBar
                 className={styles['nav-bar']}
                 backButton={true}
-                title={meta !== null && meta.content.type === 'Ready' ? meta.content.content.name : null}
+                title={metaState.selectedGroup !== null ? metaState.selectedGroup.content.content.name : null}
             />
             <div className={styles['detail-content']}>
                 {
-                    meta !== null && meta.content.type === 'Ready' ?
-                        <React.Fragment>
-                            <div className={styles['background-image-layer']}>
-                                <img className={styles['background-image']} src={meta.content.content.background} alt={' '} />
-                            </div>
-                            <MetaPreview
-                                {...meta.content.content}
-                                className={styles['meta-preview']}
-                                background={null}
-                                inLibrary={inLibrary}
-                                toggleInLibrary={toggleInLibrary}
-                            />
-                        </React.Fragment>
+                    metaState.resourceRef !== null ?
+                        metaState.selectedGroup !== null ?
+                            <React.Fragment>
+                                <div className={styles['background-image-layer']}>
+                                    <img
+                                        className={styles['background-image']}
+                                        src={metaState.selectedGroup.content.content.background}
+                                        alt={' '}
+                                    />
+                                </div>
+                                <MetaPreview
+                                    {...metaState.selectedGroup.content.content}
+                                    className={styles['meta-preview']}
+                                    background={null}
+                                />
+                            </React.Fragment>
+                            :
+                            metaState.groups.length === 0 ?
+                                <MetaPreview
+                                    className={styles['meta-preview']}
+                                    name={'No addons ware requested for this meta'}
+                                />
+                                :
+                                metaState.groups.every((group) => group.content.type === 'Err') ?
+                                    <MetaPreview
+                                        className={styles['meta-preview']}
+                                        name={'No metadata was found'}
+                                    />
+                                    :
+                                    <MetaPreview.Placeholder
+                                        className={styles['meta-preview']}
+                                    />
                         :
-                        <MetaPreview.Placeholder className={styles['meta-preview']} />
+                        null
                 }
                 {
-                    typeof urlParams.videoId === 'string' && urlParams.videoId.length > 0 ?
-                        <StreamsList className={styles['streams-list']} streams={streams} />
+                    streamsResourceRef !== null ?
+                        <StreamsList
+                            className={styles['streams-list']}
+                            streamsGroups={streamsGroups}
+                        />
                         :
-                        <VideosList className={styles['videos-list']} meta={meta} />
+                        metaState.resourceRef !== null ?
+                            <VideosList
+                                className={styles['videos-list']}
+                                metaGroup={metaState.selectedGroup}
+                            />
+                            :
+                            null
                 }
             </div>
         </div>
