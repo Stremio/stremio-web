dd8628c24a3d9a0b7f13d6e57aaad47f8d0d68ea
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-11-18 10:07:50 +0200

    MetaLinks styles updated

--- src/common/MetaPreview/MetaLinks/MetaLinks.js
+++ src/common/MetaPreview/MetaLinks/MetaLinks.js
@@ -4,21 +4,21 @@ const classnames = require('classnames');
 const Button = require('stremio/common/Button');
 const styles = require('./styles');
 
-const MetaLinks = ({ className, label, links }) => {
+const MetaLinks = ({ className, category, links }) => {
     return (
         <div className={classnames(className, styles['meta-links-container'])}>
             {
-                typeof label === 'string' && label.length > 0 ?
-                    <div className={styles['label-container']}>{label}:</div>
+                typeof category === 'string' && category.length > 0 ?
+                    <div className={styles['category-container']}>{category}:</div>
                     :
                     null
             }
             {
                 Array.isArray(links) && links.length > 0 ?
                     <div className={styles['links-container']}>
-                        {links.map(({ label, href }, index) => (
-                            <Button key={`${label}-${href}-${index}`} className={styles['link-container']} title={label} tabIndex={-1} href={href}>
-                                {label}
+                        {links.map(({ name, href }, index) => (
+                            <Button key={index} className={styles['link-container']} title={name} tabIndex={-1} href={href}>
+                                {name}
                                 {index < links.length - 1 ? ',' : null}
                             </Button>
                         ))}
@@ -32,10 +32,10 @@ const MetaLinks = ({ className, label, links }) => {
 
 MetaLinks.propTypes = {
     className: PropTypes.string,
-    label: PropTypes.string,
+    category: PropTypes.string,
     links: PropTypes.arrayOf(PropTypes.shape({
-        label: PropTypes.string.isRequired,
-        href: PropTypes.string.isRequired
+        name: PropTypes.string,
+        href: PropTypes.string
     }))
 };
 
