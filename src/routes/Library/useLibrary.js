343519b5282c631af5d912bb89da21c4de7b7a5c
svetlagasheva <svetulka_24@mail.bg>
2019-11-26 10:58:37 +0200

    lib items state mapped in the initial state; useLibrary returns lib state

--- src/routes/Library/useLibrary.js
+++ src/routes/Library/useLibrary.js
@@ -1,9 +1,20 @@
 const React = require('react');
 const { useServices } = require('stremio/services');
 
+const mapLibItemsState = (state) => {
+    const library_state = state.library.library_state.type;
+    const selected = state.library.selected.type_name;
+    const lib_items = state.library.lib_items.map((lib_item) => {
+        lib_item._ctime = new Date(lib_item._ctime);
+        return lib_item;
+    });
+    const type_names = state.library.type_names;
+    return { library_state, selected, lib_items, type_names };
+};
+
 const useLibrary = (urlParams) => {
     const { core } = useServices();
-    const [library, setLibrary] = React.useState([null, [], []]);
+    const [library, setLibrary] = React.useState([null, null, [], []]);
     React.useEffect(() => {
         const onNewState = () => {
             const state = core.getState();
@@ -19,7 +30,8 @@ const useLibrary = (urlParams) => {
                 });
                 return;
             }
-            setLibrary([state.library.selected.type_name, state.library.type_names, state.library.lib_items]);
+            const libItems = mapLibItemsState(state);
+            setLibrary([libItems.library_state, libItems.selected, libItems.type_names, libItems.lib_items]);
         };
         core.on('NewModel', onNewState);
         const state = core.getState();
