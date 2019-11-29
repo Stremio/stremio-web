32be8a633a09a9dcb373037310a0efa1891f4ebc
svetlagasheva <svetulka_24@mail.bg>
2019-11-29 17:37:49 +0200

    handle 'no requested addons' case

--- src/routes/Search/Search.js
+++ src/routes/Search/Search.js
@@ -15,42 +15,49 @@ const Search = ({ queryParams }) => {
             <div className={styles['search-content']}>
                 {
                     search.selected && search.selected[0][1] && search.selected[0][1].length > 0 ?
-                        search.items_groups && search.items_groups.length > 0 && search.items_groups.some(group => group.content.type !== 'Err') ?
-                            search.items_groups.map(({ request, content }, index) => {
-                                switch (content.type) {
-                                    case 'Ready':
-                                        return (
-                                            <MetaRow
-                                                key={`${index}${request.base}${content.type} Ready`}
-                                                className={styles['search-row']}
-                                                title={`${request.path.id} - ${request.path.type_name}`}
-                                                items={content.content}
-                                                onSeeAllButtonClicked={() => discoverUrls(request)}
-                                            />
-                                        );
-                                    case 'Err':
-                                        return (
-                                            <MetaRow
-                                                key={`${index}${request.base}${content.type}`}
-                                                className={styles['search-row']}
-                                                title={`${request.path.id} - ${request.path.type_name} Err`}
-                                                message={`${content.content.type} ${typeof content.content.content === 'string' ? content.content.content : ''}`}
-                                            />
-                                        );
-                                    case 'Loading':
-                                        return (
-                                            <MetaRow.Placeholder
-                                                key={`${index}${request.base}${content.type} Loading`}
-                                                className={styles['search-row-placeholder']}
-                                                title={`${request.path.id} - ${request.path.type_name}`}
-                                            />
-                                        );
-                                }
-                            })
+                        search.items_groups && search.items_groups.length > 0 ?
+                            search.items_groups.some(group => group.content.type !== 'Err') ?
+                                search.items_groups.map(({ request, content }, index) => {
+                                    switch (content.type) {
+                                        case 'Ready':
+                                            return (
+                                                <MetaRow
+                                                    key={`${index}${request.base}${content.type} Ready`}
+                                                    className={styles['search-row']}
+                                                    title={`${request.path.id} - ${request.path.type_name}`}
+                                                    items={content.content}
+                                                    onSeeAllButtonClicked={() => discoverUrls(request)}
+                                                />
+                                            );
+                                        case 'Err':
+                                            return (
+                                                <MetaRow
+                                                    key={`${index}${request.base}${content.type}`}
+                                                    className={styles['search-row']}
+                                                    title={`${request.path.id} - ${request.path.type_name} Err`}
+                                                    message={`${content.content.type} ${typeof content.content.content === 'string' ? content.content.content : ''}`}
+                                                />
+                                            );
+                                        case 'Loading':
+                                            return (
+                                                <MetaRow.Placeholder
+                                                    key={`${index}${request.base}${content.type} Loading`}
+                                                    className={styles['search-row-placeholder']}
+                                                    title={`${request.path.id} - ${request.path.type_name}`}
+                                                />
+                                            );
+                                    }
+                                })
+                                :
+                                <div className={styles['message-container']}>
+                                    <div className={styles['message-content']}>
+                                        <div className={styles['label']}>No metadata was found</div>
+                                    </div>
+                                </div>
                             :
                             <div className={styles['message-container']}>
                                 <div className={styles['message-content']}>
-                                    <div className={styles['label']}>No metadata was found</div>
+                                    <div className={styles['label']}> No addons were requested for metadata</div>
                                 </div>
                             </div>
                         :
