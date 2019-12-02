268d5a6e0b2863705b3682eb67c5d75aa35602b3
svetlagasheva <svetulka_24@mail.bg>
2019-12-02 16:22:24 +0200

    use query for href

--- src/routes/Search/useSearch.js
+++ src/routes/Search/useSearch.js
@@ -2,9 +2,15 @@ const React = require('react');
 const { useServices } = require('stremio/services');
 
 const mapSearchState = (state) => {
-    const selected = state.search.selected && state.search.selected[0] && state.search.selected[0][1] && state.search.selected[0][1].length > 0 ? state.search.selected[0][1] : '';
+    const query = state.search.selected.reduceRight((query, [name, value]) => {
+        if (name === 'search') {
+            return value;
+        }
+        return query;
+    }, '');
+    const selected = state.search.selected;
     const items_groups = state.search.items_groups.map((group) => {
-        group.href = `#/discover/${encodeURIComponent(group.request.base)}/${encodeURIComponent(group.request.path.id)}/${encodeURIComponent(group.request.path.type_name)}?search=${state.search.selected[0][1]}`;
+        group.href = `#/discover/${encodeURIComponent(group.request.base)}/${encodeURIComponent(group.request.path.id)}/${encodeURIComponent(group.request.path.type_name)}?search=${query}`;
         return group;
     });
     return { selected, items_groups };
