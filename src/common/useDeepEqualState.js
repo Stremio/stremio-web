ca8e8e85cc37d3cdc3626b705c891d1202d4480c
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-12-05 14:58:10 +0200

    useDeepEqualState mimic useState api

--- src/common/useDeepEqualState.js
+++ src/common/useDeepEqualState.js
@@ -2,12 +2,21 @@ const React = require('react');
 const isEqual = require('lodash.isequal');
 
 const useDeepEqualState = (initialState) => {
-    return React.useReducer((prevState, nextState) => {
-        return isEqual(prevState, nextState) ?
-            prevState
-            :
-            nextState;
-    }, initialState);
+    return React.useReducer(
+        (prevState, nextState) => {
+            return isEqual(prevState, nextState) ?
+                prevState
+                :
+                nextState;
+        },
+        undefined,
+        () => {
+            return typeof initialState === 'function' ?
+                initialState()
+                :
+                initialState;
+        }
+    );
 };
 
 module.exports = useDeepEqualState;
