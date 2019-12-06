cfe4caa9a156a47f68b5e59ca6809ed2e3b9200f
NikolaBorislavovHristov <hristovnikola@protonmail.com>
2019-12-06 13:03:46 +0200

    board addon names mapped from ctx

--- src/routes/Board/useBoard.js
+++ src/routes/Board/useBoard.js
@@ -6,9 +6,16 @@ const initBoardState = () => ({
     catalog_resources: []
 });
 
-const mapBoardState = (board) => {
+const mapBoardState = (board, ctx) => {
     const selected = board.selected;
     const catalog_resources = board.catalog_resources.map((catalog_resource) => {
+        catalog_resource.addon_name = ctx.content.addons.reduce((addon_name, addon) => {
+            if (addon.transportUrl === catalog_resource.request.base) {
+                return addon.manifest.name;
+            }
+
+            return addon_name;
+        }, catalog_resource.request.base);
         catalog_resource.href = `#/discover/${encodeURIComponent(catalog_resource.request.base)}/${encodeURIComponent(catalog_resource.request.path.type_name)}/${encodeURIComponent(catalog_resource.request.path.id)}`;
         if (catalog_resource.content.type === 'Ready') {
             catalog_resource.content.content.map((metaItem) => {
@@ -34,7 +41,7 @@ const useBoard = () => {
         model: 'board',
         action: loadBoardAction,
         timeout: 1000,
-        map: mapBoardState,
+        mapWithCtx: mapBoardState,
         init: initBoardState
     });
 };
