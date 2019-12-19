81cff243f549950f8b624ce2e51177bb4dc5529f
svetlagasheva <svetulka_24@mail.bg>
2019-12-19 17:50:29 +0200

    vertical nav bar implemented

---
+++ src/common/VerticalNavBar/VerticalNavBar.js
@@ -0,0 +1,41 @@
+const React = require('react');
+const PropTypes = require('prop-types');
+const classnames = require('classnames');
+const NavTabButton = require('../NavBar/NavTabButton');
+const styles = require('./styles');
+
+const VerticalNavBar = React.memo(({ className, tabs }) => {
+    return (
+        <nav className={classnames(className, styles['nav-bar-container'])}>
+            {
+                Array.isArray(tabs) && tabs.length > 0 ?
+                    tabs.map(({ href = '', icon = '', label = '', onClick }) => (
+                        <NavTabButton
+                            key={`${href}${icon}${label}`}
+                            className={styles['nav-tab-button']}
+                            href={href}
+                            icon={icon}
+                            label={label}
+                            onClick={onClick}
+                        />
+                    ))
+                    :
+                    null
+            }
+        </nav>
+    );
+});
+
+VerticalNavBar.displayName = 'VerticalNavBar';
+
+VerticalNavBar.propTypes = {
+    className: PropTypes.string,
+    tabs: PropTypes.arrayOf(PropTypes.shape({
+        icon: PropTypes.string,
+        label: PropTypes.string,
+        href: PropTypes.string,
+        onClick: PropTypes.func
+    }))
+};
+
+module.exports = VerticalNavBar;
