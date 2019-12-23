dc47e8429ba7655e12a5be9e6d61867a146c5ac1
svetlagasheva <svetulka_24@mail.bg>
2019-12-23 18:56:31 +0200

    Vertical nav bar added to storybook

---
+++ storybook/stories/NavBar/VerticalNavBar/VerticalNavBar.js
@@ -0,0 +1,18 @@
+const React = require('react');
+const { storiesOf } = require('@storybook/react');
+const VerticalNavBar = require('stremio/common/VerticalNavBar');
+
+const TABS = [
+    { label: 'Board', icon: 'ic_board' },
+    { label: 'Discover', icon: 'ic_discover' },
+    { label: 'Library', icon: 'ic_library' },
+    { label: 'Calendar', icon: 'ic_calendar' },
+    { label: 'Addons', icon: 'ic_addons' },
+    { label: 'Settings', icon: 'ic_settings' }
+];
+
+storiesOf('NavBar', module).add('VerticalNavBar', () => (
+    <VerticalNavBar
+        tabs={TABS}
+    />
+));
--- storybook/stories/NavBar/index.js
+++ storybook/stories/NavBar/index.js
@@ -1,1 +1,2 @@
 require('./HorizontalNavBar');
+require('./VerticalNavBar');
