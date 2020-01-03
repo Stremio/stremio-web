8c819e92b821f993786c412153f28a5ee817148b
nklhrstv <me@nikolahristov.com>
2020-01-03 09:58:45 +0200

    useSubtitlesSettings hook implemented

---
+++ src/routes/Player/useSubtitlesSettings.js
@@ -0,0 +1,23 @@
+const React = require('react');
+const { useModelState } = require('stremio/common');
+
+const mapSubtitlesSettings = (ctx) => ({
+    size: ctx.content.settings.subtitles_size,
+    text_color: ctx.content.settings.subtitles_text_color,
+    background_color: ctx.content.settings.subtitles_background_color,
+    outline_color: ctx.content.settings.subtitles_outline_color,
+});
+
+const useSubtitlesSettings = () => {
+    const initSubtitlesSettings = React.useCallback(() => {
+        const ctx = core.getState('ctx');
+        return mapSubtitlesSettings(ctx);
+    }, []);
+    return useModelState({
+        model: 'ctx',
+        map: mapSubtitlesSettings,
+        init: initSubtitlesSettings
+    });
+};
+
+module.exports = useSubtitlesSettings;
