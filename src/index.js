ba51eaa879af5b9665b4a7eb4040981027a4e942
svetlagasheva <svetulka_24@mail.bg>
2020-01-24 11:35:14 +0200

    sentry added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const Sentry = require('@sentry/browser');
 const App = require('./App');
+
+Sentry.init({dsn: "https://e47a6d214ae4478b81136c29612003a7@sentry.io/1911455"});
 
 ReactDOM.render(<App />, document.getElementById('app'));
