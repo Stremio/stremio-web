ab75849e6c7b96ed40afc69bc10dcbf7ea6065e7
svetlagasheva <svetulka_24@mail.bg>
2020-01-24 15:12:49 +0200

    sentry dsn exported from package.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const Sentry = require('@sentry/browser');
+const Package = require('../package');
 const App = require('./App');
 
-Sentry.init({dsn: "https://e47a6d214ae4478b81136c29612003a7@sentry.io/1911455"});
+Sentry.init({ dsn: Package.Sentry.dsn });
 
 ReactDOM.render(<App />, document.getElementById('app'));
