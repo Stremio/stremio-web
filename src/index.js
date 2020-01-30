f0d90e9f8b526697d1f5ad9d74b6127a8465b8aa
nklhrstv <me@nikolahristov.com>
2020-01-30 15:46:33 +0200

    sentry confix renamed with lowercase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const Sentry = require('@sentry/browser');
-const Package = require('../package');
+const package = require('../package.json');
 const App = require('./App');
 
-Sentry.init({ dsn: Package.Sentry.dsn });
+Sentry.init(package.sentry);
 
 ReactDOM.render(<App />, document.getElementById('app'));
