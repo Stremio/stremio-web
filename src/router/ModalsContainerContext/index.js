3aa4bc19c8e39988308398f511a59d5dd59cfb1e
nklhrstv <me@nikolahristov.com>
2020-02-01 10:46:15 +0200

    modals context not exported from router package

--- src/router/ModalsContainerContext/index.js
+++ src/router/ModalsContainerContext/index.js
@@ -1,9 +1,7 @@
-const ModalsContainerContext = require('./ModalsContainerContext');
 const ModalsContainerProvider = require('./ModalsContainerProvider');
 const useModalsContainer = require('./useModalsContainer');
 
 module.exports = {
-    ModalsContainerContext,
     ModalsContainerProvider,
     useModalsContainer
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,9 @@
 const { useRouteFocused } = require('./RouteFocusedContext');
 const Modal = require('./Modal');
-const { ModalsContainerContext } = require('./ModalsContainerContext');
 const Router = require('./Router');
 
 module.exports = {
     useRouteFocused,
     Modal,
-    ModalsContainerContext,
     Router
 };
