28b4e1935997cef3018f854e74d43bbb7f433281
nklhrstv <me@nikolahristov.com>
2020-02-12 09:00:01 +0200

    source code link added to settings

--- src/routes/Settings/Settings.js
+++ src/routes/Settings/Settings.js
@@ -224,6 +224,11 @@ const Settings = () => {
                             </Button>
                         </div>
                         <div className={styles['option-container']}>
+                            <Button className={classnames(styles['option-input-container'], styles['link-container'])} title={'Source code'} target={'_blank'} href={'https://github.com/stremio/stremio-web'}>
+                                <div className={styles['label']}>Source code</div>
+                            </Button>
+                        </div>
+                        <div className={styles['option-container']}>
                             <Button className={classnames(styles['option-input-container'], styles['link-container'])} title={'Terms of Service'} target={'_blank'} href={'https://www.stremio.com/tos'}>
                                 <div className={styles['label']}>Terms of Service</div>
                             </Button>
