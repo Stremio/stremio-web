1464044561aacaed9d8580a124e42d09fee7f64c
nklhrstv <me@nikolahristov.com>
2020-02-19 13:19:47 +0200

    layout navbars in board using grid

--- src/routes/Board/Board.js
+++ src/routes/Board/Board.js
@@ -21,65 +21,63 @@ const Board = () => {
                 notificationsMenu={true}
                 navMenu={true}
             />
-            <div className={styles['board-content-container']}>
-                <MainVerticalNavBar className={styles['nav-bar']} route={'board'} />
-                <div className={styles['board-content']}>
-                    {
-                        continueWatching.lib_items.length > 0 ?
-                            <MetaRow
-                                className={styles['board-row']}
-                                title={'Continue Watching'}
-                                items={continueWatching.lib_items.map(({ id, videoId, ...libItem }) => ({
-                                    ...libItem,
-                                    dataset: { id, videoId, type: libItem.type },
-                                    options,
-                                    optionOnSelect
-                                }))}
-                                limit={10}
-                            />
-                            :
-                            null
-                    }
-                    {board.catalog_resources.map((catalog_resource, index) => {
-                        const title = `${catalog_resource.addon_name} - ${catalog_resource.request.path.id} ${catalog_resource.request.path.type_name}`;
-                        switch (catalog_resource.content.type) {
-                            case 'Ready': {
-                                return (
-                                    <MetaRow
-                                        key={index}
-                                        className={styles['board-row']}
-                                        title={title}
-                                        items={catalog_resource.content.content}
-                                        href={catalog_resource.href}
-                                        limit={10}
-                                    />
-                                );
-                            }
-                            case 'Err': {
-                                const message = `Error(${catalog_resource.content.content.type})${typeof catalog_resource.content.content.content === 'string' ? ` - ${catalog_resource.content.content.content}` : ''}`;
-                                return (
-                                    <MetaRow
-                                        key={index}
-                                        className={styles['board-row']}
-                                        title={title}
-                                        message={message}
-                                        limit={10}
-                                    />
-                                );
-                            }
-                            case 'Loading': {
-                                return (
-                                    <MetaRow.Placeholder
-                                        key={index}
-                                        className={styles['board-row-placeholder']}
-                                        title={title}
-                                        limit={10}
-                                    />
-                                );
-                            }
+            <MainVerticalNavBar className={styles['vertical-nav-bar']} route={'board'} />
+            <div className={styles['board-content']}>
+                {
+                    continueWatching.lib_items.length > 0 ?
+                        <MetaRow
+                            className={styles['board-row']}
+                            title={'Continue Watching'}
+                            items={continueWatching.lib_items.map(({ id, videoId, ...libItem }) => ({
+                                ...libItem,
+                                dataset: { id, videoId, type: libItem.type },
+                                options,
+                                optionOnSelect
+                            }))}
+                            limit={10}
+                        />
+                        :
+                        null
+                }
+                {board.catalog_resources.map((catalog_resource, index) => {
+                    const title = `${catalog_resource.addon_name} - ${catalog_resource.request.path.id} ${catalog_resource.request.path.type_name}`;
+                    switch (catalog_resource.content.type) {
+                        case 'Ready': {
+                            return (
+                                <MetaRow
+                                    key={index}
+                                    className={styles['board-row']}
+                                    title={title}
+                                    items={catalog_resource.content.content}
+                                    href={catalog_resource.href}
+                                    limit={10}
+                                />
+                            );
                         }
-                    })}
-                </div>
+                        case 'Err': {
+                            const message = `Error(${catalog_resource.content.content.type})${typeof catalog_resource.content.content.content === 'string' ? ` - ${catalog_resource.content.content.content}` : ''}`;
+                            return (
+                                <MetaRow
+                                    key={index}
+                                    className={styles['board-row']}
+                                    title={title}
+                                    message={message}
+                                    limit={10}
+                                />
+                            );
+                        }
+                        case 'Loading': {
+                            return (
+                                <MetaRow.Placeholder
+                                    key={index}
+                                    className={styles['board-row-placeholder']}
+                                    title={title}
+                                    limit={10}
+                                />
+                            );
+                        }
+                    }
+                })}
             </div>
         </div>
     );
