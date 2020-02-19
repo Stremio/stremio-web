655b8654e4179fec3e2f1e3f108552bf003a582e
nklhrstv <me@nikolahristov.com>
2020-02-19 13:24:38 +0200

    nav bars layout in search using grid

--- src/routes/Search/Search.js
+++ src/routes/Search/Search.js
@@ -31,65 +31,63 @@ const Search = ({ queryParams }) => {
                 notificationsMenu={true}
                 navMenu={true}
             />
-            <div className={styles['search-content-container']}>
-                <MainVerticalNavBar className={styles['nav-bar']} route={'search'} />
-                <div className={styles['search-content']}>
-                    {
-                        search.selected === null || search.selected.extra.every(([name]) => name !== 'search') ?
+            <MainVerticalNavBar className={styles['vertical-nav-bar']} route={'search'} />
+            <div className={styles['search-content']}>
+                {
+                    search.selected === null || search.selected.extra.every(([name]) => name !== 'search') ?
+                        <div className={styles['message-container']}>
+                            <div className={styles['message-content']}>
+                                <Icon className={styles['icon']} icon={'ic_movies'} />
+                                <div className={styles['label']}>Search for movies, series, YouTube and TV channels</div>
+                            </div>
+                            <div className={styles['message-content']}>
+                                <Icon className={styles['icon']} icon={'ic_actor'} />
+                                <div className={styles['label']}>Search for actors, directors and writers</div>
+                            </div>
+                        </div>
+                        :
+                        search.catalog_resources.length === 0 ?
                             <div className={styles['message-container']}>
                                 <div className={styles['message-content']}>
-                                    <Icon className={styles['icon']} icon={'ic_movies'} />
-                                    <div className={styles['label']}>Search for movies, series, YouTube and TV channels</div>
-                                </div>
-                                <div className={styles['message-content']}>
-                                    <Icon className={styles['icon']} icon={'ic_actor'} />
-                                    <div className={styles['label']}>Search for actors, directors and writers</div>
+                                    <div className={styles['label']}> No addons were requested for catalogs</div>
                                 </div>
                             </div>
                             :
-                            search.catalog_resources.length === 0 ?
-                                <div className={styles['message-container']}>
-                                    <div className={styles['message-content']}>
-                                        <div className={styles['label']}> No addons were requested for catalogs</div>
-                                    </div>
-                                </div>
-                                :
-                                search.catalog_resources.map((catalog_resource, index) => {
-                                    switch (catalog_resource.content.type) {
-                                        case 'Ready':
-                                            return (
-                                                <MetaRow
-                                                    key={index}
-                                                    className={styles['search-row']}
-                                                    title={catalog_resource.addon_name}
-                                                    items={catalog_resource.content.content}
-                                                    href={catalog_resource.href}
-                                                    limit={10}
-                                                />
-                                            );
-                                        case 'Err':
-                                            return (
-                                                <MetaRow
-                                                    key={index}
-                                                    className={styles['search-row']}
-                                                    title={catalog_resource.addon_name}
-                                                    message={`Error(${catalog_resource.content.content.type})${typeof catalog_resource.content.content.content === 'string' ? ` - ${catalog_resource.content.content.content}` : ''}`}
-                                                    limit={10}
-                                                />
-                                            );
-                                        case 'Loading':
-                                            return (
-                                                <MetaRow.Placeholder
-                                                    key={index}
-                                                    className={styles['search-row-placeholder']}
-                                                    title={catalog_resource.addon_name}
-                                                    limit={10}
-                                                />
-                                            );
-                                    }
-                                })
-                    }
-                </div>
+                            search.catalog_resources.map((catalog_resource, index) => {
+                                switch (catalog_resource.content.type) {
+                                    case 'Ready':
+                                        return (
+                                            <MetaRow
+                                                key={index}
+                                                className={styles['search-row']}
+                                                title={catalog_resource.addon_name}
+                                                items={catalog_resource.content.content}
+                                                href={catalog_resource.href}
+                                                limit={10}
+                                            />
+                                        );
+                                    case 'Err':
+                                        return (
+                                            <MetaRow
+                                                key={index}
+                                                className={styles['search-row']}
+                                                title={catalog_resource.addon_name}
+                                                message={`Error(${catalog_resource.content.content.type})${typeof catalog_resource.content.content.content === 'string' ? ` - ${catalog_resource.content.content.content}` : ''}`}
+                                                limit={10}
+                                            />
+                                        );
+                                    case 'Loading':
+                                        return (
+                                            <MetaRow.Placeholder
+                                                key={index}
+                                                className={styles['search-row-placeholder']}
+                                                title={catalog_resource.addon_name}
+                                                limit={10}
+                                            />
+                                        );
+                                }
+                            })
+                }
             </div>
         </div>
     );
