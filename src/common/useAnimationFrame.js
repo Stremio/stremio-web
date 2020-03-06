5b9bda680fe7bd2fa20fc21ff619f212f2153a9e
nklhrstv <me@nikolahristov.com>
2020-03-06 13:41:51 +0200

    cancel previous animation frame when request new

--- src/common/useAnimationFrame.js
+++ src/common/useAnimationFrame.js
@@ -2,17 +2,16 @@ const React = require('react');
 
 const useAnimationFrame = () => {
     const animationFrameId = React.useRef(null);
-    const request = React.useCallback((cb) => {
-        if (animationFrameId.current === null) {
-            animationFrameId.current = requestAnimationFrame(() => {
-                cb();
-                animationFrameId.current = null;
-            });
-        }
-    }, []);
     const cancel = React.useCallback(() => {
         cancelAnimationFrame(animationFrameId.current);
         animationFrameId.current = null;
+    }, []);
+    const request = React.useCallback((cb) => {
+        cancel();
+        animationFrameId.current = requestAnimationFrame(() => {
+            cb();
+            animationFrameId.current = null;
+        });
     }, []);
     return [request, cancel];
 };
