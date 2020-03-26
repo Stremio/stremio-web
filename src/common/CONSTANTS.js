2fec01386535d975d5655d3e91450edacbcb4cfc
nklhrstv <me@nikolahristov.com>
2020-03-26 15:50:00 +0200

    CATALOG_PREVIEW_SIZE added to constants

--- src/common/CONSTANTS.js
+++ src/common/CONSTANTS.js
@@ -1,7 +1,9 @@
 const SUBTITLES_SIZES = [75, 100, 125, 150, 175, 200, 250];
 const SUBTITLES_FONTS = ['Roboto', 'Arial', 'Halvetica', 'Times New Roman', 'Verdana', 'Courier', 'Lucida Console', 'sans-serif', 'serif', 'monospace'];
+const CATALOG_PREVIEW_SIZE = 10;
 
 module.exports = {
     SUBTITLES_SIZES,
-    SUBTITLES_FONTS
+    SUBTITLES_FONTS,
+    CATALOG_PREVIEW_SIZE
 };
