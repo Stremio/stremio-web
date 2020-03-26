78bf3baf260f44deb77401ebf25a92512e5a10b1
nklhrstv <me@nikolahristov.com>
2020-03-26 15:50:25 +0200

    MetaRow see all button moved next to title

--- src/common/MetaRow/MetaRow.js
+++ src/common/MetaRow/MetaRow.js
@@ -4,36 +4,22 @@ const classnames = require('classnames');
 const Icon = require('stremio-icons/dom');
 const Button = require('stremio/common/Button');
 const MetaItem = require('stremio/common/MetaItem');
+const CONSTANTS = require('stremio/common/CONSTANTS');
 const MetaRowPlaceholder = require('./MetaRowPlaceholder');
 const styles = require('./styles');
 
-const MetaRow = ({ className, title, message, items, limit, href }) => {
-    items = Array.isArray(items) ? items.slice(0, limit) : [];
+const MetaRow = ({ className, title, message, items, href }) => {
     return (
         <div className={classnames(className, styles['meta-row-container'])}>
             {
-                typeof title === 'string' && title.length > 0 ?
-                    <div className={styles['title-container']} title={title}>{title}</div>
-                    :
-                    null
-            }
-            {
-                typeof message === 'string' && message.length > 0 ?
-                    <div className={styles['message-container']} title={message}>{message}</div>
-                    :
-                    <div className={styles['content-container']}>
-                        <div className={styles['meta-items-container']}>
-                            {items.map((item, index) => (
-                                <MetaItem
-                                    {...item}
-                                    key={index}
-                                    className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${item.posterShape}`])}
-                                />
-                            ))}
-                            {Array(limit - items.length).fill(null).map((_, index) => (
-                                <div key={index} className={classnames(styles['meta-item'], styles['poster-shape-poster'])} />
-                            ))}
-                        </div>
+                (typeof title === 'string' && title.length > 0) || (typeof href === 'string' && href.length > 0) ?
+                    <div className={styles['header-container']}>
+                        {
+                            typeof title === 'string' && title.length > 0 ?
+                                <div className={styles['title-container']} title={title}>{title}</div>
+                                :
+                                null
+                        }
                         {
                             typeof href === 'string' && href.length > 0 ?
                                 <Button className={styles['see-all-container']} title={'SEE ALL'} href={href}>
@@ -43,6 +29,25 @@ const MetaRow = ({ className, title, message, items, limit, href }) => {
                                 :
                                 null
                         }
+                    </div>
+                    :
+                    null
+            }
+            {
+                typeof message === 'string' && message.length > 0 ?
+                    <div className={styles['message-container']} title={message}>{message}</div>
+                    :
+                    <div className={styles['meta-items-container']}>
+                        {items.slice(0, CONSTANTS.CATALOG_PREVIEW_SIZE).map((item, index) => (
+                            <MetaItem
+                                {...item}
+                                key={index}
+                                className={classnames(styles['meta-item'], styles['poster-shape-poster'], styles[`poster-shape-${item.posterShape}`])}
+                            />
+                        ))}
+                        {Array(Math.max(0, CONSTANTS.CATALOG_PREVIEW_SIZE - items.length)).fill(null).map((_, index) => (
+                            <div key={index} className={classnames(styles['meta-item'], styles['poster-shape-poster'])} />
+                        ))}
                     </div>
             }
         </div>
@@ -58,7 +63,6 @@ MetaRow.propTypes = {
     items: PropTypes.arrayOf(PropTypes.shape({
         posterShape: PropTypes.string
     })),
-    limit: PropTypes.number.isRequired,
     href: PropTypes.string
 };
 
