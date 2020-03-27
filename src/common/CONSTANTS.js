6d67840ade0a76a6496d08b82937541524244fad
nklhrstv <me@nikolahristov.com>
2020-03-27 10:41:08 +0200

    CATALOG_PAGE_SIZE constant added

--- src/common/CONSTANTS.js
+++ src/common/CONSTANTS.js
@@ -1,9 +1,11 @@
 const SUBTITLES_SIZES = [75, 100, 125, 150, 175, 200, 250];
 const SUBTITLES_FONTS = ['Roboto', 'Arial', 'Halvetica', 'Times New Roman', 'Verdana', 'Courier', 'Lucida Console', 'sans-serif', 'serif', 'monospace'];
 const CATALOG_PREVIEW_SIZE = 10;
+const CATALOG_PAGE_SIZE = 100;
 
 module.exports = {
     SUBTITLES_SIZES,
     SUBTITLES_FONTS,
-    CATALOG_PREVIEW_SIZE
+    CATALOG_PREVIEW_SIZE,
+    CATALOG_PAGE_SIZE
 };
