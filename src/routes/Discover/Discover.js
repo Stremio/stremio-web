fec92c2957b8a69f11954c3d23b9afd980a56ca7
nklhrstv <me@nikolahristov.com>
2020-03-29 16:08:51 +0300

    discover filters modal styles updated

--- src/routes/Discover/Discover.js
+++ src/routes/Discover/Discover.js
@@ -168,13 +168,13 @@ const Discover = ({ urlParams, queryParams }) => {
                 inputsModalOpen ?
                     <ModalDialog title={'Catalog filters'} className={styles['selectable-inputs-modal-container']} onCloseRequest={closeInputsModal}>
                         {selectInputs.map(({ title, isRequired, options, selected, renderLabelText, onSelect }, index) => (
-                            <div key={index} className={styles['selectable-inputs-container']}>
-                                <div className={styles['select-input-label-container']} title={title}>
+                            <div key={index} className={styles['selectable-input-container']}>
+                                <div className={styles['select-input-label']} title={title}>
                                     {title}
                                     {isRequired ? '*' : null}
                                 </div>
                                 <Multiselect
-                                    className={styles['select-input-container']}
+                                    className={styles['select-input']}
                                     mode={'modal'}
                                     title={title}
                                     options={options}
