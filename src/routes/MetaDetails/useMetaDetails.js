60d9f5266c679b7bcc758e4b7336aee7948caebb
svetlagasheva <svetulka_24@mail.bg>
2020-04-30 15:55:28 +0300

    Date.now() instead of new Date().getTime() used

--- src/routes/MetaDetails/useMetaDetails.js
+++ src/routes/MetaDetails/useMetaDetails.js
@@ -33,7 +33,7 @@ const mapMetaDetailsStateWithCtx = (meta_details, ctx) => {
                                     :
                                     NaN
                             ),
-                            upcoming: new Date(video.released).getTime() > new Date().getTime(),
+                            upcoming: Date.parse(video.released) > Date.now(),
                             // TODO add watched and progress
                             deepLinks: deepLinking.withVideo({
                                 video,
