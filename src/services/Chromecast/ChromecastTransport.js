06b34d31dabdf71eb5e614e547b46e6fff0c2143
nklhrstv <me@nikolahristov.com>
2020-05-18 14:19:09 +0300

    handle error when dispatch message action

--- src/services/Chromecast/ChromecastTransport.js
+++ src/services/Chromecast/ChromecastTransport.js
@@ -242,7 +242,10 @@ function ChromecastTransport() {
                 case 'message': {
                     const castSession = cast.framework.CastContext.getInstance().getCurrentSession();
                     if (castSession) {
-                        castSession.sendMessage(MESSAGE_NAMESPACE, action.message);
+                        castSession.sendMessage(MESSAGE_NAMESPACE, action.message)
+                            .catch((code) => {
+                                onCastError(code);
+                            });
                     }
 
                     return;
