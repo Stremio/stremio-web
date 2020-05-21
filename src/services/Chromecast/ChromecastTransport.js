dd8b30eff0fd4abcc761768a0176514972d4af11
nklhrstv <me@nikolahristov.com>
2020-05-21 18:05:39 +0300

    ChromecastTransport simplified

--- src/services/Chromecast/ChromecastTransport.js
+++ src/services/Chromecast/ChromecastTransport.js
@@ -3,60 +3,6 @@
 const EventEmitter = require('events');
 
 const MESSAGE_NAMESPACE = 'urn:x-cast:com.stremio';
-const CAST_ERROR = {
-    UNKNOWN: {
-        code: 300,
-        message: 'Unknown error'
-    },
-    CANCEL: {
-        code: 301,
-        message: 'The operation was canceled by the user'
-    },
-    TIMEOUT: {
-        code: 302,
-        message: 'The operation timed out'
-    },
-    API_NOT_INITIALIZED: {
-        code: 303,
-        message: 'The API is not initialized'
-    },
-    INVALID_PARAMETER: {
-        code: 304,
-        message: 'The parameters to the operation were not valid'
-    },
-    EXTENSION_NOT_COMPATIBLE: {
-        code: 305,
-        message: 'The API script is not compatible with the installed Cast extension'
-    },
-    EXTENSION_MISSING: {
-        code: 306,
-        message: 'The Cast extension is not available'
-    },
-    RECEIVER_UNAVAILABLE: {
-        code: 307,
-        message: 'No receiver was compatible with the session request'
-    },
-    SESSION_ERROR: {
-        code: 308,
-        message: 'A session could not be created, or a session was invalid'
-    },
-    CHANNEL_ERROR: {
-        code: 309,
-        message: 'A channel to the receiver is not available'
-    },
-    LOAD_MEDIA_FAILED: {
-        code: 310,
-        message: 'Load media failed'
-    },
-    INVALID_MESSAGE: {
-        code: 350,
-        message: 'Invalid message received'
-    },
-    INVALID_OPTIONS: {
-        code: 351,
-        message: 'Cannot start session before cast options are provided'
-    }
-};
 
 function ChromecastTransport() {
     const events = new EventEmitter();
@@ -71,65 +17,8 @@ function ChromecastTransport() {
         onSesstionStateChanged
     );
 
-    function onCastError(code) {
-        switch (code) {
-            case chrome.cast.ErrorCode.CANCEL: {
-                events.emit('error', CAST_ERROR.CANCEL);
-                break;
-            }
-            case chrome.cast.ErrorCode.TIMEOUT: {
-                events.emit('error', CAST_ERROR.TIMEOUT);
-                break;
-            }
-            case chrome.cast.ErrorCode.API_NOT_INITIALIZED: {
-                events.emit('error', CAST_ERROR.API_NOT_INITIALIZED);
-                break;
-            }
-            case chrome.cast.ErrorCode.INVALID_PARAMETER: {
-                events.emit('error', CAST_ERROR.INVALID_PARAMETER);
-                break;
-            }
-            case chrome.cast.ErrorCode.EXTENSION_NOT_COMPATIBLE: {
-                events.emit('error', CAST_ERROR.EXTENSION_NOT_COMPATIBLE);
-                break;
-            }
-            case chrome.cast.ErrorCode.EXTENSION_MISSING: {
-                events.emit('error', CAST_ERROR.EXTENSION_MISSING);
-                break;
-            }
-            case chrome.cast.ErrorCode.RECEIVER_UNAVAILABLE: {
-                events.emit('error', CAST_ERROR.RECEIVER_UNAVAILABLE);
-                break;
-            }
-            case chrome.cast.ErrorCode.SESSION_ERROR: {
-                events.emit('error', CAST_ERROR.SESSION_ERROR);
-                break;
-            }
-            case chrome.cast.ErrorCode.CHANNEL_ERROR: {
-                events.emit('error', CAST_ERROR.CHANNEL_ERROR);
-                break;
-            }
-            case chrome.cast.ErrorCode.LOAD_MEDIA_FAILED: {
-                events.emit('error', CAST_ERROR.LOAD_MEDIA_FAILED);
-                break;
-            }
-            default: {
-                events.emit('error', {
-                    ...CAST_ERROR.UNKNOWN,
-                    error: { code }
-                });
-            }
-        }
-    }
     function onMessage(_, message) {
-        try {
-            events.emit('message', JSON.parse(message));
-        } catch (error) {
-            events.emit('error', {
-                ...CAST_ERROR.INVALID_MESSAGE,
-                error
-            });
-        }
+        events.emit('message', message);
     }
     function onApplicationStatusChanged(event) {
         events.emit(cast.framework.CastSession.APPLICATION_STATUS_CHANGED, event);
@@ -194,38 +83,20 @@ function ChromecastTransport() {
         return null;
     };
     this.setOptions = function(options) {
-        try {
-            cast.framework.CastContext.getInstance().setOptions(options);
-        } catch (error) {
-            events.emit('error', {
-                ...CAST_ERROR.INVALID_OPTIONS,
-                error
-            });
-        }
+        cast.framework.CastContext.getInstance().setOptions(options);
     };
     this.requestSession = function() {
-        try {
-            cast.framework.CastContext.getInstance().requestSession()
-                .catch((code) => {
-                    onCastError(code);
-                });
-        } catch (error) {
-            events.emit('error', {
-                ...CAST_ERROR.INVALID_OPTIONS,
-                error
-            });
-        }
+        cast.framework.CastContext.getInstance().requestSession();
     };
     this.endCurrentSession = function(stopCasting) {
         cast.framework.CastContext.getInstance().endCurrentSession(stopCasting);
     };
-    this.sendMessage = function(data) {
+    this.sendMessage = function(message) {
         const castSession = cast.framework.CastContext.getInstance().getCurrentSession();
         if (castSession !== null) {
-            castSession.sendMessage(MESSAGE_NAMESPACE, JSON.stringify(data))
-                .catch((code) => {
-                    onCastError(code);
-                });
+            return castSession.sendMessage(MESSAGE_NAMESPACE, message);
+        } else {
+            return Promise.reject(new Error('Session not started'));
         }
     };
 }
