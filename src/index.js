edf8dbf66cc7336fdea827db740826fad822581d
nklhrstv <me@nikolahristov.com>
2021-01-15 15:40:15 +0200

    condigure sentry before import app code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // Copyright (C) 2017-2020 Smart code 203358507
 
-const React = require('react');
-const ReactDOM = require('react-dom');
-const App = require('./App');
-
 if (typeof process.env.SENTRY_DSN === 'string') {
     const Sentry = require('@sentry/browser');
     Sentry.init({ dsn: process.env.SENTRY_DSN });
 }
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const App = require('./App');
 
 ReactDOM.render(<App />, document.getElementById('app'));
