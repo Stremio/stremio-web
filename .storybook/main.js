202c44da92748373315278121a0a723ee9f99655
svetlagasheva <svetulka_24@mail.bg>
2021-05-10 19:20:47 +0300

    indent fixed

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,34 +4,34 @@ const appConfig = require('../webpack.config.js')({}, { mode: 'development' });
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  webpackFinal: async (storybookConfig) => {
-    storybookConfig.module = {
-      ...storybookConfig.module,
-      rules: [
-        ...storybookConfig.module.rules,
-        ...appConfig.module.rules
-      ]
-    };
-    storybookConfig.resolve = {
-      ...storybookConfig.resolve,
-      extensions: [
-        ...storybookConfig.resolve.extensions,
-        ...appConfig.resolve.extensions
-      ],
-      alias: {
-        ...storybookConfig.resolve.alias,
-        ...appConfig.resolve.alias
-      }
-    };
-    storybookConfig.plugins = [
-      ...storybookConfig.plugins,
-      new MiniCssExtractPlugin()
-    ];
+    webpackFinal: async (storybookConfig) => {
+        storybookConfig.module = {
+            ...storybookConfig.module,
+            rules: [
+                ...storybookConfig.module.rules,
+                ...appConfig.module.rules
+            ]
+        };
+        storybookConfig.resolve = {
+            ...storybookConfig.resolve,
+            extensions: [
+                ...storybookConfig.resolve.extensions,
+                ...appConfig.resolve.extensions
+            ],
+            alias: {
+                ...storybookConfig.resolve.alias,
+                ...appConfig.resolve.alias
+            }
+        };
+        storybookConfig.plugins = [
+            ...storybookConfig.plugins,
+            new MiniCssExtractPlugin()
+        ];
 
-    return storybookConfig;
-  },
-  core: {
-    builder: 'webpack5',
-  },
-  stories: ['../src/stories/**/*.@(js)'],
+        return storybookConfig;
+    },
+    core: {
+        builder: 'webpack5',
+    },
+    stories: ['../src/stories/**/*.@(js)'],
 };
