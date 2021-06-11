05c48a043efe3455f886dc3ffd1cb2a0f841fad5
Tim <tymmesyde@gmail.com>
2021-06-11 22:29:31 +0200

    fix lint errors

--- src/common/ShimmerEffect/ShimmerEffect.js
+++ src/common/ShimmerEffect/ShimmerEffect.js
@@ -1,9 +1,14 @@
 const React = require('react');
+const PropTypes = require('prop-types');
 const classnames = require('classnames');
 const styles = require('./styles');
 
 const ShimmerEffect = ({ className, ...props }) => {
     return <div className={classnames(className, styles['shimmer'])} {...props}/>;
+};
+
+ShimmerEffect.propTypes = {
+    className: PropTypes.string,
 };
 
 module.exports = ShimmerEffect;
