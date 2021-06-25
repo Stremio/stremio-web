255cd6be16d48ac77f8a73edf6a941683b8c45a1
svetlagasheva <svetulka_24@mail.bg>
2021-06-25 15:58:50 +0300

    ServerNotification rendered in Board

--- src/routes/Board/Board.js
+++ src/routes/Board/Board.js
@@ -2,7 +2,7 @@
 
 const React = require('react');
 const classnames = require('classnames');
-const { MainNavBars, MetaRow, LibItem, MetaItem } = require('stremio/common');
+const { MainNavBars, MetaRow, LibItem, MetaItem, ServerNotification } = require('stremio/common');
 const useBoard = require('./useBoard');
 const useContinueWatchingPreview = require('./useContinueWatchingPreview');
 const styles = require('./styles');
@@ -11,59 +11,62 @@ const Board = () => {
     const board = useBoard();
     const continueWatchingPreview = useContinueWatchingPreview();
     return (
-        <MainNavBars className={styles['board-container']} route={'board'}>
-            <div className={styles['board-content']}>
-                {
-                    continueWatchingPreview.libraryItems.length > 0 ?
-                        <MetaRow
-                            className={classnames(styles['board-row'], styles['continue-watching-row'])}
-                            title={'Continue Watching'}
-                            items={continueWatchingPreview.libraryItems}
-                            itemComponent={LibItem}
-                            deepLinks={continueWatchingPreview.deepLinks}
-                        />
-                        :
-                        null
-                }
-                {board.catalogs.map((catalog, index) => {
-                    switch (catalog.content.type) {
-                        case 'Ready': {
-                            return (
-                                <MetaRow
-                                    key={index}
-                                    className={classnames(styles['board-row'], styles[`board-row-${catalog.content.content[0].posterShape}`])}
-                                    title={catalog.title}
-                                    items={catalog.content.content}
-                                    itemComponent={MetaItem}
-                                    deepLinks={catalog.deepLinks}
-                                />
-                            );
-                        }
-                        case 'Err': {
-                            return (
-                                <MetaRow
-                                    key={index}
-                                    className={styles['board-row']}
-                                    title={catalog.title}
-                                    message={catalog.content.content}
-                                    deepLinks={catalog.deepLinks}
-                                />
-                            );
-                        }
-                        case 'Loading': {
-                            return (
-                                <MetaRow.Placeholder
-                                    key={index}
-                                    className={classnames(styles['board-row'], styles['board-row-poster'])}
-                                    title={catalog.title}
-                                    deepLinks={catalog.deepLinks}
-                                />
-                            );
-                        }
+        <div className={styles['board-content-container']}>
+            <MainNavBars className={styles['board-container']} route={'board'}>
+                <div className={styles['board-content']}>
+                    {
+                        continueWatchingPreview.libraryItems.length > 0 ?
+                            <MetaRow
+                                className={classnames(styles['board-row'], styles['continue-watching-row'])}
+                                title={'Continue Watching'}
+                                items={continueWatchingPreview.libraryItems}
+                                itemComponent={LibItem}
+                                deepLinks={continueWatchingPreview.deepLinks}
+                            />
+                            :
+                            null
                     }
-                })}
-            </div>
-        </MainNavBars>
+                    {board.catalogs.map((catalog, index) => {
+                        switch (catalog.content.type) {
+                            case 'Ready': {
+                                return (
+                                    <MetaRow
+                                        key={index}
+                                        className={classnames(styles['board-row'], styles[`board-row-${catalog.content.content[0].posterShape}`])}
+                                        title={catalog.title}
+                                        items={catalog.content.content}
+                                        itemComponent={MetaItem}
+                                        deepLinks={catalog.deepLinks}
+                                    />
+                                );
+                            }
+                            case 'Err': {
+                                return (
+                                    <MetaRow
+                                        key={index}
+                                        className={styles['board-row']}
+                                        title={catalog.title}
+                                        message={catalog.content.content}
+                                        deepLinks={catalog.deepLinks}
+                                    />
+                                );
+                            }
+                            case 'Loading': {
+                                return (
+                                    <MetaRow.Placeholder
+                                        key={index}
+                                        className={classnames(styles['board-row'], styles['board-row-poster'])}
+                                        title={catalog.title}
+                                        deepLinks={catalog.deepLinks}
+                                    />
+                                );
+                            }
+                        }
+                    })}
+                </div>
+            </MainNavBars>
+            <ServerNotification />
+        </div>
     );
 };
 
