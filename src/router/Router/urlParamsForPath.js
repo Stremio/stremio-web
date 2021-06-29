4e7303c9cf1257621fbbb0977cffe6efdaed1fbb
abdev <ab.inpathtoadev@gmail.com>
2021-06-29 23:45:24 +0530

    urlparam exception for movies fixed

--- src/router/Router/urlParamsForPath.js
+++ src/router/Router/urlParamsForPath.js
@@ -2,13 +2,17 @@
 
 const urlParamsForPath = (routeConfig, path) => {
     const matches = path.match(routeConfig.regexp);
+
+    if (matches[1] === 'movie' && typeof matches[3] != 'string') {
+        matches[3] = matches[2]
+    }
+
     return routeConfig.urlParamsNames.reduce((urlParams, name, index) => {
         if (Array.isArray(matches) && typeof matches[index + 1] === 'string') {
             urlParams[name] = decodeURIComponent(matches[index + 1]);
         } else {
             urlParams[name] = null;
         }
-
         return urlParams;
     }, { path });
 };
