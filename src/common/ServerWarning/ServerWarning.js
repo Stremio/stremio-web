245956d5100cb676c0f9a45a52571e8483eea925
svetlagasheva <svetulka_24@mail.bg>
2021-07-21 18:24:33 +0300

    ServerWarning onLaterClick/onDismissedClick func added

--- src/common/ServerWarning/ServerWarning.js
+++ src/common/ServerWarning/ServerWarning.js
@@ -1,39 +1,48 @@
 // Copyright (C) 2017-2020 Smart code 203358507
 
 const React = require('react');
-// const { useServices } = require('stremio/services');
+const { useServices } = require('stremio/services');
 const PropTypes = require('prop-types');
 const classnames = require('classnames');
 const Button = require('stremio/common/Button');
+const useProfile = require('stremio/common/useProfile');
 const styles = require('./styles');
 
 const ServerWarning = ({ className }) => {
-    // const { core } = useServices();
-    const RewindServerWarning = React.useCallback(() => {
-        console.log('later');
-        // core.transport.dispatch({
-        //     action: 'Ctx',
-        //     args: {
-        //         action: 'RewindServerWarning',
-        //     }
-        // });
-    }, []);
-    const DismissServerWarning = React.useCallback(() => {
-        console.log('dismiss');
-        // core.transport.dispatch({
-        //     action: 'Ctx',
-        //     args: {
-        //         action: 'DismissServerWarning',
-        //     }
-        // });
-    }, []);
+    const { core } = useServices();
+    const profile = useProfile();
+    const streamingServerWarningDismissed = new Date();
+    const onLaterClick = React.useCallback(() => {
+        core.transport.dispatch({
+            action: 'Ctx',
+            args: {
+                action: 'UpdateSettings',
+                args: {
+                    ...profile.settings,
+                    streamingServerWarningDismissed: streamingServerWarningDismissed.setMonth(new Date().getMonth() + 1)
+                }
+            }
+        });
+    }, [profile.settings]);
+    const onDismissedClick = React.useCallback(() => {
+        core.transport.dispatch({
+            action: 'Ctx',
+            args: {
+                action: 'UpdateSettings',
+                args: {
+                    ...profile.settings,
+                    streamingServerWarningDismissed: streamingServerWarningDismissed.setFullYear(new Date().getFullYear() + 50)
+                }
+            }
+        });
+    }, [profile.settings]);
     return (
         <div className={classnames(className, styles['warning-container'])}>
             <div className={styles['warning-statement']}>Streaming server must be available.</div>
-            <Button className={styles['warning-button']} title={'later'} onClick={RewindServerWarning}>
+            <Button className={styles['warning-button']} title={'later'} onClick={onLaterClick}>
                 <span className={styles['warning-label']}>Later</span>
             </Button>
-            <Button className={styles['warning-button']} title={'dismiss'} onClick={DismissServerWarning}>
+            <Button className={styles['warning-button']} title={'dismiss'} onClick={onDismissedClick}>
                 <span className={styles['warning-label']}>Dismiss</span>
             </Button>
         </div>
