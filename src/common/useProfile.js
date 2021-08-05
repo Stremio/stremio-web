76042a5893297075ca5cea4fa1bd992fbf209e36
svetlagasheva <svetulka_24@mail.bg>
2021-08-05 13:11:58 +0300

    useProfile map func modified

--- src/common/useProfile.js
+++ src/common/useProfile.js
@@ -4,7 +4,18 @@ const React = require('react');
 const { useServices } = require('stremio/services');
 const useModelState = require('stremio/common/useModelState');
 
-const map = (ctx) => ctx.profile;
+const map = (ctx) => ({
+    ...ctx.profile,
+    settings: {
+        ...ctx.profile.settings,
+        streamingServerWarningDismissed: new Date(
+            typeof ctx.profile.settings.streamingServerWarningDismissed === 'string' ?
+                ctx.profile.settings.streamingServerWarningDismissed
+                :
+                NaN
+        )
+    }
+});
 
 const useProfile = () => {
     const { core } = useServices();
