099c799d670e1286fe0fd56f034e6284aab7ee4a
nklhrstv <me@nikolahristov.com>
2021-10-05 14:30:14 +0300

    write test for copyright headers

---
+++ tests/copyright.spec.js
@@ -0,0 +1,15 @@
+// Copyright (C) 2017-2020 Smart code 203358507
+
+const fs = require('fs');
+const readdirp = require('readdirp');
+
+const COPYRIGHT_HEADER = /^\/\/ Copyright \(C\) 2017-2020 Smart code 203358507.*/;
+
+describe('copyright', () => {
+    test('js', async () => {
+        for await (const { fullPath } of readdirp('src', { fileFilter: '*.js' })) {
+            const content = await fs.promises.readFile(fullPath, 'utf8');
+            expect(content).toEqual(expect.stringMatching(COPYRIGHT_HEADER));
+        }
+    });
+});
