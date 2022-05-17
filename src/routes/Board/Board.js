24589034df072b5ae69d2425936ee8f070470535
nklhrstv <me@nikolahristov.com>
2022-05-17 15:56:17 +0300

    switch to react-virtual

--- src/routes/Board/Board.js
+++ src/routes/Board/Board.js
@@ -2,96 +2,66 @@
 
 const React = require('react');
 const classnames = require('classnames');
-const { AutoSizer, List, CellMeasurer, CellMeasurerCache } = require('react-virtualized');
+const { useVirtual } = require('react-virtual');
 const { MainNavBars, MetaRow, LibItem, MetaItem, StreamingServerWarning, useProfile, useStreamingServer } = require('stremio/common');
 const useBoard = require('./useBoard');
 const useContinueWatchingPreview = require('./useContinueWatchingPreview');
 const styles = require('./styles');
 
-const withMeasure = (Component) => {
-    return React.forwardRef(function withMeasure({ measure, ...props }, ref) {
-        React.useLayoutEffect(() => {
-            measure();
-        }, [measure]);
-        return (
-            <Component {...props} ref={ref} />
-        );
-    });
-};
-
-const MetaRowWithMeasure = withMeasure(MetaRow);
-const MetaRowPlaceholderWithMeasure = withMeasure(MetaRow.Placeholder);
-
 const Board = () => {
-    const board = useBoard();
     const profile = useProfile();
     const streamingServer = useStreamingServer();
+    const board = useBoard();
     const continueWatchingPreview = useContinueWatchingPreview();
-    const cache = React.useMemo(() => new CellMeasurerCache({
-        fixedWidth: true
-    }), []);
+    const boardCatalogsOffset = continueWatchingPreview.libraryItems.length > 0 ? 1 : 0;
+    const scrollContainerRef = React.useRef();
+    const virtualizer = useVirtual({
+        size: board.catalogs.length + boardCatalogsOffset,
+        parentRef: scrollContainerRef,
+    });
     return (
         <div className={styles['board-container']}>
             <MainNavBars className={styles['board-content-container']} route={'board'}>
-                <div className={styles['board-content']}>
-                    <AutoSizer>
-                        {({ width, height }) => (
-                            <List
-                                deferredMeasurementCache={cache}
-                                width={width}
-                                height={height}
-                                rowCount={board.catalogs.length}
-                                rowHeight={cache.rowHeight}
-                                onRowsRendered={(args) => console.log('onRowsRendered', args)}
-                                rowRenderer={({ index, key, style, parent }) => {
-                                    const catalog = board.catalogs[index];
-                                    return (
-                                        <CellMeasurer
-                                            key={key}
-                                            parent={parent}
-                                            cache={cache}
-                                            rowIndex={index}
-                                            columnIndex={0}
-                                        >
-                                            {({ registerChild, measure }) => (
-                                                catalog.content.type === 'Ready' ?
-                                                    <MetaRowWithMeasure
-                                                        ref={registerChild}
-                                                        measure={measure}
-                                                        style={style}
-                                                        className={classnames(styles['board-row'], styles[`board-row-${catalog.content.content[0].posterShape}`])}
-                                                        title={catalog.title}
-                                                        items={catalog.content.content}
-                                                        itemComponent={MetaItem}
-                                                        deepLinks={catalog.deepLinks}
-                                                    />
-                                                    :
-                                                    catalog.content.type === 'Err' ?
-                                                        <MetaRowWithMeasure
-                                                            ref={registerChild}
-                                                            measure={measure}
-                                                            style={style}
-                                                            className={styles['board-row']}
-                                                            title={catalog.title}
-                                                            message={catalog.content.content}
-                                                            deepLinks={catalog.deepLinks}
-                                                        />
-                                                        :
-                                                        <MetaRowPlaceholderWithMeasure
-                                                            ref={registerChild}
-                                                            measure={measure}
-                                                            style={style}
-                                                            className={classnames(styles['board-row'], styles['board-row-poster'])}
-                                                            title={catalog.title}
-                                                            deepLinks={catalog.deepLinks}
-                                                        />
-                                            )}
-                                        </CellMeasurer>
-                                    );
-                                }}
-                            />
-                        )}
-                    </AutoSizer>
+                <div ref={scrollContainerRef} className={styles['board-content']}>
+                    <div style={{ height: virtualizer.totalSize }} className={styles['board-rows-window']}>
+                        {virtualizer.virtualItems.map(({ index, measureRef, start }) => (
+                            <div key={index} ref={measureRef} style={{ transform: `translateY(${start}px)` }} className={styles['board-row-container']}>
+                                {
+                                    index === 0 && continueWatchingPreview.libraryItems.length > 0 ?
+                                        <MetaRow
+                                            className={classnames(styles['board-row'], styles['continue-watching-row'])}
+                                            title={'Continue Watching'}
+                                            items={continueWatchingPreview.libraryItems}
+                                            itemComponent={LibItem}
+                                            deepLinks={continueWatchingPreview.deepLinks}
+                                        />
+                                        :
+                                        board.catalogs[index - boardCatalogsOffset].content.type === 'Ready' ?
+                                            <MetaRow
+                                                className={classnames(styles['board-row'], styles[`board-row-${board.catalogs[index - boardCatalogsOffset].content.content[0].posterShape}`])}
+                                                title={board.catalogs[index - boardCatalogsOffset].title}
+                                                items={board.catalogs[index - boardCatalogsOffset].content.content}
+                                                itemComponent={MetaItem}
+                                                deepLinks={board.catalogs[index - boardCatalogsOffset].deepLinks}
+                                            />
+                                            :
+                                            board.catalogs[index - boardCatalogsOffset].content.type === 'Err' ?
+                                                <MetaRow
+                                                    className={styles['board-row']}
+                                                    title={board.catalogs[index - boardCatalogsOffset].title}
+                                                    message={board.catalogs[index - boardCatalogsOffset].content.content}
+                                                    deepLinks={board.catalogs[index - boardCatalogsOffset].deepLinks}
+                                                />
+                                                :
+                                                <MetaRow.Placeholder
+                                                    className={classnames(styles['board-row'], styles['board-row-poster'])}
+                                                    title={board.catalogs[index - boardCatalogsOffset].title}
+                                                    deepLinks={board.catalogs[index - boardCatalogsOffset].deepLinks}
+                                                />
+                                }
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </MainNavBars>
             {
