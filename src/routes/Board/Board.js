3e14913e20f2d892b445487699aa95799d1c074c
Alexandru Branza <branza.alex@gmail.com>
2022-05-30 20:53:05 +0300

    POC: Board Infinite Scroll

--- src/routes/Board/Board.js
+++ src/routes/Board/Board.js
@@ -2,7 +2,7 @@
 
 const React = require('react');
 const classnames = require('classnames');
-const debounce = require('lodash.debounce');
+const throttle = require('lodash.throttle');
 const { MainNavBars, MetaRow, LibItem, MetaItem, StreamingServerWarning, useProfile, useStreamingServer, getVisibleChildrenRange } = require('stremio/common');
 const useBoard = require('./useBoard');
 const useContinueWatchingPreview = require('./useContinueWatchingPreview');
@@ -15,24 +15,31 @@ const Board = () => {
     const streamingServer = useStreamingServer();
     const continueWatchingPreview = useContinueWatchingPreview();
     const [board, loadBoardRows] = useBoard();
+    const [loadedCatalogs, setLoadedCatalogs] = React.useState(false);
     const boardCatalogsOffset = continueWatchingPreview.libraryItems.length > 0 ? 1 : 0;
     const scrollContainerRef = React.useRef();
     const onVisibleRangeChange = React.useCallback(() => {
+        if (loadedCatalogs) {
+            return;
+        }
         const range = getVisibleChildrenRange(scrollContainerRef.current, THRESHOLD);
         if (range === null) {
             return;
         }
 
-        const start = Math.max(0, range.start - boardCatalogsOffset);
+        range.end += 8;
+        let start = Math.max(0, range.start - boardCatalogsOffset);
         const end = range.end - boardCatalogsOffset;
         if (end < start) {
             return;
         }
 
-        loadBoardRows({ start, end });
-    }, [boardCatalogsOffset]);
-    const onScroll = React.useCallback(debounce(onVisibleRangeChange, 250), [onVisibleRangeChange]);
+        for (let i = start; i <= end - 2; i += 2)
+            loadBoardRows({ start: i, end: i + 2 });
+    }, [loadedCatalogs, boardCatalogsOffset]);
+    const onScroll = React.useCallback(throttle(onVisibleRangeChange, 250), [onVisibleRangeChange]);
     React.useLayoutEffect(() => {
+        setLoadedCatalogs(board?.catalogs.filter(catalog => ['Ready','Err'].includes(catalog?.content?.type)).length === board?.catalogs?.length);
         onVisibleRangeChange();
     }, [board.catalogs, onVisibleRangeChange]);
     return (
@@ -65,29 +72,11 @@ const Board = () => {
                                     />
                                 );
                             }
-                            case 'Err': {
-                                return (
-                                    <MetaRow
-                                        key={index}
-                                        className={styles['board-row']}
-                                        title={catalog.title}
-                                        message={catalog.content.content}
-                                        deepLinks={catalog.deepLinks}
-                                    />
-                                );
-                            }
-                            default: {
-                                return (
-                                    <MetaRow.Placeholder
-                                        key={index}
-                                        className={classnames(styles['board-row'], styles['board-row-poster'])}
-                                        title={catalog.title}
-                                        deepLinks={catalog.deepLinks}
-                                    />
-                                );
-                            }
                         }
                     })}
+                    <div style={{ height: '70%', textAlign: 'center', display: loadedCatalogs ? 'none' : 'block' }}>
+                        <span className={styles['blinker']}>Loding catalogs</span>
+                    </div>
                 </div>
             </MainNavBars>
             {
