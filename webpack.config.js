d2e5b45702c9ed5614ca2abb4fa473c0ca086a67
nklhrstv <me@nikolahristov.com>
2022-08-05 16:00:05 +0300

    remove plugin-transform-runtime

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,7 @@ module.exports = (env, argv) => ({
                         ],
                         plugins: [
                             '@babel/plugin-proposal-class-properties',
-                            '@babel/plugin-proposal-object-rest-spread',
-                            '@babel/plugin-transform-runtime'
+                            '@babel/plugin-proposal-object-rest-spread'
                         ]
                     }
                 }
