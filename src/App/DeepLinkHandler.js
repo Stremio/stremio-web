3c28f671c5ff012f34ea76637a4b1e6c1dcdbe13
nklhrstv <me@nikolahristov.com>
2022-10-19 14:55:28 +0300

    code refactor in DeepLinkHandler

--- src/App/DeepLinkHandler.js
+++ src/App/DeepLinkHandler.js
@@ -4,10 +4,13 @@ const { withCoreSuspender, useStreamingServer } = require('stremio/common');
 const DeepLinkHandler = () => {
     const streamingServer = useStreamingServer();
     React.useEffect(() => {
-        if (streamingServer.torrent && streamingServer.torrent[1].type === 'Ready') {
-            const [_, deepLinks] = streamingServer.torrent[1].content;
-            if (typeof deepLinks.metaDetailsVideos === 'string') {
-                window.location = deepLinks.metaDetailsVideos;
+        if (streamingServer.torrent !== null) {
+            const [, { type, content }] = streamingServer.torrent;
+            if (type === 'Ready') {
+                const [, deepLinks] = content;
+                if (typeof deepLinks.metaDetailsVideos === 'string') {
+                    window.location = deepLinks.metaDetailsVideos;
+                }
             }
         }
     }, [streamingServer.torrent]);
