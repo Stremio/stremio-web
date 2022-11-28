796ec387aa653cdb1008208f815fb5fcc578736c
Tim <tymmesyde@gmail.com>
2022-11-28 11:30:43 +0100

    refactor(Player): incorrect naming for NextVideoPopup index

--- src/routes/Player/NextVideoPopup/index.js
+++ src/routes/Player/NextVideoPopup/index.js
@@ -1,5 +1,5 @@
 // Copyright (C) 2017-2022 Smart code 203358507
 
-const NextEpisodeModal = require('./NextVideoPopup');
+const NextVideoPopup = require('./NextVideoPopup');
 
-module.exports = NextEpisodeModal;
+module.exports = NextVideoPopup;
