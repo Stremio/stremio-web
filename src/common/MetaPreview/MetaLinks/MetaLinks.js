445b0934cb19e08e4299dd7564ce596d0fbf9309
Tim <tymmesyde@gmail.com>
2022-12-16 02:14:57 +0100

    refactor(MetaLinks): translate buttons

--- src/common/MetaPreview/MetaLinks/MetaLinks.js
+++ src/common/MetaPreview/MetaLinks/MetaLinks.js
@@ -3,10 +3,12 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 const classnames = require('classnames');
+const { useTranslation } = require('react-i18next');
 const Button = require('stremio/common/Button');
 const styles = require('./styles');
 
 const MetaLinks = ({ className, label, links }) => {
+    const { t } = useTranslation();
     return (
         <div className={classnames(className, styles['meta-links-container'])}>
             {
@@ -20,7 +22,7 @@ const MetaLinks = ({ className, label, links }) => {
                     <div className={styles['links-container']}>
                         {links.map(({ label, href }, index) => (
                             <Button key={index} className={styles['link-container']} title={label} href={href}>
-                                {label}
+                                { t(label) }
                             </Button>
                         ))}
                     </div>
