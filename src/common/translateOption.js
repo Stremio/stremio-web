32a7117493418d335de092107cfc7b20160f1486
Tim <tymmesyde@gmail.com>
2023-04-12 13:36:10 +0200

    refactor(translateOption): use Sleeyax suggestion

--- src/common/translateOption.js
+++ src/common/translateOption.js
@@ -4,15 +4,12 @@ const { t } = require('i18next');
 
 const translateOption = (option, translateKeyPrefix = '') => {
     const translateKey = `${translateKeyPrefix}${option}`;
-    const translateKeyUppercase = translateKey.toUpperCase();
-    const translateValue = t(translateKey);
-    const translateValueUppercase = t(translateKeyUppercase);
-    if (translateKey !== translateValue) {
-        return translateValue;
-    } else if (translateKeyUppercase !== translateValueUppercase) {
-        return translateValueUppercase;
-    }
-    return option.charAt(0).toUpperCase() + option.slice(1);
+    const translateValue = t(translateKey, {
+        defaultValue: t(translateKey.toUpperCase(), {
+            defaultValue: null
+        })
+    });
+    return translateValue ?? option.charAt(0).toUpperCase() + option.slice(1);
 };
 
 module.exports = translateOption;
