1e98fc2a974dd590541d0ce13c92afd70cf6b8e2
Christopher <Christopher.bartos@dawsoncollege.qc.ca>
2023-05-15 21:58:41 -0400

    fix(AvatarImage): removed null in navmenu

--- src/common/NavBar/HorizontalNavBar/NavMenu/NavMenuContent.js
+++ src/common/NavBar/HorizontalNavBar/NavMenu/NavMenuContent.js
@@ -44,7 +44,10 @@ const NavMenuContent = ({ onClick }) => {
                         backgroundImage: profile.auth === null ?
                             `url('${require('/images/anonymous.png')}')`
                             :
-                            `url('${profile.auth.user.avatar}'), url('${require('/images/default_avatar.png')}')`
+                            profile.auth.user.avatar ?
+                            `url('${profile.auth.user.avatar}')`
+                            :
+                            `url('${require('/images/default_avatar.png')}')`
                     }}
                 />
                 <div className={styles['email-container']}>
