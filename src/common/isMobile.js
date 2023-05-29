c9347885e0c43b1e087db4ab7bf5db24bca53045
Alexandru Branza <branza.alex@gmail.com>
2023-05-29 17:38:13 +0300

    Use Bowser to Detect Mobile

--- src/common/isMobile.js
+++ src/common/isMobile.js
@@ -1,24 +1,11 @@
 // Copyright (C) 2017-2022 Smart code 203358507
 
-function iOS() {
-    return [
-        'iPad Simulator',
-        'iPhone Simulator',
-        'iPod Simulator',
-        'iPad',
-        'iPhone',
-        'iPod'
-    ].includes(navigator.platform)
-    || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
-}
+const Bowser = require('bowser');
 
-function android() {
-    const ua = navigator.userAgent.toLowerCase();
-    return ua.indexOf('android') > -1;
-}
+const browser = Bowser.parse(window.navigator?.userAgent || '');
 
 module.exports = () => {
-    if (iOS()) return 'ios';
-    if (android()) return 'android';
+    if (browser?.os?.name === 'iOS') return 'ios';
+    if (browser?.os?.name === 'Android') return 'android';
     return false;
 };
