32aee9de6ad5a3721418abcb186d889902137d95
Tim <tymmesyde@gmail.com>
2023-06-09 19:16:32 +0200

    refactor: update copyright header year

--- src/common/AddonDetailsModal/useAddonDetails.js
+++ src/common/AddonDetailsModal/useAddonDetails.js
@@ -1,4 +1,4 @@
-// Copyright (C) 2017-2022 Smart code 203358507
+// Copyright (C) 2017-2023 Smart code 203358507
 
 const React = require('react');
 const useModelState = require('stremio/common/useModelState');
