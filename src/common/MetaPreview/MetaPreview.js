b669fe08fca3f5b6f83ce73bc1245a9140548418
kKaskak <117831817+kKaskak@users.noreply.github.com>
2023-10-03 12:41:00 +0300

    Revert "Revert "stream info design update""
    This reverts commit 22b8a54c4a9f255dd25c485e8a60f88f89d27194.

--- src/common/MetaPreview/MetaPreview.js
+++ src/common/MetaPreview/MetaPreview.js
@@ -194,34 +194,50 @@ const MetaPreview = ({ className, compact, name, logo, background, runtime, rele
                 }
             </div>
             <div className={styles['action-buttons-container']}>
-                {
-                    typeof toggleInLibrary === 'function' ?
-                        <ActionButton
-                            className={styles['action-button']}
-                            icon={inLibrary ? 'remove-from-library' : 'add-to-library'}
-                            label={compact ? null : inLibrary ? t('REMOVE_FROM_LIB') : t('ADD_TO_LIB')}
-                            tabIndex={compact ? -1 : 0}
-                            onClick={toggleInLibrary}
-                        />
-                        :
-                        null
-                }
-                {
-                    typeof trailerHref === 'string' ?
-                        <ActionButton
-                            className={styles['action-button']}
-                            icon={'trailer'}
-                            label={compact ? null : t('TRAILER')}
-                            tabIndex={compact ? -1 : 0}
-                            href={trailerHref}
-                        />
-                        :
-                        null
-                }
+                <div className={styles['action-button-wrapper']}>
+                    {
+                        typeof toggleInLibrary === 'function' ?
+                            <ActionButton
+                                className={styles['action-button']}
+                                icon={inLibrary ? 'remove-from-library' : 'add-to-library'}
+                                label={compact ? null : inLibrary ? t('REMOVE_FROM_LIB') : t('ADD_TO_LIB')}
+                                tabIndex={compact ? -1 : 0}
+                                onClick={toggleInLibrary}
+                            />
+                            :
+                            null
+                    }
+                    {
+                        typeof toggleInLibrary === 'function' && compact ?
+                            <div className={styles['label']}>{inLibrary ? t('REMOVE_FROM_LIB') : t('ADD_TO_LIB')}</div>
+                            :
+                            null
+                    }
+                </div>
+                <div className={styles['action-button-wrapper']}>
+                    {
+                        typeof trailerHref === 'string' ?
+                            <ActionButton
+                                className={styles['action-button']}
+                                icon={'trailer'}
+                                label={compact ? null : t('TRAILER')}
+                                tabIndex={compact ? -1 : 0}
+                                href={trailerHref}
+                            />
+                            :
+                            null
+                    }
+                    {
+                        typeof trailerHref === 'string' && compact ?
+                            <div className={styles['label']}>{t('WATCH_TRAILER')}</div>
+                            :
+                            null
+                    }
+                </div>
                 {
                     typeof showHref === 'string' && compact ?
                         <ActionButton
-                            className={styles['action-button']}
+                            className={`${styles['action-button']} ${styles['show-button']}`}
                             icon={'play'}
                             label={t('SHOW')}
                             tabIndex={compact ? -1 : 0}
