fc85c18a8ef43cd3a001a6f3bfecabd465de3ac6
kKaskak <117831817+kKaskak@users.noreply.github.com>
2023-11-17 15:09:21 +0200

    refactor: usePWA to use useMemo instead

--- src/common/usePWA.js
+++ src/common/usePWA.js
@@ -3,14 +3,10 @@
 const React = require('react');
 
 const usePWA = () => {
-    const [isPWA, setIsPWA] = React.useState(false);
-
-    React.useEffect(() => {
+    const isPWA = React.useMemo(() => {
         const isIOSPWA = window.navigator.standalone;
         const isAndroidPWA = window.matchMedia('(display-mode: standalone)').matches;
-        if (isIOSPWA || isAndroidPWA) {
-            setIsPWA(true);
-        }
+        return isIOSPWA || isAndroidPWA;
     }, []);
     return isPWA;
 };
