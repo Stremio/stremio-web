14839d220e662b1ad63c28aae491d8a3984cd394
Tim <tymmesyde@gmail.com>
2023-12-15 16:13:27 +0100

    refactor(EventModal): implement two types of modals

--- src/common/EventModal/EventModal.js
+++ src/common/EventModal/EventModal.js
@@ -1,12 +1,16 @@
 // Copyright (C) 2017-2023 Smart code 203358507
 
 const React = require('react');
-const styles = require('./styles');
+const { useTranslation } = require('react-i18next');
 const Button = require('stremio/common/Button');
 const ModalDialog = require('stremio/common/ModalDialog');
 const useEvents = require('./useEvents');
+const styles = require('./styles');
+const { default: Icon } = require('@stremio/stremio-icons/react');
 
 const EventModal = () => {
+    const { t } = useTranslation();
+
     const { events, pullEvents, dismissEvent } = useEvents();
 
     const modal = React.useMemo(() => {
@@ -16,7 +20,7 @@ const EventModal = () => {
             null;
     }, [events]);
 
-    const onCloseRequest = React.useCallback(() => {
+    const onClose = React.useCallback(() => {
         modal?.id && dismissEvent(modal.id);
     }, [modal]);
 
@@ -26,10 +30,10 @@ const EventModal = () => {
 
     return (
         modal !== null ?
-            <ModalDialog className={styles['notification-modal']} onCloseRequest={onCloseRequest}>
+            <ModalDialog className={styles['event-modal']} onCloseRequest={onClose}>
                 {
                     modal.imageUrl ?
-                        <img className={styles['notification-image']} src={modal.imageUrl} />
+                        <img className={styles['image']} src={modal.imageUrl} />
                         :
                         null
                 }
@@ -43,18 +47,38 @@ const EventModal = () => {
                         }
                         {
                             modal.message ?
-                                <div className={styles['notification-label']}>{modal.message}</div>
+                                <div className={styles['label']}>{modal.message}</div>
                                 :
                                 null
                         }
                     </div>
                     {
-                        modal.addon.manifestUrl ?
-                            <Button className={styles['action-button']} href={`#/addons?addon=${encodeURIComponent(modal.addon.manifestUrl)}`} onClick={onCloseRequest}>
-                                <div className={styles['button-label']}>Learn more</div>
-                            </Button>
+                        modal?.addon?.name ?
+                            <div className={styles['addon-container']}>
+                                <Icon className={styles['icon']} name={'addons'} />
+                                <div className={styles['name']}>
+                                    { modal.addon.name }
+                                </div>
+                            </div>
                             :
                             null
+                    }
+                    {
+                        modal?.addon?.manifestUrl ?
+                            <Button className={styles['action-button']} href={`#/addons?addon=${encodeURIComponent(modal.addon.manifestUrl)}`} onClick={onClose}>
+                                <div className={styles['button-label']}>
+                                    { t('INSTALL_ADDON') }
+                                </div>
+                            </Button>
+                            :
+                            modal.externalUrl ?
+                                <Button className={styles['action-button']} href={modal.externalUrl} target={'_blank'}>
+                                    <div className={styles['button-label']}>
+                                        { t('LEARN_MORE') }
+                                    </div>
+                                </Button>
+                                :
+                                null
                     }
                 </div>
             </ModalDialog>
