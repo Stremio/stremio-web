be0ee2657ee9718545efb6ae3b96d97f571fb5f1
Tim <tymmesyde@gmail.com>
2024-09-25 15:34:32 +0200

    feat: impl openExternal for shell compatibility

--- src/common/Platform/Platform.tsx
+++ src/common/Platform/Platform.tsx
@@ -1,6 +1,5 @@
 import React, { createContext, useContext } from 'react';
 import useShell from './useShell';
-import { WHITELISTED_HOSTS } from 'stremio/common/CONSTANTS';
 
 interface PlatformContext {
     openExternal: (url: string) => void,
@@ -16,19 +15,11 @@ const PlatformProvider = ({ children }: Props) => {
     const shell = useShell();
 
     const openExternal = (url: string) => {
-        try {
-            const { hostname } = new URL(url);
-            const isWhitelisted = WHITELISTED_HOSTS.some((host: string) => hostname.endsWith(host));
-            const finalUrl = !isWhitelisted ? 'https://www.stremio.com/warning#' + encodeURIComponent(url) : url;
-        
-            if (shell.active) {
-                shell.send('open-external', finalUrl);
-            } else {
-                window.open(finalUrl, '_blank');
-            }
-        } catch (e) {
-            console.error('Failed to parse external url:', e);
-        }        
+        if (shell.active) {
+            shell.send('open-external', url);
+        } else {
+            window.open(url, '_blank');
+        }
     };
 
     return (
