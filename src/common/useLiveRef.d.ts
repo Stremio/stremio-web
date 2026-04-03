7ceacec79e6ead2c5d3f267ce290354831ea0c5a
mrcanelas <sialsan@hotmail.com>
2026-04-03 17:41:18 -0300

    refactor(useLiveRef): remove unused type declaration file

--- src/common/useLiveRef.d.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2017-2023 Smart code 203358507
-
-import type { MutableRefObject } from 'react';
-
-declare function useLiveRef<T>(value: T): MutableRefObject<T>;
-
-export = useLiveRef;
