d69a84f4a215e1240936e3623748fad2b77efe5a
Timothy Z. <timothy@stremio.com>
2026-06-16 11:45:58 +0300

    guard the support check for windows only

--- src/common/Platform/gpu.ts
+++ src/common/Platform/gpu.ts
@@ -1,7 +1,9 @@
+import { name } from './device';
+
 const rendererName = (): string => {
     const gl = document.createElement('canvas').getContext('webgl', { powerPreference: 'high-performance' });
     const ext = gl?.getExtension('WEBGL_debug_renderer_info');
     return (ext && gl?.getParameter(ext.UNMASKED_RENDERER_WEBGL)) || '';
 };
 
-export const supportsGpuVideoProcessing = /\brtx\b/i.test(rendererName());
+export const supportsGpuVideoProcessing = name === 'windows' && /\brtx\b/i.test(rendererName());
