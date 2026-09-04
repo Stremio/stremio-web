b961031426ed6a355e9bd739ddaa521e17774aae
Timothy Z. <timothy@stremio.com>
2026-09-04 23:21:05 +0300

    Destroy color picker with its change listener

--- src/components/ColorInput/ColorPicker/ColorPicker.js
+++ src/components/ColorInput/ColorPicker/ColorPicker.js
@@ -4,6 +4,7 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const classnames = require('classnames');
 const AColorPicker = require('a-color-picker');
+const useLiveRef = require('stremio/common/useLiveRef');
 const styles = require('./styles');
 
 const parseColor = (value) => {
@@ -13,29 +14,31 @@ const parseColor = (value) => {
 const ColorPicker = ({ className, value, onInput }) => {
     const pickerRef = React.useRef(null);
     const pickerElementRef = React.useRef(null);
+    const onInputRef = useLiveRef(onInput);
     React.useLayoutEffect(() => {
-        pickerRef.current = AColorPicker.createPicker(pickerElementRef.current, {
+        const picker = AColorPicker.createPicker(pickerElementRef.current, {
             color: parseColor(value),
             showHSL: false,
             showHEX: false,
             showRGB: false,
             showAlpha: true
         });
+        pickerRef.current = picker;
+        picker.on('change', (picker, value) => {
+            if (typeof onInputRef.current === 'function') {
+                onInputRef.current(parseColor(value));
+            }
+        });
         const pickerClipboard = pickerElementRef.current.querySelector('.a-color-picker-clipboard');
         if (pickerClipboard instanceof HTMLElement) {
             pickerClipboard.tabIndex = -1;
         }
-    }, []);
-    React.useLayoutEffect(() => {
-        if (typeof onInput === 'function') {
-            pickerRef.current.on('change', (picker, value) => {
-                onInput(parseColor(value));
-            });
-        }
         return () => {
-            pickerRef.current.off('change');
+            picker.off('change');
+            picker.destroy();
+            pickerRef.current = null;
         };
-    }, [onInput]);
+    }, []);
     React.useLayoutEffect(() => {
         const nextValue = parseColor(value);
         if (nextValue !== parseColor(pickerRef.current.color)) {
