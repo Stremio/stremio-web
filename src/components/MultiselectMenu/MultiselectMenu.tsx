eb88908024b02c8d433b93f4ca55f0e38dfd30d1
Timothy Z. <timothy@stremio.com>
2026-09-15 17:11:16 +0000

    Separate the multiselect label and handlers

--- src/components/MultiselectMenu/MultiselectMenu.tsx
+++ src/components/MultiselectMenu/MultiselectMenu.tsx
@@ -1,24 +1,23 @@
 // Copyright (C) 2017-2024 Smart code 203358507
 
 import React from 'react';
-import { Button } from 'stremio/components';
-import useBinaryState from 'stremio/common/useBinaryState';
-import Dropdown from './Dropdown';
 import classNames from 'classnames';
-import Icon from '@stremio/stremio-icons/react';
-import styles from './MultiselectMenu.less';
+import useBinaryState from 'stremio/common/useBinaryState';
 import useOutsideClick from 'stremio/common/useOutsideClick';
 import Popup from 'stremio/components/Popup';
+import Dropdown from './Dropdown';
+import Label from './Label';
+import styles from './MultiselectMenu.less';
 
 type Props = {
     className?: string,
-    title?: string | (() => string | null);
-    icon?: string;
-    options: MultiselectMenuOption[];
-    value?: any;
+    title?: string | (() => string | null),
+    icon?: string,
+    options: MultiselectMenuOption[],
+    value?: any,
     disabled?: boolean,
-    portal?: boolean;
-    onSelect: (value: any) => void;
+    portal?: boolean,
+    onSelect: (value: any) => void,
 };
 
 const MultiselectMenu = ({ className, title, icon, options, value, disabled, portal = false, onSelect }: Props) => {
@@ -30,53 +29,50 @@ const MultiselectMenu = ({ className, title, icon, options, value, disabled, por
     const selectedOption = options.find((opt) => opt.value === value);
     const labelText = typeof title === 'function' ? title() : title ?? selectedOption?.label;
 
-    const onOptionSelect = (selectedValue: string | number) => {
-        level ? setLevel(level + 1) : onSelect(selectedValue), closeMenu();
-    };
+    const onOptionSelect = React.useCallback((selectedValue: string | number) => {
+        if (level) {
+            setLevel(level + 1);
+        } else {
+            onSelect(selectedValue);
+        }
+        closeMenu();
+    }, [level, onSelect, closeMenu]);
+    const onLabelClick = React.useCallback((event: React.MouseEvent<HTMLDivElement>) => {
+        setAutoFocus(event.detail === 0);
+        toggleMenu();
+    }, [toggleMenu]);
 
     const containerClassName = classNames(styles['multiselect-menu'], { [styles['active']]: menuOpen }, className);
-    const label = <Button
-        className={classNames(styles['multiselect-button'], { [styles['open']]: menuOpen })}
-        disabled={disabled}
-        onClick={(event) => {
-            setAutoFocus(event.detail === 0);
-            toggleMenu();
-        }}
-        tabIndex={0}
-        aria-haspopup='listbox'
-        aria-expanded={menuOpen}
-        role={icon ? 'button' : undefined}
-        aria-label={icon ? labelText : undefined}
-        title={icon ? labelText : undefined}
-    >
-        <div className={styles['label']}>
-            {icon ? <Icon name={icon} className={styles['label-icon']} /> : labelText}
+    const renderLabel = React.useCallback(({ ref, className: popupClassName }: { ref: React.Ref<HTMLDivElement>; className?: string }) => (
+        <div ref={ref} className={classNames(containerClassName, popupClassName)}>
+            <Label text={labelText} icon={icon} disabled={disabled} open={menuOpen} onClick={onLabelClick} />
         </div>
-        {!icon && <Icon name={'caret-down'} className={classNames(styles['icon'], { [styles['open']]: menuOpen })} />}
-    </Button>;
-    const menu = menuOpen ? <Dropdown
-        level={level}
-        setLevel={setLevel}
-        options={options}
-        onSelect={onOptionSelect}
-        menuOpen={menuOpen}
-        value={value}
-    /> : null;
+    ), [containerClassName, labelText, icon, disabled, menuOpen, onLabelClick]);
+    const renderMenu = React.useCallback(() => menuOpen ? (
+        <Dropdown
+            level={level}
+            setLevel={setLevel}
+            options={options}
+            onSelect={onOptionSelect}
+            menuOpen={menuOpen}
+            value={value}
+        />
+    ) : null, [menuOpen, level, options, onOptionSelect, value]);
 
-    return portal ? <Popup
-        open={menuOpen}
-        portal={true}
-        autoFocus={autoFocus}
-        menuClassName={styles['menu-popup']}
-        onCloseRequest={closeMenu}
-        renderLabel={({ ref, className: popupClassName }: { ref: React.Ref<HTMLDivElement>; className: string }) => (
-            <div ref={ref} className={classNames(containerClassName, popupClassName)}>{label}</div>
-        )}
-        renderMenu={() => menu}
-    /> : (
+    return portal ? (
+        <Popup
+            open={menuOpen}
+            portal={true}
+            autoFocus={autoFocus}
+            menuClassName={styles['menu-popup']}
+            onCloseRequest={closeMenu}
+            renderLabel={renderLabel}
+            renderMenu={renderMenu}
+        />
+    ) : (
         <div className={containerClassName} ref={multiselectMenuRef}>
-            {label}
-            {menu}
+            <Label text={labelText} icon={icon} disabled={disabled} open={menuOpen} onClick={onLabelClick} />
+            {renderMenu()}
         </div>
     );
 };
