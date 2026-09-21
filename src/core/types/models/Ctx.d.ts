775c2cf9aa46221f80b968e5339fe3db9e041328
TeeJay <3618451+ThiaJay@users.noreply.github.com>
2026-09-21 11:28:21 +0100

    Type shared Skip Intro preference

--- src/core/types/models/Ctx.d.ts
+++ src/core/types/models/Ctx.d.ts
@@ -29,6 +29,7 @@ type Settings = {
     hideSpoilers: boolean,
     gamepadSupport: boolean,
     nextVideoNotificationDuration: number,
+    skipIntro: boolean,
     playInBackground: boolean,
     playerType: string | null,
     secondaryAudioLanguage: string | null,
